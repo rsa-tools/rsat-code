2a0af587b244054d795dde8d54606f66a01c51c2
jvanheld <jvanheld>
2000-03-10 14:22:59 +0000

    - export expression only for polypeptides that have an identifier in the Polypeptide.mldbm file - select an organism with the -org option

--- perl-scripts/parsers/parse_genes.pl
+++ perl-scripts/parsers/parse_genes.pl
@@ -1,9 +1,9 @@
 #!/usr/bin/perl
 ############################################################
 #
-# $Id: parse_genes.pl,v 1.2 2000/03/08 00:22:15 jvanheld Exp $
+# $Id: parse_genes.pl,v 1.3 2000/03/10 14:22:59 jvanheld Exp $
 #
-# Time-stamp: <2000-03-08 01:21:40 jvanheld>
+# Time-stamp: <2000-03-10 02:17:58 jvanheld>
 #
 ############################################################
 
@@ -15,32 +15,32 @@ if ($0 =~ /([^(\/)]+)$/) {
     push (@INC, "$`"); ### add the program's directory to the lib path
 }
 require "PFBP_classes.pl";
+require "PFBP_config.pl";
+require "PFBP_util.pl";
+require "PFBP_loading_util.pl"; ### for converting polypeptide IDs into ACs
 require "PFBP_parsing_util.pl";
 
 
 package main;
 
 ### initialization
-$start_time = `date +%Y%m%d_%H%M%S`;
+$start_time = &AlphaDate;
 
 ### files to parse
 $KEGG_dir = "~jvanheld/pfmp/Databases/KEGG/downloaded";
 
+
+@selected_organisms= ();
+
 $in_file{yeast} = "gunzip -c $KEGG_dir/genomes/genes/S.cerevisiae.ent.gz | ";
 $in_file{human} = "gunzip -c $KEGG_dir/genomes/genes/H.sapiens.ent.gz | ";
-$in_file{coli} = "gunzip -c $KEGG_dir/genomes/genes/E.coli.ent.gz | ";
+$in_file{ecoli} = "gunzip -c $KEGG_dir/genomes/genes/E.coli.ent.gz | ";
 
 $organism{yeast}->{name} = "Saccharomyces cerevisiae";
-$organism{coli}->{name} = "Escherichia coli";
+$organism{ecoli}->{name} = "Escherichia coli";
 $organism{human}->{name} = "Homo sapiens";
 
-$out_file{error} = "Gene.errors.txt";
-$out_file{stats} = "Gene.stats.txt";
-$out_file{genes} = "Gene.obj";
-$out_file{synonyms} = "Gene.synonyms.tab";
-$out_file{mldbm_genes} = "Gene.mldbm";
-$out_file{expressions} = "Expression.obj";
-$out_file{mldbm_expressions} = "Expression.mldbm";
+
 $warn_level = 0;
 $out_format = "obj";
 
@@ -58,10 +58,41 @@ open ERR, ">$out_file{error}" || die "Error: cannot write error file $out_file{e
 ### default output fields for each class
 @{$out_fields{'PFBP::Gene'}} = qw( id organism names position definition source);
 
-@{$out_fields{'PFBP::Expression'}} = qw( id input output );
+@{$out_fields{'PFBP::Expression'}} = qw( id gene_id polypeptide_id );
 
 &ReadArguments;
+
+
+### outfile names
+$suffix = "";
+foreach $organism (@selected_organisms) {
+  $suffix .= "_$organism";
+}
+if ($export{all}) {
+  $suffix .= "_all";
+} elsif ($export{enzymes}) {
+  $suffix .= "_enz";
+}
+$suffix .= "_test" if ($test);
+
+$out_file{error} = "Gene".$suffix.".errors.txt";
+$out_file{stats} = "Gene".$suffix.".stats.txt";
+$out_file{genes} = "Gene".$suffix.".obj";
+$out_file{synonyms} = "Gene".$suffix.".synonyms.tab";
+$out_file{mldbm_genes} = "Gene".$suffix.".mldbm";
+$out_file{expressions} = "Expression".$suffix.".obj";
+$out_file{mldbm_expressions} = "Expression".$suffix.".mldbm";
+
+### select all organisms if none was selected (-org)
+unless ($#selected_organisms >= 0) {
+  push @selected_organisms, "ecoli";
+  push @selected_organisms, "human";
+  push @selected_organisms, "yeast";
+}
+
 &DefaultVerbose if ($warn_level >= 1);
+warn "; Selected organisms\n;\t", join("\n;\t", @selected_organisms), "\n"
+    if ($warn_level >= 1);
 
 ### actualize parameters
 if ($test) {
@@ -72,14 +103,16 @@ if ($test) {
   }
 }
 
+
 ### parse data from original files
-while (my ($org, $file) = each %in_file) {
-  &ParseKeggFile($file, 
+foreach $org (@selected_organisms) {
+  &ParseKeggFile($in_file{$org}, 
 		 $genes, 
 		 source=>"KEGG", 
 		 organism=>$organism{$org}->{name});
 }
 $genes->index_names();
+
 
 &CreateExpression;
 $expressions->index_names();
@@ -88,8 +121,9 @@ $expressions->index_names();
 &ExportClasses($out_file{genes}, $out_format, PFBP::Gene);
 &ExportClasses($out_file{expressions}, $out_format, PFBP::Expression);
 
-$genes->dump_tables();
-$expressions->dump_tables();
+
+$genes->dump_tables($suffix);
+$expressions->dump_tables($suffix);
 
 $genes->export('MLDBM',$out_file{mldbm_genes});
 $expressions->export('MLDBM',$out_file{mldbm_expressions});
@@ -98,10 +132,10 @@ $expressions->export('MLDBM',$out_file{mldbm_expressions});
 
 ### report execution time
 if ($warn_level >= 1) {
-  $done_time = `date +%Y%m%d_%H%M%S`;
+  $done_time = &AlphaDate;
   print STDERR ";\n";
-  print STDERR "; job started $start_time";
-  print STDERR "; job done    $done_time";
+  print STDERR "; job started $start_time\n";
+  print STDERR "; job done    $done_time\n";
 }
 
 close ERR;
@@ -147,6 +181,17 @@ OPTIONS
 	-o	output file
 		If ommited, STDOUT is used. 
 		This allows to insert the program within a unix pipe
+	-enz	export enzymes only
+	-all	export all polypeptides for the selected organisms
+		(default)
+	-org	select an organism for exportation
+		can be used reiteratively in the command line 
+		to select several organisms
+		   Supported organisms :
+			ecoli
+			human
+			yeast
+		by default, all organisms are selected
 EndHelp
   close HELP;
 }
@@ -183,7 +228,18 @@ sub ReadArguments {
 	     ($ARGV[$a] eq "-help")) {
       &PrintHelp;
       exit(0);
+
+      ### select enzymes for exportation
+    } elsif ($ARGV[$a] =~ /^-org/) {
+      push @selected_organisms, lc($ARGV[$a+1]);
+      
+      ### select enzymes for exportation
+    } elsif ($ARGV[$a] =~ /^-enz/) {
+      $main::export{enzymes} = 1;
+    } elsif ($ARGV[$a] =~ /^-all/) {
+      $main::export{all} = 1;
     }
+    
   }
 }
 
@@ -194,29 +250,65 @@ sub CreateExpression {
 ### of the polypeptide that the gene codes for
 ### this sbroutine etxracts this information, and instantiates 
 ### objects in the PFBP::Expression class
+
+  ### load polypeptide class 
+  ### for converting Swissprot IDs in ACs
+#  $class_holder{polypeptides} = &ImportClass("$data_dir/polypeptides/Polypeptide.mldbm", "PFBP::Polypeptide");
+ 
+  
+  $infile{polypeptide_index} = "$mldbm_dir/Polypeptide".$suffix."__name_index.mldbm";
+  warn "; opening polpeptide index $infile{polypeptide_index}\n"
+      if ($warn_level >= 1);
+
+  ### open the persistent polypeptide index
+  tie %index, 'MLDBM', $infile{polypeptide_index} ||
+      die "Error: cannot tie file $infile{polypeptide_index}";
+  
   foreach my $gene (PFBP::Gene->get_objects()) {
     my $gene_id = $gene->get_attribute("id");
     if (my $definition = join(" ", $gene->get_attribute("definition"))) {
       if ($definition =~ /\[SP:(\S+)\]/) {
-	while ($definition =~ /\[SP:(\S+)\]/) {
-	  my $prot_id = $1;
-	  if (my $expression = $expressions->new_object()) {
-	    print STDERR "created expression for gene $gene_id\n" 
-	      if ($warn_level >= 2);
-	    $expression->set_attribute("input",$gene_id);
-	    $expression->set_attribute("output",$prot_id);
-	    $definition = $';
+	while ($definition =~ s/\[SP:(\S+)\]//) {
+	  
+	  ### identify the polypeptide
+	  my $swissprot_id = $1;
+	  my $polypeptide = undef;
+	  my $polypeptide_id = undef;
+	  
+	  if (exists $index{uc($swissprot_id)}) {
+	    $id_table = $index{uc($swissprot_id)};
+	    $polypeptide_id = $$id_table[0];
 	  } else {
-	    die "Error: could not instantiated expression\n";
+	    $error_message = "ERROR : could not identify polypeptide $swissprot_id\n";
+	    print ERR $error_message;
+	    warn  $error_message if ($warn_level >= 2);
+	    next;
+	  }
+	  
+	  $expr_id = "expr_".$gene_id."_".$polypeptide_id;
+	  if (my $expression = $expressions->new_object(id=>$expr_id,
+							source=>"KEGG")) {
+	    print STDERR "created expression from gene $gene_id to polypeptide $polypeptide_id\n" 
+		if ($warn_level >= 2);
+	    $expression->set_attribute("gene_id",$gene_id);
+	    $expression->set_attribute("polypeptide_id",$polypeptide_id);
+	  } else {
+	    die "Error: could not instantiate expression\n";
 	  }
 	}
       } else {
-	print ERR "WARNING: gene $gene_id could not be linked to swissprot\n";
-	print ERR "\tdefinition\t$definition\n";
+	$error_message =  "WARNING: gene $gene_id could not be linked to swissprot\n";
+	$error_message .= "\tdefinition\t$definition\n";
+	print ERR $error_message;
+	warn $error_message if ($warn_level >= 3);
       }
     } else {
       print ERR "WARNING: gene $gene_id has no definition field\n";
     }
+    
+    untie %polypeptides_name_index ||
+	die "Error: cannot untie file $infile{polypeptide_index}";
+    
   }
 }
 
