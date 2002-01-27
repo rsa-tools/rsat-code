a501ff2b3515fbedc0da81ac83206a31be0bc5eb
jvanheld <jvanheld>
2002-01-27 00:41:52 +0000

    *** empty log message ***

--- perl-scripts/parse-location.pl
+++ perl-scripts/parse-location.pl
@@ -1,9 +1,9 @@
 #!/usr/bin/perl -w
 ############################################################
 #
-# $Id: parse-location.pl,v 1.1 2002/01/26 23:35:07 jvanheld Exp $
+# $Id: parse-location.pl,v 1.2 2002/01/27 00:41:52 jvanheld Exp $
 #
-# Time-stamp: <2001-12-09 22:57:25 jvanheld>
+# Time-stamp: <2002-01-27 01:28:23 jvanheld>
 #
 ############################################################
 #use strict;;
@@ -20,8 +20,6 @@ local %infile = ();
 local %outfile = ();
 
 local $verbose = 0;
-local $in = STDIN;
-local $out = STDOUT;
 
 &ReadArguments;
 
@@ -29,12 +27,41 @@ local $out = STDOUT;
 #### check argument values ####
 
 
-### open output file ###
+### open output files ###
 $out = &OpenOutputFile($outfile{output});
+print $out join ("\t", ";ORF", "EXON"), "\n";
 
 ##### read input #####
 $in = &OpenInputFile($infile{input});
 while (<$in>) {
+    chomp;
+    next if (/^;/);
+    next unless (/\S/);
+    my @fields = split "\t";
+    my $orf_id = $fields[0];
+    my $location = $fields[1];
+    my @exons = split ",", $location;
+    my @lefts = ();
+    my @rights = ();
+
+#    print join ("\t", "HELLO", $orf_id, $#exons + 1, $location);
+    foreach my $e (0..$#exons) {
+	my $exon = $exons[$e];
+  	my @limits = split "-", $exon;
+  	if ($limits[0] < $limits[1] ) {
+  	    $strand = "R";
+  	} else {
+  	    $strand = "D";
+  	}
+  	my $left = &min(@limits);
+  	my $right = &max(@limits);
+	$lefts[$e] = $left;
+	$rights[$e] = $right;
+	$exon =~ s/\-/\.\./;
+	print $out join ("\t", $orf_id, $left."..".$right, 
+#			$exon, $e+1, $left, $right, $strand
+			), "\n";
+    }
 
 }
 
@@ -71,15 +98,17 @@ sub PrintHelp {
   open HELP, "| more";
   print HELP <<End_of_help;
 NAME
-	template
+	parse-location
 
         2001 by Jacques van Helden (jvanheld\@ucmb.ulb.ac.be)
 	
 USAGE
-        template [-i inputfile] [-o outputfile] [-v]
+        parse-location [-i inputfile] [-o outputfile] [-v]
 
 DESCRIPTION
-	
+	Parse ORFS locations and generates one file with introns 
+	and one with exons.
+
 OPTIONS
 	-h	(must be first argument) display full help message
 	-help	(must be first argument) display options
@@ -87,7 +116,7 @@ OPTIONS
 	-i inputfile
 		if not specified, the standard input is used.
 		This allows to place the command within a pipe.
-	-o outputfile
+	-out	output file
 		if not specified, the standard output is used.
 		This allows to place the command within a pipe.
 
@@ -100,12 +129,12 @@ sub PrintOptions {
 #### display short help message #####
   open HELP, "| more";
   print HELP <<End_short_help;
-template options
+parse-location options
 ----------------
 -h	(must be first argument) display full help message
 -help	(must be first argument) display options
 -i	input file
--o	output file
+-out	output file
 -v	verbose
 End_short_help
   close HELP;
@@ -136,25 +165,16 @@ sub ReadArguments {
 	} elsif ($ARGV[$a] eq "-i") {
 	    $infile{input} = $ARGV[$a+1];
 	    
-	    ### output file ###
+	    ### output file
 	} elsif ($ARGV[$a] eq "-o") {
 	    $outfile{output} = $ARGV[$a+1];
-	    
-	    ### organism
-	} elsif ($ARGV[$a] eq "-org") {
-	    $organism_name = $ARGV[$a+1];
-	    unless (defined($supported_organism{$organism_name})) {
-		die ("Error: organism '$organism_name' is not supported on this site",
-		     $supported_organisms,
-		     "\n");
-	    }
 	    
 	}
     }
 }
 
 sub Verbose {
-    print $out "; template ";
+    print $out "; parse-location ";
     &PrintArguments($out);
     if (defined(%infile)) {
 	print $out "; Input files\n";
