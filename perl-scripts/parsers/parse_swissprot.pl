11ccccf97dda0ee92100e4db7b6269eb44e14d3f
jvanheld <jvanheld>
2002-03-19 12:05:00 +0000

    - added the option -acs, to allow specifying a list of accession numbers

--- perl-scripts/parsers/parse_swissprot.pl
+++ perl-scripts/parsers/parse_swissprot.pl
@@ -1,9 +1,9 @@
 #!/usr/bin/perl
 ############################################################
 #
-# $Id: parse_swissprot.pl,v 1.10 2002/03/19 11:43:38 jvanheld Exp $
+# $Id: parse_swissprot.pl,v 1.11 2002/03/19 12:05:00 jvanheld Exp $
 #
-# Time-stamp: <2002-03-19 12:41:45 jvanheld>
+# Time-stamp: <2002-03-19 13:04:44 jvanheld>
 #
 ############################################################
 
@@ -26,7 +26,7 @@ package main;
 {
     
     
-    ### organism selection
+    #### organism selection
     @selected_organisms = ();
     $full_name{yeast} = "Saccharomyces cerevisiae (Baker's yeast)";
     $full_name{ecoli} = "Escherichia coli";
@@ -34,28 +34,22 @@ package main;
 
     $dir{delivery} = "/rubens/dsk3/genomics/delivery/internal/swissprot_parsed";
 
-    ### input directories and files
-#    $dir{input} = "/win/databases/downloads/ftp.ebi.ac.uk/pub/databases/";
+    #### input directories and files
     $dir{input} = "/win/databases/downloads/ftp.expasy.org/databases/sp_tr_nrdb";
-#    $dir{swissprot} = $dir{input};
-#    $dir{swissprot} = $dir{input}."/swiss-prot/release_compressed";
-#    $dir{trembl} = $dir{input};
-
     $source{swissprot} = "sprot";
     $source{trembl} = "trembl";
     $source{trembl_new} = "trembl_new";
-#    $source{swissprot} = "sprot40";
-#    $source{yeast} = "sptrembl/fun";
-#    $source{human} = "sptrembl/hum";
-#    $source{ecoli} = "sptrembl/pro";
-#  &OpenIndex("ECSet");
     
-    ### export classes
+    #### export classes
     push @classes, "PFBP::Polypeptide";
-#    @{$out_fields{'PFBP::Polypeptide'}} = qw( id name description gene organisms swissprot_acs swissprot_ids names );
     
     &ReadArguments;
     
+    #### check option compatibility
+    if ($in_file{acs} && ($#selected_organisms >= 0)) {
+	die "Error: options -acs and -org are incompatible\n";
+    }
+
     #### output directory
     $out_format = "obj";
     unless (defined($dir{output})) {
@@ -75,30 +69,16 @@ package main;
 	system "\\rm -f $dir{output}/*";
     }
 
-    #### output file names depend on the organisms to parse and source 
-    #### -> defined after reading arguments
-#  $suffix = "";
-#  foreach $organism (@selected_organisms) {
-#      $suffix .= "_$organism";
-#  }
-#  if ($export{enzymes}) {
-#      $suffix .= "_enz";
-#  }
-#  $suffix .= "_test" if ($test);
-    
+    #### output file names
     $out_file{polypeptides} = $dir{output}."/Polypeptide".$suffix.".obj";
-##  $out_file{mldbm_polypeptides} = $dir{output}."/Polypeptide".$suffix.".mldbm";
-#  $out_file{mldbm_polypeptide_index} = $dir{output}."/Polypeptide".$suffix."__name_index.mldbm";
-#  $out_file{catalyses} = $dir{output}."/Catalysis".$suffix.".obj";
-#  $out_file{mldbm_catalyses} = $dir{output}."/Catalysis".$suffix.".mldbm";
     $out_file{errors} = $dir{output}."/swissprot".$suffix.".errors.txt";
     $out_file{stats} = $dir{output}."/swissprot".$suffix.".stats.txt";
     
-    ### open error report file
+    #### open error report file
     open ERR, ">$out_file{errors}" || 
 	die "Error: cannot write error file $out_file{errors}\n";
     
-    ### select three predefined organisms if none was selected (-org)
+    #### select three predefined organisms if none was selected (-org)
     unless (($#selected_organisms >= 0) || 
 	    ($export{allorg})){
 	push @selected_organisms, "ecoli";
@@ -106,14 +86,14 @@ package main;
 	push @selected_organisms, "yeast";
     }
 
-    ### define regular expressions to match the selected organisms
+    #### define regular expressions to match the selected organisms
     foreach $organism (@selected_organisms) {
 	$regexp = $full_name{$organism};
 	$regexp = $` if ($regexp =~ /\(/); 
 	$regexp = $` if ($regexp =~ /\)/);
 	$regexp = $` if ($regexp =~ /\'/);
 	$selected_organism{uc($full_name{$organism})} = $regexp;
-	push @regexps, qr /$regexp/; ### list of regular expressions
+	push @regexps, qr /$regexp/; #### list of regular expressions
     }
     
 
@@ -139,23 +119,20 @@ package main;
 	warn "; Selected ACs\n;\t",  join ("\n;\t", keys %selected_acs), "\n" if ($warn_level >= 0);
     }
 
-    ### by default, parse swissprot only
+    #### by default, parse swissprot only
     unless (defined(%data_sources)) {
 	$data_sources{swissprot} = 1;
     }
-#    if ($data_sources{swissprot}) {
-#	$in_file{$source{swissprot}} = "gunzip -c ".$dir{swissprot}."/".$source{swissprot}.".dat.gz | ";
-#	$files_to_parse{$source{swissprot}} = 1;
-#    }
+
     foreach $db (keys %data_sources) {
 	if ($data_sources{$db}) {
 	    $in_file{$source{$db}} = "gunzip -c ".$dir{input}."/".$source{$db}.".dat.gz | ";
 	    $files_to_parse{$source{$db}} = 1;
 	}
-	warn $db, "\t", $source{$db}, "\t", $in_file{$source{$db}}, "\n";
+	warn ";\tdata source\t", $db, "\t", $in_file{$source{$db}}, "\n" if ($warn_level >= 2);
     }
 
-    ### create class holders
+    #### create class holders
     $polypeptides = PFBP::ClassFactory->new_class(object_type=>"PFBP::Polypeptide",
 						  prefix=>"spp_");
     $polypeptides->set_out_fields( qw( id 
@@ -174,7 +151,7 @@ package main;
     $polypeptides->set_attribute_header("features", join ("\t", "Feature_key", "from", "to", "description") );
     $polypeptides->set_attribute_header("comments", join ("\t", "topic", "comment") );
 
-    ### testing mode
+    #### testing mode
     if ($test) {
 	warn ";TEST\n" if ($warn_level >= 1);
 	### fast partial parsing for debugging
@@ -185,7 +162,7 @@ package main;
     }
 
 
-    ### default verbose message
+    #### default verbose message
     if ($warn_level >= 1) {
 	warn "; Selected organisms\n;\t", join("\n;\t", @selected_organisms), "\n";
 	warn "; Polypeptide classes\n;\t", join("\n;\t", keys %export), "\n";
@@ -194,11 +171,10 @@ package main;
     }
 
 
-    ### parse data from original files
+    #### parse data from original files
     foreach $source (keys %files_to_parse) {
 	&ParseSwissprot($in_file{$source}, "SWISSPROT:".$source);
     }
-#  &CreateCatalyses;
 
     #### use first name as primary name
 #  foreach $polypeptide ($polypeptides->get_objects()) {
@@ -208,26 +184,12 @@ package main;
 #  }
 
 
-#### print the result
+    #### print the result
     &PrintStats($out_file{stats}, @classes);
     $polypeptides->dump_tables($suffix);
-    #  $catalyses->dump_tables($suffix);
     &ExportClasses($out_file{polypeptides}, $out_format,PFBP::Polypeptide) if ($export{obj});
-#  &ExportClasses($out_file{catalyses}, $out_format, PFBP::Catalysis);
-#  $catalyses->export('MLDBM',$out_file{mldbm_catalyses});
-#  $polypeptides->export("MLDBM",$out_file{mldbm_polypeptides});
-#  $polypeptides->export_name_index("MLDBM",$out_file{mldbm_polypeptide_index});
 
-
-#if ($out_file{stats}) {
-#  open STDOUT, ">>$out_file{stats}"
-#    || die "Error: cannot write stat file $out_file{stats}\n";
-#  &SwissStats;
-#  close $out if ($out_file{stats});
-#}
-
-
-### report execution time
+    ### report execution time
     if ($warn_level >= 1) {
 	$done_time = &AlphaDate;
 	warn (";\n",
@@ -235,12 +197,11 @@ package main;
 	      "; job done    $done_time\n")
 	}
 
-#  &CloseIndex("ECSet");
     close ERR;
-
+    
     system "gzip -f $dir{output}/*.tab $dir{output}/*.txt";
     system "gzip -f $dir{output}/*.obj" if ($export{obj});
-
+    
     exit(0);
 }
 
@@ -305,6 +266,7 @@ OPTIONS
 		accession numbers (AC field) specified in the
 		file. Each accession number must come as the first
 		word of a new line (all subsequent words are ignored).
+		(this option is incompatible with the option -org)
 	-clean	remove all files from the output directory before
 		parsing
 EXAMPLE
@@ -322,50 +284,50 @@ sub ReadArguments {
 	    ($ARGV[$a+1] =~ /^\d+$/)){
 	    $main::warn_level = $ARGV[$a+1];
 	    
-	    ### test run
+	    #### test run
 	} elsif ($ARGV[$a] eq "-test") {
 	    $main::test = 1;
 	    
-	    ### clean
+	    #### clean
 	} elsif ($ARGV[$a] eq "-clean") {
 	    $main::clean = 1;
 	    
-	    ### output file
+	    #### output file
 	} elsif ($ARGV[$a] eq "-obj") {
 	    $a++;
 	    $main::export{obj} = 1;
 	    
-	    ### help
+	    #### help
 	} elsif (($ARGV[$a] eq "-h") ||
 		 ($ARGV[$a] eq "-help")) {
 	    &PrintHelp;
 	    exit(0);
 
-	    ### input directory
+	    #### input directory
 	} elsif ($ARGV[$a] =~ /^-indir/) {
 	    $dir{input} = $ARGV[$a+1];
 
-	    ### input file with a list of Swissprot AC
+	    #### input file with a list of Swissprot AC
 	} elsif ($ARGV[$a] =~ /^-acs/) {
 	    $in_file{acs} = $ARGV[$a+1];
 
-	    ### output directory
+	    #### output directory
 	} elsif ($ARGV[$a] =~ /^-outdir/) {
 	    $dir{output} = $ARGV[$a+1];
 
-	    ### select organisms for exportation
+	    #### select organisms for exportation
 	} elsif ($ARGV[$a] =~ /^-org/) {
 	    push @selected_organisms, lc($ARGV[$a+1]);
 	    #### export all organisms
 	} elsif ($ARGV[$a] =~ /^-allorg/) {
 	    $main::export{allorg} = 1;
 	    
-	    ### select enzymes for exportation
+	    #### select enzymes for exportation
 	} elsif ($ARGV[$a] =~ /^-data/) {
 	    $data_sources{lc($ARGV[$a+1])} = 1;
 	    
 
-	    ### select enzymes for exportation
+	    #### select enzymes for exportation
 	} elsif ($ARGV[$a] =~ /^-enz/) {
 	    $main::export{enzymes} = 1;
 	}
@@ -379,14 +341,6 @@ sub ReadArguments {
 sub ParseSwissprot {
     my ($input_file, $source) = @_;
     $source = $input_file unless ($source);
-#    my %attrib_keys = (
-#		       id=>"ID",
-#		       ac=>"AC",
-#		       gn=>"Gene",
-#		       ft=>"Features",
-#		       cc=>"Comments"
-#		       );
-#    my $class = PFBP::Polypeptide;
     
     warn (";\n; ", &AlphaDate,  " parsing polypeptides from $input_file\n")
 	if ($warn_level >= 1);
@@ -394,14 +348,12 @@ sub ParseSwissprot {
     open DATA, $input_file || 
 	die "Error: cannot open data file $input_file\n";
     
-    # Read an entire record at a time
+    #### Read an entire record at a time
     $/ = "\/\/\n";
     
     my $entries = 0;
     while ($text_entry = <DATA>){
 	$entries++;
-	
-	my $export = 0;
 	
 	### check that the entry matches organism name 
 	### before converting it to an object
@@ -417,13 +369,24 @@ sub ParseSwissprot {
 	}
 	next unless $parse;
 	
-	# Read the entry
+	#### convert the entry into an object
 	my $object_entry = SWISS::Entry->fromText($text_entry);
+
+	#### initialize the export flag
+	my $export = 0;
 	
-	### check whether the polypeptide organism(s)
-	### match the organism selection
-	my @organisms = $object_entry->OSs->elements;
-	unless ($export{allorg}) {
+	#### get the polypeptide accession number
+	my $swissprot_ac = $object_entry->AC;
+
+	#### check whether the accession number was specified for export 
+	if ($in_file{acs}) {
+	    $export = 1 if ($selected_acs{$swissprot_ac});
+	} elsif ($export{allorg}) {
+	    $export = 1;
+	} else {
+	    #### check whether the polypeptide organism(s)
+	    #### match the organism selection
+	    my @organisms = $object_entry->OSs->elements;
 	    foreach $organism (@organisms) {
 		if ($selected_organism{uc($organism)}) {
 		    $export = 1;
@@ -431,12 +394,6 @@ sub ParseSwissprot {
 		}
 	    }
 	}
-	
-	# get the polypeptide accession number
-	my $swissprot_ac = $object_entry->AC;
-
-	#### check whether the accession number was specified for export 
-	$export = 1 if ($selected_acs{$swissprot_ac});
 	
 	next unless $export;
 	my @swissprot_ids = $object_entry->IDs->elements;
