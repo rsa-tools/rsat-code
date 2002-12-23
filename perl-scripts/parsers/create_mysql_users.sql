41b72dd2e30addae7a9b201ac82d51bc15464095
jvanheld <jvanheld>
2002-12-23 23:07:57 +0000

    *** empty log message ***

--- perl-scripts/parsers/create_mysql_users.sql
+++ perl-scripts/parsers/create_mysql_users.sql
@@ -7,14 +7,17 @@
 ################################################################
 # Create databases for amaze parsing result
 create database swissprot;
-GRANT ALL ON swissprot TO jvanheld;
-GRANT ALL ON swissprot TO rekins;
+GRANT ALL ON swissprot.* TO swissprot;
+GRANT ALL ON swissprot.* TO jvanheld;
+GRANT ALL ON swissprot.* TO rekins;
 
 create database kegg;
-GRANT ALL ON kegg TO jvanheld;
-GRANT ALL ON kegg TO rekins;
+GRANT ALL ON kegg.* TO kegg;
+GRANT ALL ON kegg.* TO jvanheld;
+GRANT ALL ON kegg.* TO rekins;
 
 create database sigtrans;
+GRANT ALL ON sigtrans TO sigtrans;
 GRANT ALL ON sigtrans TO jvanheld;
 GRANT ALL ON sigtrans TO rekins;
 
@@ -22,7 +25,21 @@ GRANT ALL ON sigtrans TO rekins;
 ################################################################
 # For info: commands required to throw a user
 ################################################################
-# use mysql;
+
+use mysql;
+
+DELETE FROM user WHERE user='kegg';
+DELETE FROM tables_priv WHERE user='kegg';
+DELETE FROM db WHERE user='kegg';
+
+DELETE FROM user WHERE user='swissprot';
+DELETE FROM tables_priv WHERE user='swissprot';
+DELETE FROM db WHERE user='swissprot';
+
+DELETE FROM user WHERE user='sigtrans';
+DELETE FROM tables_priv WHERE user='sigtrans';
+DELETE FROM db WHERE user='sigtrans';
+
 DELETE FROM user WHERE user='jvanheld';
 DELETE FROM tables_priv WHERE user='jvanheld';
 DELETE FROM db WHERE user='jvanheld';
