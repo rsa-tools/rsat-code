e12a136a3ec5afae84abb337ee535b381c3e9597
jvanheld <jvanheld>
2005-04-07 22:16:23 +0000

    *** empty log message ***

--- R-scripts/distrib_fitting.R
+++ R-scripts/distrib_fitting.R
@@ -1,4 +1,3 @@
-
 source("~/research/R-files/config.R")
 source(file.path(dir.util, 'util.R'))
 source(file.path(dir.util, 'util_test_fitting_poisson.R'))
@@ -10,16 +9,22 @@ analyze.fitting <- function(file.prefix, ## file resulting from the perl script 
 			    theor, ## theoretcial distribution; supported: negbin, poisson
 			    dir.input='.', ## Input directory (containing the input file)
 			    dir.output='.', ## Directory where the fitting files have to be stored)
-			    dir.figures=NA ## Directorb where the figrues have to be stored
+			    dir.figures=NA, ## Directorb where the figrues have to be stored
+                            export.plots=T ## Export plots
 			    ) {
 
-
+  ## generate an object with the result
+  result <- list()
+  result$file.prefix <- file.prefix
+  result$theor <- theor
+  result$dir.input <- dir.input
+  result$dir.output <- dir.output
+  
+  ## Check output directories
   if (is.na(dir.figures)) {
       dir.figures <- file.path(dir.output, 'figures')
   }
-
   verbose (dir.output)
-
   for (dir in c(dir.output, dir.figures)) {
     if (!file.exists(dir)) {
       dir.create(dir)
@@ -52,12 +57,13 @@ analyze.fitting <- function(file.prefix, ## file resulting from the perl script 
   fitting <- read.table(file.fitting, sep="\t", comment.char=";",row.names = 1)
   names(fitting) <- header
 
+  result$nrow <- nrow(fitting)
+  
   ################################################################
   ## Calculate P-value, E-value and significance
   fitting$p.value <- pchisq(fitting$chi2, fitting$df,lower.tail=F)
   fitting$e.value <- fitting$p.value * nrow(fitting)
   fitting$sig <- -log(fitting$e.value, base=10)
-  
   
   ## Sort the table according to the P-value
   fitting.sorted <- fitting[order(fitting$p.value),]
@@ -74,51 +80,59 @@ analyze.fitting <- function(file.prefix, ## file resulting from the perl script 
   ## Count number of significant motifs
   verbose(paste(theor, 'fitting; paterns with sig > 0'))
   print(table(fitting$sig >= 0))
+  result$rows.fitting <- sum(fitting$sig < 0)
       
   ## count the number of patterns for which the variance is greater than the mean
   verbose(paste(theor, 'fitting; paterns with var > avg'))
   print(table(fitting$var > fitting$avg))
+  result$rows.var.gt.avg <- sum(fitting$var > fitting$avg)
   
-  ## plot a graph with variance as a function of avg
-  plot(fitting$avg,fitting$var,
-       main=file.prefix,
-#       sub='variance versus average',
-       xlab='average',
-       ylab='variance',
-       log="xy",
-       panel.first=c(grid(col="blue"),
-  		   abline(a=0,b=1,col="red")
-  		   )
-       )
-  setwd(dir.figures); export.plot(file.prefix=paste(file.prefix, 'avg', 'var', sep='_'), export.formats=export.formats.plots, width=8,height=8)
 
+  if (export.plots) {
   
-  ## Select the most significant patterns
-  selected.patterns <- row.names(fitting.sorted)[1:16]
-  
-  ## select the less significant patterns
-  # selected.patterns <- rev(row.names(fitting.sorted))[1:16]
-  
-  ## pattern count distribution
-  setwd(dir.input); distrib <- read.table(file.prefix, sep="\t", comment.char=";",row.names = 1)
-  distrib <- distrib[, c(2:ncol(distrib))]
-  values <- 0:(ncol(distrib)-1)
-  names(distrib) <- values
-
-
-  ## Plot the distribution of the selected patterns - one image per word
-  for (pattern in selected.patterns) {
+    ## plot a graph with variance as a function of avg
+    plot(fitting$avg,fitting$var,
+         main=file.prefix,
+                                        #       sub='variance versus average',
+         xlab='average',
+         ylab='variance',
+         log="xy",
+         panel.first=c(grid(col="blue"),
+           abline(a=0,b=1,col="red")
+           )
+         )
+    setwd(dir.figures); export.plot(file.prefix=paste(file.prefix, 'avg', 'var', sep='_'), export.formats=export.formats.plots, width=8,height=8)
+    
+    
+    ## Select the 16 most significant patterns
+    selected.patterns <- row.names(fitting.sorted)[1:16]
+    
+    ## select the less significant patterns
+                                        # selected.patterns <- rev(row.names(fitting.sorted))[1:16]
+    
+    ## pattern count distribution
+    setwd(dir.input); distrib <- read.table(file.prefix, sep="\t", comment.char=";",row.names = 1)
+    distrib <- distrib[, c(2:ncol(distrib))]
+    values <- 0:(ncol(distrib)-1)
+    names(distrib) <- values
+    
+    
+    ## Plot the distribution of the selected patterns - one image per word
+    for (pattern in selected.patterns) {
       test.fitting.poisson.histo(values,distrib[pattern,],subtitle=paste(toupper(pattern), fitting[pattern, "sig"]))
       lines(values,fitting[pattern,"repet"]*dnegbin(values, m=fitting[pattern,"avg"], v=fitting[pattern,"var"]), col="#008800",lwd=2)
       setwd(dir.figures); export.plot(file.prefix=paste(file.prefix, theor, 'fitting', toupper(pattern), sep='_'), export.formats=export.formats.plots, width=8,height=8)
-  }
-
-  ## Plot the distribution of the selected patterns - one big image with 16 frames
-  par(mfrow=c(4,4))
-  for (pattern in selected.patterns) {
+    }
+    
+    ## Plot the distribution of the selected patterns - one big image with 16 frames
+    par(mfrow=c(4,4))
+    for (pattern in selected.patterns) {
       test.fitting.poisson.histo(values,distrib[pattern,],subtitle=paste(toupper(pattern), fitting[pattern, "sig"]))
       lines(values,fitting[pattern,"repet"]*dnegbin(values, m=fitting[pattern,"avg"], v=fitting[pattern,"var"]), col="#008800",lwd=2)
-  }
-  par(mfrow=c(1,1))
-  setwd(dir.figures); export.plot(file.prefix=paste(file.prefix, 'fitting', 'top16', sep='_'), export.formats=export.formats.plots, width=12,height=9)
+    }
+    par(mfrow=c(1,1))
+    setwd(dir.figures); export.plot(file.prefix=paste(file.prefix, 'fitting', 'top16', sep='_'), export.formats=export.formats.plots, width=12,height=9)
+   }
+  
+  return(result)
 }
--- R-scripts/upstream_calibrations.R
+++ R-scripts/upstream_calibrations.R
@@ -1,36 +1,50 @@
 ################################################################
 ## Analyze the results of upstream calibration
 
-dir.rsat <- file.path('~', "rsa-tools")
+
+dir.rsat <- Sys.getenv("RSAT")
+if (dir.rsat == "") {dir.rsat <- file.path('~', "rsa-tools")}
 source(file.path(dir.rsat, 'R-scripts', 'negbin.R'))
 source(file.path(dir.rsat, 'R-scripts', 'distrib_fitting.R'))
 
-org <- 'Saccharomyces_cerevisiae_no_mito'
-seq.len <- 1000
+## Default parameters
+org <- 'Saccharomyces_cerevisiae'
+seq.len <- 800
 oligo.len <- 6
 str <- '-1str'
 ov <- '-noov'
 
+result.table <- matrix(ncol=7,nrow=0)
+export.plots <- F
+
 dir.upstream.calibrations <- file.path('~', 'research', 'upstream_calibrations')
 
-upstream.calibration <- function (org='Homo_sapiens', 
-				  seq.len = 1000,
+upstream.calibration <- function (org='Saccharomyces_cerevisiae', 
+				  seq.len = 800,
 				  oligo.len = 6,
 				  str='-1str',
-				  ov='-noov') {
+				  ov='-noov',
+                                  result.table,
+                                  ...) {
   dir.org <- file.path(dir.upstream.calibrations, 'results', org)
   dir.distrib <- file.path(dir.org, 'oligo_distrib')
   setwd(dir.distrib) 
   
   theor <- 'negbin'
-  for (theor in c('negbin', 'poisson')) {
+  for (theor in c('poisson','negbin')) {
     file.prefix <- paste(org, '_allup', seq.len, '_', oligo.len, 'nt', str, ov, sep="")
-    analyze.fitting(file.prefix=file.prefix, theor=theor, dir.input=dir.distrib, dir.output=dir.distrib)
+    result <- analyze.fitting(file.prefix=file.prefix, theor=theor, dir.input=dir.distrib, dir.output=dir.distrib,...)
+    result.table <- rbind(result.table,t(as.matrix(result)))
   }
+  return (result.table)
 }
 
 
-upstream.calibration(org='Saccharomyces_cerevisiae_no_mito', seq.len=800)
-upstream.calibration(org='Homo_sapiens', seq.len=1000)
-upstream.calibration(org='Arabidopsis_thaliana', seq.len=1000)
-upstream.calibration(org='Escherichia_coli_K12', seq.len=200)
+
+result.table <- upstream.calibration(org='Ashbya_gossypii', seq.len=800,export.plots=export.plots,result.table=result.table)
+result.table <- upstream.calibration(org='Saccharomyces_cerevisiae_no_mito', seq.len=800,export.plots=export.plots,result.table=result.table)
+result.table <- upstream.calibration(org='Homo_sapiens', seq.len=1000,export.plots=export.plots,result.table=result.table)
+result.table <- upstream.calibration(org='Arabidopsis_thaliana', seq.len=1000,export.plots=export.plots,result.table=result.table)
+result.table <- upstream.calibration(org='Escherichia_coli_K12', seq.len=200,export.plots=export.plots,result.table=result.table)
+
+setwd(dir.results); export.object(result.table,file.prefix='fitting_tests_summary')
