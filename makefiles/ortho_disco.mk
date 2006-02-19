61c8ed3803d64abfef18abd229ccc1778d78a38a
rsat <rsat>
2006-02-19 11:53:35 +0000

    *** empty log message ***

--- makefiles/ortho_disco.mk
+++ makefiles/ortho_disco.mk
@@ -1,24 +1,53 @@
-################################################################
-## Playing with Pseudomonas promoters
+## ##############################################################
+## Discover conserved motifs in upstream sequences of orthologs for each gene
+## of a genome, and infer a graph of co-regulation by comparing discovered
+## motifs between each pair of genes
+
+## THE FOLLOWING VARIABLES HAVE TO BE DEFINED
+## GENE
+## REF_ORG
+## TAXON
+## MAIN_DIR
+##
+
+## Default parameters
+GENE=lexA
+REF_ORG=Escherichia_coli_K12
+TAXON=Gammaproteobacteria
+MAIN_DIR=.
 
 include ${RSAT}/makefiles/util.mk
-MAKEFILE=makefiles/ortho_disco.mk
+MAKEFILE=${RSAT}/makefiles/ortho_disco.mk
+MAKE=make -s -f ${MAKEFILE} REF_ORG=${REF_ORG} TAXON=${TAXON} GENE=${GENE} MAIN_DIR=${MAIN_DIR}
 
 ## Verbosity level
 V=1
+
+## List the parameters
+list_parameters:
+	@echo ""
+	@echo "REF_ORG      ${REF_ORG}"
+	@echo "TAXON        ${TAXON}"
+	@echo "MAIN_DIR     ${MAIN_DIR}"
+	@echo "RESULT_DIR   ${RESULT_DIR}"
 
 ## List of all genes to be analyzed. 
 ## By default, all the genes of the considered organism (REF_ORG)
 ALL_GENES=` grep -v '^--' ${RSAT}/data/genomes/${REF_ORG}/genome/feature.tab | awk -F '\t' '$$2=="CDS"'| cut -f 3 | sort -u | xargs`
 list_all_genes:
+	@echo ""
+	@echo "All genes"
 	@echo "${ALL_GENES}"
+
 
 ################################################################
 ## Run all the tasks on all the genes
 all_tasks_all_genes:
-	@${MAKE} list_all_genes
+	@echo "All tasks applied on all genes	${REF_ORG}	${TAXON}"
+	@${MAKE} list_all_genes 
+	@${MAKE} list_parameters
 	@for g in ${ALL_GENES} ; do \
-		${MAKE} all_tasks GENE=$${g} ; \
+		${MAKE} all_tasks REF_ORG=${REF_ORG} TAXON=${TAXON} MAIN_DIR=${MAIN_DIR} GENE=$${g} ; \
 	done
 
 ################################################################
@@ -30,12 +59,6 @@ all_tasks:
 ################################################################
 ## Identify orthologs for a given gene (${GENE}) in the taxon of
 ## interest (${TAXON})
-#MAIN_DIR=`pwd`
-MAIN_DIR=${HOME}/research/collaborations/Ildefonso_Cases
-GENE=putA
-#REF_ORG=Escherichia_coli_K12
-REF_ORG=Pseudomonas_putida_KT2440
-TAXON=Pseudomonadales
 RESULT_DIR=${MAIN_DIR}/results/${REF_ORG}/${TAXON}
 GENE_DIR=${RESULT_DIR}/${GENE}
 PREFIX=${GENE_DIR}/${GENE}_${REF_ORG}_${TAXON}
@@ -47,7 +70,7 @@ orthologs:
 	@echo "Getting orthologs	${GENE}		${REF_ORG}	${TAXON}"
 	@@echo "${ORTHO_CMD}"
 	@${ORTHO_CMD}
-
+	@echo "${ORTHOLOGS}"
 
 ################################################################
 ## Retrieve upstream sequences of set of orthologous genes
@@ -130,33 +153,6 @@ index_one_result:
 	@echo "<td><a href=${MAP}>map</a></td>" >> ${INDEX_FILE}
 	@echo "</tr>" >> ${INDEX_FILE}
 
-################################################################
-## Discover patterns in upstream regions of all genes from RegulonDB
-REGULONDB_TABLE=data/gene_factor_Escherichia_coli_K12.tab 
-REGULONDB_GENES=data/regulondb_genes.tab
-regulondb_genes:
-	@cut -f 1 ${REGULONDB_TABLE} | sort -u > ${REGULONDB_GENES}
-	@echo "${REGULONDB_GENES}"
-
-REGULONDB_FACTORS=data/regulondb_factors.tab
-regulondb_factors:
-	@cut -f 2 ${REGULONDB_TABLE} | sort -u  > ${REGULONDB_FACTORS}
-	@echo "${REGULONDB_FACTORS}"
-
-REGULONDB_GF=data/regulondb_genes_and_factors.tab
-regulondb_gf: regulondb_genes regulondb_factors
-	cat ${REGULONDB_GENES} ${REGULONDB_FACTORS} | tr 'a-z' 'A-Z'  | sort -u > ${REGULONDB_GF}
-	@echo "${REGULONDB_GF}"
-
-REGULONDB=`cat ${REGULONDB_GF}| grep -v '^;' | xargs`
-regulondb_analysis:
-	@echo "Analyzing genes from RegulonDB"
-	@${MAKE} all_tasks_all_genes REF_ORG=Escherichia_coli_K12 TAXON=Gammaproteobacteria ALL_GENES='${REGULONDB}'
-	@${MAKE} index_regulondb
-
-index_regulondb:
-	@${MAKE} index_results REF_ORG=Escherichia_coli_K12 TAXON=Gammaproteobacteria ALL_GENES='${REGULONDB}'
-
 
 ################################################################
 ## Compare dyads discovered in the different genes
@@ -166,7 +162,7 @@ DYAD_FILE_LIST=${RESULT_DIR}/dyad_files.txt
 dyad_file_list:
 	(cd ${RESULT_DIR}; find . -name '*_${REF_ORG}_${TAXON}_dyads.tab'  > ${DYAD_FILE_LIST})
 	@echo ${DYAD_FILE_LIST}
-	@echo `wc ${DYAD_FILE_LIST}`
+	@echo "Number of dyad files	`wc -l ${DYAD_FILE_LIST}`"
 
 compare_dyads: dyad_file_list
 	@mkdir -p ${COMPA_DIR}
@@ -179,7 +175,7 @@ compare_dyads: dyad_file_list
 		| perl -pe 's/\/\S+//' \
 		> ${COMPA_TABLE})
 	@echo ${COMPA_TABLE}
-	@echo `wc ${COMPA_TABLE}`
+	@echo "`grep -v '^;' ${COMPA_TABLE} | grep -v '^#' | wc -l`	profiles"
 
 ################################################################
 ## Compare profiles of dyad significance between each pair of genes
@@ -189,7 +185,7 @@ gene_pairs:
 	compare-profiles -v ${V} -i ${COMPA_TABLE} -base 2 -distinct -return dotprod -lth AB 1 \
 		-o ${GENE_PAIRS}.tab
 	@echo ${GENE_PAIRS}.tab
-	@echo `wc ${GENE_PAIRS}.tab`
+	@echo "`grep -v ';' ${GENE_PAIRS}.tab | grep -v '^#' |  wc -l`	gene pairs"
 
 MIN_DP=1
 PAIR_GRAPH=${GENE_PAIRS}_dp${MIN_DP}
@@ -217,11 +213,42 @@ comparisons:
 
 
 ################################################################
-## Synchronize results from SCMBB to my machine
-from_scmbb:
-	(cd ..; rsync -ruptvlz -e ssh --exclude jobs --exclude *.fasta ${OPT} \
-	jvanheld@merlin.scmbb.ulb.ac.be:research/collaborations/Ildefonso_Cases .)
+## Validation of the method : 
+## - Discover patterns in upstream regions of all genes from RegulonDB
+## - create a graph with genes as nodes and arcs representing co-dicovered
+##   patterns
+## - compare the discovered graph with the co-factor graph off regulonDB
+##   (to be done)
 
-to_scmbb:
-	(cd ..; rsync -ruptvlz -e ssh ${OPT} Ildefonso_Cases jvanheld@merlin.scmbb.ulb.ac.be:research/collaborations/)
+## Select target genes from RegulonDB
+REGULONDB_TABLE=data/gene_factor_Escherichia_coli_K12.tab 
+REGULONDB_GENES=data/regulondb_genes.tab
+regulondb_genes:
+	@cut -f 1 ${REGULONDB_TABLE} | sort -u > ${REGULONDB_GENES}
+	@echo "${REGULONDB_GENES}"
+
+## Select transcription factors from RegulonDB
+REGULONDB_FACTORS=data/regulondb_factors.tab
+regulondb_factors:
+	@cut -f 2 ${REGULONDB_TABLE} | sort -u  > ${REGULONDB_FACTORS}
+	@echo "${REGULONDB_FACTORS}"
+
+## Select genes and transcription factors from RegulonDB, both will be used
+## for pattern discovery
+REGULONDB_GF=data/regulondb_genes_and_factors.tab
+regulondb_gf: regulondb_genes regulondb_factors
+	cat ${REGULONDB_GENES} ${REGULONDB_FACTORS} | tr 'a-z' 'A-Z'  | sort -u > ${REGULONDB_GF}
+	@echo "${REGULONDB_GF}"
+
+## Run all the tasks for reuglonDB analysis
+REGULONDB=`cat ${REGULONDB_GF}| grep -v '^;' | xargs`
+regulondb_analysis:
+	@echo "Analyzing genes from RegulonDB"
+	@${MAKE} all_tasks_all_genes REF_ORG=Escherichia_coli_K12 TAXON=Gammaproteobacteria ALL_GENES='${REGULONDB}'
+	@${MAKE} index_regulondb
+
+## Index the results obtained with RegulonDB
+index_regulondb:
+	@${MAKE} index_results REF_ORG=Escherichia_coli_K12 TAXON=Gammaproteobacteria ALL_GENES='${REGULONDB}'
+
 
