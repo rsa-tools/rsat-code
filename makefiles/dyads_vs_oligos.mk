d4fae7344538d81ff0a2d74462c907c44338c419
jvanheld <jvanheld>
2006-04-08 10:31:49 +0000

    *** empty log message ***

--- makefiles/dyads_vs_oligos.mk
+++ makefiles/dyads_vs_oligos.mk
@@ -11,38 +11,49 @@ include ${RSAT}/makefiles/util.mk
 
 all: oligos dyads all_compa
 
-
-
 V=1 
 #SEQ_FILE=PURA_Escherichia_coli_K12_Gammaproteobacteria_up_purged.fasta
 SEQ_FILE=ACEB.fasta
 
+################################################################
 ## Common parmeters for oligo-analysis and dyad-analysis
 NOOV=-noov
 STR=-1str
-OLIGOS=oligos${STR}${NOOV}.tab
+RETURN=-return occ,freq,proba,rank,zscore,ratio
 ORG=Escherichia_coli_K12
 BG=-bg upstream-noorf -org ${ORG}
-RETURN=-return occ,freq,proba,rank,zscore,ratio
 OPTIONS=-v ${V} -i ${SEQ_FILE} ${STR} ${NOOV} ${BG} ${RETURN} -zeroocc
+SUFFIX=${STR}${NOOV}
+
+################################################################
+## Run oligo-analysis
+OLIGOS=oligos${SUFFIX}.tab
 oligos:
 	oligo-analysis -l 6 ${OPTIONS} -o ${OLIGOS}
 	@echo ${OLIGOS}
 
-DYADS=dyads${STR}${NOOV}.tab
-SP=0-0
+################################################################
+## Run dyad-analysis
+DYADS=dyads_sp${SP}_${SUFFIX}.tab
+DYADS_NOSPACING=dyads_sp${SP}_${SUFFIX}_nospacing.tab
+SP=0-4
 dyads:
-	dyad-analysis -l 3 -sp ${SP} ${OPTIONS} \
-		| perl -pe 's/n\{0\}//g' \
-		> ${DYADS}
+	dyad-analysis -l 3 -sp ${SP} ${OPTIONS} -o ${DYADS} 
 	@echo ${DYADS}
+	grep 'n{0}' ${DYADS} \
+		| perl -pe 's/n\{0\}//g' \
+		> ${DYADS_NOSPACING}
+	@echo ${DYADS_NOSPACING}
 
-COMPA=compa${STR}${NOOV}_sc${SC}
+################################################################
+## Compare one column of the oligo-analysis and dyad-analysis result
+## files
+COMPA=compa${SP}_${SUFFIX}_sc${SC}
 SC=3
 LOG=
-DIFF=diff${STR}${NOOV}_sc${SC}
+DIFF=diff_${COMPA}
 one_compa:
-	compare-scores -i ${OLIGOS} -i ${DYADS} -sc ${SC} -o ${COMPA}.tab
+	compare-scores -i ${OLIGOS} -i ${DYADS_NOSPACING} -sc ${SC} -o ${COMPA}.tab
 	awk -F'\t' '$$2 != $$3 {print $$0"\t"($$3-$$2)}' ${COMPA}.tab > ${DIFF}
 	@echo ${COMPA}.tab
 	XYgraph -i ${COMPA}.tab \
@@ -55,6 +66,8 @@ one_compa:
 		-o ${COMPA}.jpg
 	@echo ${COMPA}.jpg
 
+################################################################
+## Compare all the comparable columns between oligo-analysis and dyad-analysis
 all_compa:
 	${MAKE} one_compa SC=3
 	${MAKE} one_compa SC=4
