68664eb73758b91c650866bdbfc4c9c59aeb69f1
jvanheld <jvanheld>
2006-04-13 05:24:58 +0000

    fixed a bug and separated the return_fields from calc_fields; cleaned the dependencies between fields to calculate

--- makefiles/check_disco_sig_distrib.mk
+++ makefiles/check_disco_sig_distrib.mk
@@ -87,5 +87,3 @@ score_distrib: list_disco_files
 
 	@echo ${SCORE_FILE}.jpg
 
-problem:
-	dyad-analysis  -i ${SEQ_FILE} -v 1 -sort -timeout 3600 -type any -2str -noov -org Saccharomyces_cerevisiae -lth occ 1 -lth occ_sig 0 -return occ -l 3 -spacing 0-0 -v 2
