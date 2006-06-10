695c81743571d5a0d5b0a70461ffaee5f9eaf243
jvanheld <jvanheld>
2006-06-10 22:20:17 +0000

    update

--- makefiles/genome_statistics.mk
+++ makefiles/genome_statistics.mk
@@ -1,7 +1,8 @@
 ################################################################
-## Calculate statistics on genome sizes
+## Calculate statistics on genome sizes, genics/intergenic proportions, protein sizes, ...
 
 include ${RSAT}/makefiles/util.mk
+MAKEFILE=${RSAT}/makefiles/genome_statistics.mk
 
 ORGANISMS=`supported-organisms -format full | cut -f 1 | grep -v ";"`
 
@@ -27,16 +28,8 @@ list_organisms:
 ## genic versus intergenic fraction for each genome
 coding_or_not: coding_or_not_all_orgs coding_or_not_cat
 
-coding_or_not_cat:
-	grep "^;" ${STAT_FILE_ONE_ORG} > ${STAT_FILE}.tab
-	cat ${STAT_DIR}/*_stats.tab | grep -v "^;"  >> ${STAT_FILE}.tab
-	text-to-html -i ${STAT_FILE}.tab -o ${STAT_FILE}.html -font variable -chunk 1000
-	@echo ${STAT_FILE}.tab
 
-STAT_FILE=${STAT_DIR}/stats_all_organisms
-coding_or_not_all_orgs:
-	${MAKE} iterate_organisms ORG_TASK=coding_or_not_one_org
-
+## Calculate genome statistics for one organism
 STAT_DIR=data/genome_stats
 STAT_FILE_ONE_ORG=${STAT_DIR}/${ORG}_stats.tab
 coding_or_not_one_org:
@@ -44,8 +37,36 @@ coding_or_not_one_org:
 	@(cd ${STAT_DIR}; coding-or-not -org ${ORG} -return stats)
 	@echo ${STAT_FILE_ONE_ORG}
 
+## Calculate genome statistics for all organisms
+STAT_FILE=${STAT_DIR}/stats_all_organisms
+coding_or_not_all_orgs:
+	${MAKE} iterate_organisms ORG_TASK=coding_or_not_one_org
+
+## Merge all genome statistics in a single file
+coding_or_not_cat:
+	grep "^;" ${STAT_FILE_ONE_ORG} > ${STAT_FILE}.tab
+	cat ${STAT_DIR}/*_stats.tab | grep -v "^;"  >> ${STAT_FILE}.tab
+	text-to-html -i ${STAT_FILE}.tab -o ${STAT_FILE}.html -font variable -chunk 1000
+	@echo ${STAT_FILE}.tab
+
 ################################################################
 ## Protein sizes
+
+## Calculate protein size statistics for one organism
+PROT_DIR=${RSAT}/data/genomes/${ORG}/genome
+PROT_SEQ=${PROT_DIR}/${ORG}_aa.fasta
+aa_lengths_one_org:
+	@mkdir -p ${PROT_STAT_DIR}
+	cat ${PROT_SEQ} | sequence-lengths -format fasta | cut -f 2 > ${LEN_FILE}.txt
+	classfreq -ci 100 -v  -i ${LEN_FILE}.txt -o ${LEN_FILE}_distrib.tab
+	stats -i ${LEN_FILE}.txt -o ${LEN_FILE}_stats.tab -table -add organism_name ${ORG}
+	@echo ${LEN_FILE}_stats.tab
+
+## Calculate protein size statistics for all organisms
+aa_lengths_all_orgs:
+	${MAKE} iterate_organisms ORG_TASK=aa_lengths_one_org
+
+## Merge all protein sizes in a single file
 CDS_FILE=${GENOME_DIR}/cds.tab
 PROT_STAT_DIR=data/protein_stats
 LEN_FILE=${PROT_STAT_DIR}/${ORG}_protein_lengths
@@ -56,28 +77,12 @@ aa_lengths_cat:
 	text-to-html -i ${PROT_STAT_FILE}.tab -o ${PROT_STAT_FILE}.html -font variable -chunk 1000
 	@echo ${PROT_STAT_FILE}.tab
 
-aa_lengths_all_orgs:
-	${MAKE} iterate_organisms ORG_TASK=aa_lengths_one_org
-
-
-NCBI_DIR=${HOME}/downloads/ftp.ncbi.nih.gov/genomes
-EU_PROT_DIR=${NCBI_DIR}/${ORG}
-BACT_PROT_DIR=${NCBI_DIR}/Bacteria/${ORG}
-aa_lengths_one_org:
-	@if [ -d "${BACT_PROT_DIR}" ] ; then	\
-		echo "${ORG}	prokaryote ${BACT_PROT_DIR}" ; \
-		${MAKE} aa_lengths_one_org_2 PROT_DIR=${BACT_PROT_DIR} ; \
-	fi
-	@if [ -d "${EU_PROT_DIR}" ] ; then						 \
-		echo "${ORG}	eukaryote ${EU_PROT_DIR}"; \
-		${MAKE} aa_lengths_one_org_2 PROT_DIR='${EU_PROT_DIR}' ; \
-	fi
-
-PROT_DIR=${NCBI_DIR}/${ORG}
-aa_lengths_one_org_2:
-	@mkdir -p ${PROT_STAT_DIR}
-#	grep -v '^--' ${CDS_FILE} | awk -F\t '{print $$15}' | sequence-lengths -format multi | cut -f 2 > ${LEN_FILE}.txt
-	cat ${PROT_DIR}/*.faa | sequence-lengths -format fasta | cut -f 2 > ${LEN_FILE}.txt
-	classfreq -ci 100 -v  -i ${LEN_FILE}.txt -o ${LEN_FILE}_distrib.tab
-	stats -i ${LEN_FILE}.txt -o ${LEN_FILE}_stats.tab -table -add organism_name ${ORG}
-	@echo ${LEN_FILE}_stats.tab
+################################################################
+## Perform all tasks
+NB_ORGS=`${MAKE} list_organisms | wc -w`
+all:
+	@echo "Calculating genome and protein statistics for ${NB_ORGS} organisms"
+	@${MAKE} aa_lengths_all_orgs
+	@${MAKE} aa_lengths_cat
+	@${MAKE} coding_or_not_all_orgs
+	@${MAKE} coding_or_not_cat
