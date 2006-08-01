d40c731cd569e5a742d1a0bf26f47c59f39b4900
jvanheld <jvanheld>
2006-08-01 04:35:44 +0000

    added calculation of row margins and column margins + col freq + row freq

--- public_html/lib/RSAT/table.pm
+++ public_html/lib/RSAT/table.pm
@@ -6,6 +6,7 @@ package RSAT::table;
 
 use RSAT::GenericObject;
 use RSAT::util;
+use RSAT::stats;
 @ISA = qw( RSAT::GenericObject );
 
 =pod
@@ -61,18 +62,132 @@ sub transpose {
 }
 
 
+################################################################
+=pod
+
+=item B<col_freq>
+
+Return the frequency per column. Computation is cached (computed only
+once for the life of the object).
+
+=cut
+sub col_freq {
+  my ($self) = @_;
+  unless ($self->get_attribute("col_freq_calculated")) {
+    $self->calc_col_freq();
+  }
+  return $self->get_attribute("col_freq");
+}
 
 ################################################################
 =pod
 
-=item B<calc_col_sums>
+=item B<calc_col_freq>
 
-Calculate the sum of each column
+Calculate the frequency per column, i.e. the cell counts divided by
+sum per column.
 
 =cut
-sub calc_col_sums {
+sub calc_col_freq {
+  my ($self) = @_;
+  my @col_sum = $self->col_sum();;
+  my @col_freq = ();
+  my $ncol = $self->ncol();
+  my $nrow = $self->nrow();
+  my @table = $self->getTable();
+  warn join("\t", "; Calculating frequencies per column","rows:".$nrow, "columns:".$ncol),"\n"
+    if ($main::verbose > 2);
+  foreach my $c (0..($ncol-1)) {
+    for my $r (0..($nrow-1)) {
+      if ($col_sum[$c] > 0) {
+	$col_freq[$c][$r] = $table[$c][$r]/$col_sum[$c];
+      } else {
+	$col_freq[$c][$r] = 0;
+      }
+    }
+  }
+  @{$self->{col_freq}} = @col_freq;
+  $self->force_attribute("col_freq_calculated", 1);
+}
+
+
+################################################################
+=pod
+
+=item B<row_freq>
+
+Return the frequency per row. Computation is cached (computed only
+once for the life of the object).
+
+=cut
+sub row_freq {
+  my ($self) = @_;
+  unless ($self->get_attribute("row_freq_calculated")) {
+    $self->calc_row_freq();
+  }
+  return $self->get_attribute("row_freq");
+}
+
+################################################################
+=pod
+
+=item B<calc_row_freq>
+
+Calculate the frequency per row, i.e. the cell counts divided by
+sum per row.
+
+=cut
+sub calc_row_freq {
+  my ($self) = @_;
+  my @row_sum = $self->row_sum();;
+  my @row_freq = ();
+  my $ncol = $self->ncol();
+  my $nrow = $self->nrow();
+  my @table = $self->getTable();
+  warn join("\t", "; Calculating frequencies per row","rows:".$nrow, "columns:".$ncol),"\n"
+    if ($main::verbose > 2);
+  foreach my $r (0..($nrow-1)) {
+    for my $c (0..($ncol-1)) {
+      if ($row_sum[$r] > 0) {
+	$row_freq[$c][$r] = $table[$c][$r]/$row_sum[$r];
+      } else {
+	$row_freq[$c][$r] = 0;
+      }
+    }
+  }
+  @{$self->{row_freq}} = @row_freq;
+  $self->force_attribute("row_freq_calculated", 1);
+}
+
+
+################################################################
+=pod
+
+=item B<col_sum>
+
+Return the sum of each column. Computation is cached (computed only
+once for the life of the object).
+
+=cut
+sub col_sum {
+  my ($self) = @_;
+  unless ($self->get_attribute("col_sum_calculated")) {
+    $self->calc_col_sum();
+  }
+  return $self->get_attribute("col_sum");
+}
+
+################################################################
+=pod
+
+=item B<calc_col_sum>
+
+Calculate the sum of each column.
+
+=cut
+sub calc_col_sum {
     my ($self) = @_;
-    my @col_sums = ();
+    my @col_sum = ();
     my $ncol = $self->ncol();
     my $nrow = $self->nrow();
     my @table = $self->getTable();
@@ -83,39 +198,239 @@ sub calc_col_sums {
 	for my $r (0..($nrow-1)) {
 	    $col_sum += $table[$c][$r];
 	}
-	push @col_sums, $col_sum;
+	push @col_sum, $col_sum;
 #	warn join ("\t", ";", "column", $c, "sum", $col_sum), "\n"  if ($main::verbose >= 5);
     }
-    @{$self->{col_sums}} = @col_sums;
+    @{$self->{col_sum}} = @col_sum;
+    $self->force_attribute("col_sum_calculated", 1);
+}
+
+################################################################
+=pod
+
+=item B<col_max>
+
+Return the max of each column. Computation is cached (computed only
+once for the life of the object).
+
+=cut
+sub col_max {
+  my ($self) = @_;
+  unless ($self->get_attribute("col_max_calculated")) {
+    $self->calc_col_max();
+  }
+  return $self->get_attribute("col_max");
+}
+
+################################################################
+=pod
+
+=item B<calc_col_max>
+
+Calculate the max of each column.
+
+=cut
+sub calc_col_max {
+    my ($self) = @_;
+    my @col_max = ();
+    my $ncol = $self->ncol();
+    my $nrow = $self->nrow();
+    my @table = $self->getTable();
+    warn join("\t", "; Calculating max per column","rows:".$nrow, "columns:".$ncol),"\n"
+	if ($main::verbose > 2);
+    foreach my $c (0..($ncol-1)) {
+	my $col_max;
+	for my $r (0..($nrow-1)) {
+	    $col_max = &RSAT::stats::checked_max($col_max, $table[$c][$r]);
+	}
+	push @col_max, $col_max;
+#	warn join ("\t", ";", "column", $c, "max", $col_max), "\n"  if ($main::verbose >= 5);
+    }
+    @{$self->{col_max}} = @col_max;
+    $self->force_attribute("col_max_calculated", 1);
+}
+
+################################################################
+=pod
+
+=item B<col_min>
+
+Return the min of each column. Computation is cached (computed only
+once for the life of the object).
+
+=cut
+sub col_min {
+  my ($self) = @_;
+  unless ($self->get_attribute("col_min_calculated")) {
+    $self->calc_col_min();
+  }
+  return $self->get_attribute("col_min");
+}
+
+################################################################
+=pod
+
+=item B<calc_col_min>
+
+Calculate the min of each column.
+
+=cut
+sub calc_col_min {
+    my ($self) = @_;
+    my @col_min = ();
+    my $ncol = $self->ncol();
+    my $nrow = $self->nrow();
+    my @table = $self->getTable();
+    warn join("\t", "; Calculating min per column","rows:".$nrow, "columns:".$ncol),"\n"
+	if ($main::verbose > 2);
+    foreach my $c (0..($ncol-1)) {
+	my $col_min;
+	for my $r (0..($nrow-1)) {
+	    $col_min = &RSAT::stats::checked_min($col_min, $table[$c][$r]);
+	}
+	push @col_min, $col_min;
+#	warn join ("\t", ";", "column", $c, "min", $col_min), "\n"  if ($main::verbose >= 5);
+    }
+    @{$self->{col_min}} = @col_min;
+    $self->force_attribute("col_min_calculated", 1);
 }
 
 
 ################################################################
 =pod
 
-=item B<calc_row_sums>
+=item B<row_sum>
 
-Calculate the sum of each rowumn
+Return the sum of each row. Computation is cached (computed only
+once for the life of the object).
 
 =cut
-sub calc_row_sums {
+sub row_sum {
+  my ($self) = @_;
+  unless ($self->get_attribute("row_sum_calculated")) {
+    $self->calc_row_sum();
+  }
+  return $self->get_attribute("row_sum");
+}
+
+################################################################
+=pod
+
+=item B<calc_row_sum>
+
+Calculate the sum of each row.
+
+=cut
+sub calc_row_sum {
     my ($self) = @_;
-    my @row_sums = ();
+    my @row_sum = ();
     my $ncol = $self->ncol();
     my $nrow = $self->nrow();
     my @table = $self->getTable();
     warn join("\t", "; Calculating sum per row","rows:".$nrow, "columns:".$ncol),"\n"
 	if ($main::verbose > 2);
-    for my $r (0..($nrow-1)) {
+    foreach my $r (0..($nrow-1)) {
 	my $row_sum = 0;
-	foreach my $c (0..($ncol-1)) {
+	for my $c (0..($ncol-1)) {
 	    $row_sum += $table[$c][$r];
 	}
-	push @row_sums, $row_sum;
-#	warn join ("\t", ";", "row", $r, "sum", $row_sum), "\n"  if ($main::verbose >= 5);
+	push @row_sum, $row_sum;
+#	warn join ("\t", ";", "column", $c, "sum", $col_sum), "\n"  if ($main::verbose >= 5);
     }
-    @{$self->{row_sums}} = @row_sums;
+    @{$self->{row_sum}} = @row_sum;
+    $self->force_attribute("row_sum_calculated", 1);
 }
+
+################################################################
+=pod
+
+=item B<row_max>
+
+Return the max of each row. Computation is cached (computed only
+once for the life of the object).
+
+=cut
+sub row_max {
+  my ($self) = @_;
+  unless ($self->get_attribute("row_max_calculated")) {
+    $self->calc_row_max();
+  }
+  return $self->get_attribute("row_max");
+}
+
+################################################################
+=pod
+
+=item B<calc_row_max>
+
+Calculate the max of each row.
+
+=cut
+sub calc_row_max {
+    my ($self) = @_;
+    my @row_max = ();
+    my $ncol = $self->ncol();
+    my $nrow = $self->nrow();
+    my @table = $self->getTable();
+    warn join("\t", "; Calculating max per row","rows:".$nrow, "columns:".$ncol),"\n"
+	if ($main::verbose > 2);
+    foreach my $r (0..($nrow-1)) {
+	my $row_max;
+	for my $c (0..($ncol-1)) {
+	    $row_max = &RSAT::stats::checked_max($row_max, $table[$c][$r]);
+	}
+	push @row_max, $row_max;
+#	warn join ("\t", ";", "column", $c, "max", $col_max), "\n"  if ($main::verbose >= 5);
+    }
+    @{$self->{row_max}} = @row_max;
+    $self->force_attribute("row_max_calculated", 1);
+}
+
+################################################################
+=pod
+
+=item B<row_min>
+
+Return the min of each row. Computation is cached (computed only
+once for the life of the object).
+
+=cut
+sub row_min {
+  my ($self) = @_;
+  unless ($self->get_attribute("row_min_calculated")) {
+    $self->calc_row_min();
+  }
+  return $self->get_attribute("row_min");
+}
+
+################################################################
+=pod
+
+=item B<calc_row_min>
+
+Calculate the min of each row.
+
+=cut
+sub calc_row_min {
+    my ($self) = @_;
+    my @row_min = ();
+    my $ncol = $self->ncol();
+    my $nrow = $self->nrow();
+    my @table = $self->getTable();
+    warn join("\t", "; Calculating min per row","rows:".$nrow, "columns:".$ncol),"\n"
+	if ($main::verbose > 2);
+    foreach my $r (0..($nrow-1)) {
+	my $row_min;
+	for my $c (0..($ncol-1)) {
+	    $row_min = &RSAT::stats::checked_min($row_min, $table[$c][$r]);
+	}
+	push @row_min, $row_min;
+#	warn join ("\t", ";", "column", $c, "min", $col_min), "\n"  if ($main::verbose >= 5);
+    }
+    @{$self->{row_min}} = @row_min;
+    $self->force_attribute("row_min_calculated", 1);
+}
+
 
 
 ################################################################
@@ -396,17 +711,9 @@ sub _readFromTabFile {
     my ($self, $file, %args) = @_;
     warn ("; Reading table from tab file\t",$file, "\n") if ($main::verbose >= 2);
     
-#	($in, $dir) = &main::OpenInputFile($file);
-#
     
     ## open input stream
     my ($in, $dir) = &RSAT::util::OpenInputFile($file);
-#     my $in = STDIN;
-#     if ($file) {
-#	 open $in, $file;
-# 	open INPUT, $file;
-# 	$in = INPUT;
-#     }
     my $current_table_nb = 0;
     my $l=0;
     ## read header
@@ -414,8 +721,9 @@ sub _readFromTabFile {
 	$header = <$in>;
 	chomp ($header);
 	@header = split "\t", $header;
+	shift @header;
 	$self->push_attribute("header", @header);
-	$l++
+	$l++;
     }
     while (<$in>) {
 	$l++;
@@ -439,7 +747,7 @@ sub _readFromTabFile {
 	}
     }
     close $in if ($file);
-#    die;
+
 }
 
 
@@ -551,6 +859,24 @@ sub get_column {
 
 Return a string representing the table. 
 
+Arguments:
+
+=over 4
+
+=item I<title>
+
+Text printed above the table.
+
+=item I<corner>
+
+Text printed in the left top corner of the tabe.
+
+=item I<decimals>
+
+Number of decimals.
+
+=back
+
 =cut
 sub toString {
     my ($self, %args) = @_;
@@ -584,25 +910,40 @@ sub toString {
     ## Print a table
     my @table = @{$self->{table}};
     my @alphabet = $self->getAlphabet();
+
     my $ncol = $self->ncol();
     my $nrow = $self->nrow();
     
     ## Header for the table
+    my @header = $self->get_attribute("header");
+    my $header_given = 1;
+    if (scalar(@header) <= 0) {
+	for my $c (0..($ncol-1)) {
+	    my $pos = $c+1;
+	    push @header, $pos;
+	  }
+    }
+
+    if ($self->get_attribute("margins")) {
+      push @header, "|", "row.sum", "row.max", "row.min";
+    }
+
     if ($main::verbose >= 1) {
 	$to_print .= ";\n";
+	if ($args{"title"}) {
+	  $to_print .= "; $args{title}\n";
+	}
 	if (($col_width) && ($col_width < 6)) {
 	    $to_print .= ";";
 	} else {
-	    $to_print .= "; Table";
+	    $to_print .= "; $args{corner}";
 	}
-#	$to_print .= $sep."|";
-	for my $c (0..($ncol-1)) {
-	    my $pos = $c+1;
+	foreach my $header_item (@header) {
 	    if ($col_width) {
-		$to_print .= sprintf "%${col_width}s", $pos;
+	      $to_print .= sprintf "%${col_width}s", $header_item;
 	    } else {
-		$to_print .= $sep;
-		$to_print .= $pos;
+	      $to_print .= $sep;
+	      $to_print .= $header_item;
 	    }
 	}
 	$to_print .= "\n";
@@ -611,34 +952,44 @@ sub toString {
     }
     
     ## Print the table
+    my @row_sum;
+    my @row_max;
+    my @row_min;
+    if ($self->get_attribute("margins")) {
+      @row_sum = $self->row_sum();
+      @row_max = $self->row_max();
+      @row_min = $self->row_min();
+    }
     for $a (0..$#alphabet) {
 	my @row = $self->get_row($a+1, $ncol, @table);
+	if ($self->get_attribute("margins")) {
+	  push @row, "|", $row_sum[$a], $row_max[$a], $row_min[$a];
+	}
 	$to_print .= $self->_printTableRow($alphabet[$a], @row);
     }
 
-#    die "HELLO";
     ################################################################
     ##Print column statistics
     if ($self->get_attribute("margins")) {
 	$prefix_letter = substr($type, 0, 1);
-	$to_print .= $self->_printSeparator($ncol, $to_print);
+	$to_print .= $self->_printSeparator($ncol+4, $to_print);
 	
 	## Sum per column
 #	my @col_sum = &col_sum($nrow, $ncol, @table);
 	my @col_sum = $self->col_sum($nrow, $ncol, @table);
-	push @col_sum, &main::sum(@col_sum);
+	push @col_sum, "|", &RSAT::stats::sum(@col_sum);
 	$to_print .= $self->_printTableRow("; ".$prefix_letter.".sum", @col_sum);
 	
 	## Maximum per column
 #	my @col_max = &col_max($nrow, $ncol, @table);
 	my @col_max = $self->col_max($nrow, $ncol, @table);
-	push @col_max, &main::max(@col_max);
+	push @col_max, "|", "", &RSAT::stats::checked_max(@col_max);
 	$to_print .= $self->_printTableRow("; ".$prefix_letter.".max", @col_max);
 	
 	## Minimum per column
 #	my @col_min = &col_min($nrow, $ncol, @table);
 	my @col_min = $self->col_min($nrow, $ncol, @table);
-	push @col_min, &main::min(@col_min);
+	push @col_min, "|", "", "", &RSAT::stats::checked_min(@col_min);
 	$to_print .= $self->_printTableRow("; ".$prefix_letter.".min", @col_min);
     }
 
@@ -654,50 +1005,54 @@ Print a row for the table output.
 
 =cut
 
-    sub _printTableRow {
-	my ($self, $row_name, @values) = @_;
-	my $row_string = $row_name;
-	my $ncol = scalar(@values);
+sub _printTableRow {
+  my ($self, $row_name, @values) = @_;
+  my $row_string = $row_name;
+  my $ncol = scalar(@values);
 
-#	my ($decimals, $sep, $col_width, $number_width) = $self->_get_format;
+  #	my ($decimals, $sep, $col_width, $number_width) = $self->_get_format;
 	
-    my $decimals = $self->get_attribute("decimals");
-	unless ($decimals) {
-	    if ($type eq "counts") {
-		$decimals = 0;
-  	} else {
-  	    $decimals = $number_width - 2;
-  	}
-    }
-    my $sep = $self->get_attribute("sep") || "\t";
-    my $col_width = $self->get_attribute("col_width");
+  my $decimals = $self->get_attribute("decimals");
+
+  my $sep = $self->get_attribute("sep") || "\t";
+  my $col_width = $self->get_attribute("col_width");
     
-    ## Format for the table entries
-    my $number_width = 0;
+  ## Format for the table entries
+  my $number_width = 0;
+  if ($col_width) {
+    $number_width = $col_width - 1;
+  }
+  unless ($decimals) {
+    if ($type eq "counts") {
+      $decimals = 0;
+    } else {
+      $decimals = $number_width - 2;
+    }
+  }
+    
+  ## Print the table row
+  #    $row_string .= $sep."|";
+  for $c (0..($ncol-1)) {
+    my $value = $values[$c];
     if ($col_width) {
-  	$number_width = $col_width - 1;
-    }
-    
-    ## Print the table row
-#    $row_string .= $sep."|";
-    for $c (0..($ncol-1)) {
-	my $value = $values[$c];
-	if ($col_width) {
-	    my $value_format = "%${number_width}s";
-	    if (&main::IsReal($value)){
-		if ($type eq "counts") {
-		    $value_format = "%${number_width}d";
-		} else {
-		    $value_format= "%${number_width}.${decimals}f";
-		}
-	    }
-	    $row_string .= sprintf " ${value_format}", $value;
+      my $value_format = "%${number_width}s";
+      if (&main::IsReal($value)) {
+	if ($type eq "counts") {
+	  $value_format = "%${number_width}d";
 	} else {
-	    $row_string .= $sep.$value;
+	  $value_format= "%${number_width}.${decimals}f";
 	}
+      }
+      $row_string .= sprintf " ${value_format}", $value;
+    } elsif (($decimals > 0) && (&main::IsReal($value))) {
+      $row_string .= $sep;
+      $row_string .= sprintf "%.${decimals}g", $value;
+    } else {
+      $row_string .= $sep.$value;
     }
-    $row_string .= "\n";
-    return $row_string
+  }
+  $row_string .= "\n";
+  return $row_string;
 }
 
 
