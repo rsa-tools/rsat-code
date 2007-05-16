33d725128a7df5a0bfaf594681a784fe881a1aa0
rsat <rsat>
2007-05-16 13:01:54 +0000

    AUC geometric corrected

--- perl-scripts/lib/RSAT/auc.pm
+++ perl-scripts/lib/RSAT/auc.pm
@@ -8,10 +8,14 @@
 
 package RSAT::auc;
 
+
+
 use RSAT::GenericObject;
 use RSAT::message;
 use RSAT::error;
 @ISA = qw( RSAT::GenericObject );
+
+
 
 ################################################################
 #### Calculate geometrically the Area Under the Curves (AUC)
@@ -21,10 +25,13 @@ use RSAT::error;
 #### calculating the area of the rectangle between (x1,0),(x2,0),(x1,y1) and
 #### (x2,y1), then the area of the triangle corresponding to (x1,y1),(x2,y1)
 #### and (x2,y2).
+#### 
+#### This method is also called the trapezoïdal method
 ################################################################
 
 sub calc_geometric{
     my ($Sn, $FPR) = @_;
+
 
     my ($base, # distance between 2 points in x axis
 	$x1, $x2, # x points
@@ -35,6 +42,11 @@ sub calc_geometric{
 	);
 
     my $AUC=0 ;	
+#    my $i=0;
+    my $maxFPR=0;    
+    my $minFPR=0;
+    my $maxSn=0;    
+    my $minSn=0;
     for my $i (0..$#{$FPR}-1) { # foreach element of the x array (FPR)
 	
 	$x1=$FPR->[$i]; 
@@ -46,8 +58,16 @@ sub calc_geometric{
 	$areaR= $base * $y2;
 	$areaT= ($base * $height_t) / 2 ;
 	$AUC += ($areaR + $areaT);
-#	&OPTOOLS::util::Warning(1,join("\t", "x1",$x1,"x2",$x2,"AUC",$AUC));
+
+	$maxFPR = $x2 if ($x2 > $maxFPR);
+	$maxSn = $y2 if ($y2 > $maxSn);
+	$minFPR = $x1 if ($x1 < $minFPR);
+	$minSn = $y1 if ($y1 < $minSn);
+
     }
+    my $i =$#{$FPR}-1;
+#    &RSAT::message::Warning(join("\t",$AUC,$maxFPR,$minFPR,$maxSn,$minSn));
+    $AUC = $AUC/(($maxFPR - $minFPR)*($maxSn - $minSn));
     return ($AUC);
 }
 
@@ -84,5 +104,93 @@ sub mean_and_var {
 	
 	return ($mean,$var);
 }
+
+# sub calc_MannWhitney{
+#     my ($Sn, $FPR) = @_;
+
+
+#     my ($base, # distance between 2 points in x axis
+# 	$x1, $x2, # x points
+# 	$y1, $y2, # y points
+# 	$height_t, # height of the triangle
+# 	$areaR, # area of rectangle
+# 	$areaT, # area of triangle
+# 	);
+
+#     my $AUC=0 ;
+
+#     #Before that add the points (TP,FP) =(0,0) and (TP,FP) = (1,1)
+#     unshift(@$FPR,0);  
+#     unshift(@$Sn,0);
+#     push(@$FPR,1);  
+#     push(@$Sn,1);
+    
+#     #Use the ranks to calculate the Mann-Whitney statistic 
+# 	#U = Npos*Nneg + Nneg(Nneg+1)/2 - R, 
+    
+# for my $i (0..$#{$FPR}-1) { # foreach element of the x array (FPR)
+# 	my $U = $Npos*$Nneg + $Nneg($Nneg+1)/2 - $R, 
+# 	$x1=$FPR->[$i]; 
+# 	$x2=$FPR->[$i+1];
+# 	$base= $x2 - $x1;
+# 	$y1=$Sn->[$i];
+# 	$y2=$Sn->[$i+1];
+# 	$height_t=abs($y2-$y1);
+# 	$areaR= $base * $y2;
+# 	$areaT= ($base * $height_t) / 2 ;
+# 	$AUC += ($areaR + $areaT);
+# #	&OPTOOLS::util::Warning(1,join("\t", "x1",$x1,"x2",$x2,"AUC",$AUC));
+#     }
+#     return ($AUC);
+
+# # https://list.scms.waikato.ac.nz/pipermail/wekalist/2004-January/002113.html
+# #
+# # 1. extract TP and FP rates (forming the ROC curve), keep them in threshold
+# # (= P) order
+
+# # 2. Use the nonparametric AUC (Area Under the Curve) approximation with the
+# # Mann-Whitney rank sum test as follows. Before that add the points (TP,FP) =
+# # (0,0) and (TP,FP) = (1,1) of the
+# # ROC (Weka leaves them out of the curve) and link them to the most extreme
+# # threshold values (mostly 0 and 1 if it
+# # scales between 0 and 1).
+
+# # a. The threshold order (0 to 1) determines the rank (assign an average rankorder
+# # over obeservations with the same threshold to correct for ties)
+
+# # b. Use the ranks to calculate the Mann-Whitney statistic 
+# # U = Npos*Nneg + Nneg(Nneg+1)/2 - R, 
+# # with 
+# #         R = the ranksum of the negative sample,
+# #         Npos= number of positives in the sample and
+# #         Nneg= the number of negatives in the sample
+
+# # c. AUC = U/(Npos + Nneg)
+# #    ---
+# # 3. The standard error for AUC can be approximated Using the Hanley MacNeil
+# # test following:
+# #        --------------
+# #         /* For clarity, the same symbols are used as in the Hanley-McNeil
+# # paper. */
+# #         n_A    = Npos;
+# #         n_N    = Nneg;
+# #         theta  = AUC;
+# #         //
+# #         theta2 = theta * theta;
+# #         Q1     = theta / (2 - theta);
+# #         Q2     = 2 * theta2 / (1 + theta);
+# #         se2    = (theta * (1 - theta) + (n_A - 1) * (Q1 - theta2) + (n_N -
+# # 1) * (Q2
+# #         - theta2)) / (n_A * n_N);
+# #         //
+# #         SE_auc = squareRoot (se2);
+# #         Assumption:
+# #         0.0 > AUC < 1.0 , Npos > 0 and Npos > 0
+	
+# # The threshold values for every observation can also be obtained from the
+# # classification function if made external (like for weka logistic regression), 
+# # and the TP and FP fraction from the cummulative frequency for negatives and 
+# # positives when sorted by the classification function values.
+# }
 
 1;
