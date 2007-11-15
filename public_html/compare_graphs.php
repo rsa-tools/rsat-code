145f829266400123e6ee7a4c336ecbabc304b18f
rsat <rsat>
2007-11-15 08:14:28 +0000

    Added the php web interface to compare-graphs

---
+++ public_html/compare_graphs.php
@@ -0,0 +1,165 @@
+<html>
+<head>
+   <title>GrA-tools - convert-graph</title>
+   <link rel="stylesheet" type="text/css" href = "main_grat.css" media="screen">
+</head>
+<body class="results">
+<?php 
+  require ('functions.php');
+  title('compare-graphs - results');
+  # Error status
+  $error = 0;
+  # Get parameters
+  $in_formatQ = $_REQUEST['in_formatQ'];
+  $in_formatR = $_REQUEST['in_formatR'];
+  $out_format = $_REQUEST['out_format'];
+  if ($_FILES['graph_fileQ']['name'] != "") {
+    $graph_fileQ = uploadFile('graph_fileQ');
+    echo "MERDE";
+  } else if ($_REQUEST['pipe_query_graph_file'] != "") {
+    echo "GRAPH FILE $graph_fileQ";
+    $graph_fileQ = $_REQUEST['pipe_query_graph_file'];
+    echo ".Qdfq dfq df qdf";
+  }
+  if ($_FILES['graph_fileR']['name'] != "") {
+    $graph_fileR = uploadFile('graph_fileR');
+  }
+  $now = date("Ymd_His");
+  $graphQ = $_REQUEST['graphQ'];
+  $graphR = $_REQUEST['graphR'];
+  $directed = $_REQUEST['directed'];
+  $s_colQ = $_REQUEST['s_colQ'];
+  $t_colQ = $_REQUEST['t_colQ'];
+  $w_colQ = $_REQUEST['w_colQ'];
+  $s_colR = $_REQUEST['s_colR'];
+  $t_colR = $_REQUEST['t_colR'];
+  $w_colR = $_REQUEST['w_colR'];
+  if ($directed == "on") {
+    $directed = 1;
+  }
+  $self = $_REQUEST['self'];  
+  if ($self == "on") {
+    $self = 1;
+  }
+  $return =  $_REQUEST['return'];
+  $outweight =  $_REQUEST['outweight']; 
+  ## If a query graph file and a query graph are submitted -> error
+  if ($graphQ != "" && $graph_fileQ!= "") {
+    $error = 1;
+    error("You must not submit both a query graph and a query graph file");
+  }
+  ## If a query graph file and a query graph are submitted -> error
+  if ($graphR != "" && $graph_fileR!= "") {
+    $error = 1;
+    error("You must not submit both a reference graph and a reference graph file");
+  }
+  ## No specification of the source and target columns
+  if ($in_format_Q == "tab" && $s_colQ == "" && $t_colQ == "") {
+    warning("Default value for source and target columns for query graph in tab-delimited input format are 1 and 2");
+  }
+  ## No specification of the source and target columns
+  if ($in_format_R == "tab" && $s_colR == "" && $t_colR == "") {
+    warning("Default value for source and target columns for reference graph in tab-delimited input format are 1 and 2");
+  }
+  ## put the content of the file $graph_file in $graph
+  if ($graph_fileQ != "" && $graphQ == "") {
+    $graphQ = storeFile($graph_fileQ);
+  } 
+  ## put the content of the file $graph_file in $graph
+  if ($graph_fileR != "" && $graphR == "") {
+    $graphR = storeFile($graph_fileR);
+  }
+  ## If no graph are submitted -> error
+  if ($graphQ == "" && $graph_fileQ == "") {
+    $error = 1;
+    error("You must submit a query input graph");
+  }
+  ## If no graph are submitted -> error
+  if ($graphR == "" && $graph_fileR == "") {
+    $error = 1;
+    error("You must submit a reference input graph");
+  }
+  
+  
+   if (!$error) { 
+     $graphQ = trim_text($graphQ);
+     $graphR = trim_text($graphR);
+     $parameters = array( 
+       "request" => array (
+         "Qinformat"=>$in_formatQ,
+         "Rinformat"=>$in_formatR,
+         "outformat"=>$out_format,
+         "outweight"=>$outweight,
+         "Rinputgraph"=>$graphR,
+         "Qinputgraph"=>$graphQ,
+         "Qwcol"=>$w_colQ,
+         "Qscol"=>$s_colQ,
+         "Qtcol"=>$t_colQ,
+         "Rwcol"=>$w_colR,
+         "Rscol"=>$s_colR,
+         "Rtcol"=>$t_colR,
+         "directed"=>$directed,
+         "return"=>$return,
+         "self"=>$self
+       )
+     );
+         
+    # Info message
+    info("Results will appear below");
+    echo"<hr>\n";
+  
+    # Open the SOAP client
+    $client = new SoapClient(
+                       'http://rsat.scmbb.ulb.ac.be/rsat/web_services/RSATWS.wsdl',
+                           array(
+                                 'trace' => 1,
+                                 'soap_version' => SOAP_1_1,
+                                 'style' => SOAP_DOCUMENT,
+                                 'encoding' => SOAP_LITERAL
+                                 )
+                           );
+    # Execute the command
+    $echoed = $client->compare_graphs($parameters);
+
+    $response =  $echoed->response;
+    $command = $response->command;
+    $server = $response->server;
+    $client = $response->client;
+    $server = rtrim ($server);
+    $temp_file = explode('/',$server);
+    $temp_file = end($temp_file);
+    $resultURL = $WWW_RSA."/tmp/".$temp_file;
+    # Display the results
+    echo "The results is available at the following URL ";
+    echo "<a href = '$resultURL'>$resultURL</a>"; 
+    echo "<hr>\n";
+    echo "
+  <TABLE CLASS = 'nextstep'>
+    <TR>
+      <Th colspan = 3>
+        Next step
+      </Th>
+    </TR>
+    <TR>
+      <td>
+      </td>
+      <TD>
+        <FORM METHOD='POST' ACTION='display_graph_form.php'>
+          <input type='hidden' NAME='pipe' VALUE='1'>
+          <input type='hidden' NAME='graph_file' VALUE='$server'>
+          <input type='hidden' NAME='graph_format' VALUE='$out_format'>";
+          if ($out_format = 'tab') {
+            echo "
+            <input type='hidden' NAME='scol' VALUE='1'>
+            <input type='hidden' NAME='tcol' VALUE='2'>
+            <input type='hidden' NAME='wcol' VALUE='3'>
+            ";
+          }
+          echo "
+          <INPUT type='submit' value='Display the graph'>
+        </form>
+      </td>
+    </tr>
+  </table>";
+  }
+?>
---
+++ public_html/compare_graphs_form.php
@@ -0,0 +1,147 @@
+<html>
+<head>
+   <title>GrA-tools - compare-graphs</title>
+   <link rel="stylesheet" type="text/css" href = "main_grat.css" media="screen">
+</head>
+<body class="form">
+<?
+  require ('functions.php');
+  require ('demo_dataset.php');
+  # variables definition
+  $default_scolQ = 1;
+  $default_tcolQ = 2;
+  $default_scolR = 1;
+  $default_tcolR = 2;
+  # PIPE VALUES
+  $pipe = $_REQUEST['pipe'];
+  $query_graph_file = $_REQUEST['graph_file'];
+  $query_graph_format = $_REQUEST['graph_format'];
+  $query_scol = $_REQUEST['scol'];
+  $query_tcol = $_REQUEST['tcol'];
+  $query_wcol = $_REQUEST['wcol'];
+  
+  
+  
+  # demo graph
+  $demo = $_REQUEST['demo'];
+  if ($demo == 1) {
+    $demo_graphQ = $uetz;
+    $demo_graphR = $ito;
+  }
+  title('compare-graphs');
+  echo ("<center>Computes the intersection, the union or the difference of two graphs</center>\n");
+  echo ("<form method='post' action='compare_graphs.php' enctype='multipart/form-data'>");
+  echo ("<table>\n");
+  echo ("<tr><th>Query graph</th><th>Reference graph</th></tr>\n");
+  ## QUERY INPUT FORMAT
+  echo "<tr><td>";
+  if (!$pipe) {
+    echo ("
+    <B>Input format</B></a>&nbsp;<select name='in_formatQ'>
+    <option selected value = 'tab'> tab-delimited format
+    <option value = 'adj_matrix'> Adjacency matrix
+    <option value = 'gml'> GML format
+    </select><br>");
+  } else {
+    echo ": $graph_format<br>";
+    echo "<input type='hidden' NAME='in_formatQ' VALUE='$graph_format'>";
+  } 
+  echo "</td>";
+  ## REFERENCE INPUT FORMAT
+  echo ("<td>
+  <B>Input format</B></a>&nbsp;<select name='in_formatR'>
+  <option selected value = 'tab'> tab-delimited format
+  <option value = 'adj_matrix'> Adjacency matrix
+  <option value = 'gml'> GML format
+  </select><br></td></tr>");
+  ## QUERY INPUT GRAPH TEXTAREA
+  echo "<td>";
+  if (!$pipe) {
+    echo ("<textarea name='graphQ' rows='6' cols='65'>$demo_graphQ</textarea></td>");
+  }
+  echo "</td>";
+  ## REFERENCE INPUT GRAPH TEXTAREA
+  echo ("<td><textarea name='graphR' rows='6' cols='65'>$demo_graphR</textarea></td>");
+  echo ("<tr><td>");
+  ## QUERY INPUT GRAPH FILE
+  if (!$pipe) {
+    echo ("Upload query graph from file : <br>
+    <input type='hidden' name='MAX_FILE_SIZE' value='30000' />
+    <input type='file' name='graph_fileQ' size='45' />");
+  } else {
+    info("Query graph uploaded from the previous treatment");
+    echo "<input type='hidden' NAME='pipe_query_graph_file' VALUE='$query_graph_file'/>";
+  }
+  echo "</td>";
+  ## REFERENCE INPUT GRAPH FILE
+  echo ("<td>Upload reference graph from file : <br>
+  <input type='hidden' name='MAX_FILE_SIZE' value='30000' />
+  <input type='file' name='graph_fileR' size='45' /></td>");
+  echo ("</tr>");
+  ## QUERY INPUT GRAPH PARAMETERS
+  echo ("<tr>");
+  echo ("<td>");
+  if (!$pipe) {
+    echo("Column specification (only relevant for tab-delimited input)</a><br>
+    <table>
+    <tr><td><B><a href = 'help.compare_graphs.html#scol'>Source node</a></B></td><td><input type = 'text' name='s_colQ' value = '$default_scolQ' size = 1></input></td></tr>
+    <tr><td><B><a href = 'help.compare_graphs.html#scol'>Target node</a></B></td><td><input type = 'text' name='t_colQ' value = '$default_tcolQ' size = 1></input></td></tr>
+    <tr><td><B><a href = 'help.compare_graphs.html#wcol'>Weight or label column</a></B></td><td><input type = 'text' name='w_colQ' size = 1></input></td></tr>
+    </table>");
+  } else {
+    if ($graph_format == 'tab') {
+      echo "<input type='hidden' NAME='s_colQ' VALUE='$query_scol'>";
+      echo "<input type='hidden' NAME='t_colQ' VALUE='$query_tcol'>";
+      echo "<input type='hidden' NAME='w_colQ' VALUE='$query_wcol'>";
+    }
+  }
+  echo ("</td");
+  ## REFENCE INPUT GRAPH PARAMETERS
+  echo ("<td>");
+  echo("Column specification (only relevant for tab-delimited input)</a><br>
+  <table>
+  <tr><td><B><a href = 'help.compare_graphs.html#scol'>Source node</a></B></td><td><input type = 'text' name='s_colR' value = '$default_scolR' size = 1></input></td></tr>
+  <tr><td><B><a href = 'help.compare_graphs.html#scol'>Target node</a></B></td><td><input type = 'text' name='t_colR' value = '$default_tcolR' size = 1></input></td></tr>
+  <tr><td><B><a href = 'help.compare_graphs.html#wcol'>Weight or label column</a></B></td><td><input type = 'text' name='w_colR' size = 1></input></td></tr>
+  </table>");
+  echo ("</table><br><br>");
+  ## OUTPUT SPECIFICATION
+
+  echo("
+  <B><a href = 'help.compare_graphs.html#return'>Output</B></a>&nbsp;<select name='return'>
+  <option selected value = 'union'> union
+  <option value = 'intersection'> intersection
+  <option value = 'difference'> difference
+  <option value = 'intersection+Q'> intersection + query
+  <option value = 'intersection+R'> intersection + reference
+  </select><br><br>");
+  echo ("<ul>
+  <B><a href = 'help.compare_graphs.html#formats'>Output format</a></B>&nbsp;<select name='out_format'>
+  <option value = 'tab'> tab-delimited format
+  <option selected value = 'gml'> GML format
+  <option value = 'adj_matrix'> Adjacency matrix
+  </select><br><br>");
+  ## Weight on the edges
+  echo ("
+  <B><a href = 'help.compare_graphs.html#outweights'>Weight on the edges of the output graph</a></B></a>&nbsp;<select name='outweight'>
+  <option value = 'Q'> weight (label) of the query
+  <option value = 'R'> weight (label) of the reference
+  <option value = 'sum'> sum of the weights
+  <option value = 'mean'> arithemetic mean of the weights
+  <option value = 'mean.g'> geometrical mean of the weights
+  <option value = 'min'> min of the weights
+  <option value = 'max'> max of the weights
+  <option selected value = 'Q::R'> weights of query and reference
+  </select><br><br>");
+  ## Directed graph
+  echo("
+    <input type='checkbox' name='directed' value='on'/>&nbsp;<B><a href = 'help.compare_graphs.html#directed'>Graphs must be considered as directed</a></B><br><br>");
+  echo("<input type='checkbox' name='self' value='on'/>&nbsp;<B><a href = 'help.compare_graphs.html#directed'>Graph admit self loops</a></B><br><br></ul>");
+  echo ("<ul><ul><table class='formbutton'>
+  <TD><input type='submit' name='.submit' value='GO' /></TD>
+  <TD><B><A HREF='compare_graphs_form.php?demo=0'>RESET</A></B></TD>
+  <TD><B><A HREF='compare_graphs_form.php?demo=1'>DEMO</A></B></TD>
+  </form>
+  <TD><B><A HREF='help.compare_graphs.html'>MANUAL</A></B></TD>
+  <TD><B><A HREF='mailto:sylvain@scmbb.ulb.ac.be'>MAIL</A></B></TD>
+  </TR></TABLE></ul></ul>");
