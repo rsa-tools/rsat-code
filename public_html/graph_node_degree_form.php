5b1645f20d78df958962689505f9a9ddef99166a
rsat <rsat>
2007-11-26 15:25:47 +0000

    Changes to the web interface, perl script and WSDL file for graph treatment

--- public_html/graph_node_degree_form.php
+++ public_html/graph_node_degree_form.php
@@ -11,40 +11,73 @@
   $default_scol = 1;
   $default_tcol = 2;
   $default_wcol = "";
+  # PIPE VALUES
+  $pipe = $_REQUEST['pipe'];
+  $graph_file = $_REQUEST['graph_file'];
+  $graph_format = $_REQUEST['graph_format'];
+  $scol = $_REQUEST['scol'];
+  $tcol = $_REQUEST['tcol'];
+  $wcol = $_REQUEST['wcol'];
+  $eccol = $_REQUEST['eccol'];
+  $selection_nodes_selected = '';
+  $all_nodes_selected = 'checked';
   # demo graph
   $demo = $_REQUEST['demo'];
   if ($demo == 1) {
     $demo_graph = $uetz;
     $demo_nodes = $uetz_nodes;
+    $selection_nodes_selected = 'checked';
+    $all_nodes_selected = '';
   }
 
   title('graph-node-degree');
   echo ("<center>Calculate the node degree of each node and specifies if this node is a
     seed or a target node.</center>\n");
   echo ("<form method='post' action='graph_node_degree.php' enctype='multipart/form-data'>
-  &nbsp;&nbsp;&nbsp;<a href = 'help.graph_node_degree.html#formats'><B>Input format</B></a>&nbsp;<select name='in_format'>
-  <option selected value = 'tab'> tab-delimited format
-  <option value = 'adj_matrix'> Adjacency matrix
-  <option value = 'gml'> GML format
-  </select><br><br><b>Graph</b><br>");
-  if ($demo) {
-    demo("This demonstration graph is the yeast two-hybrid dataset produced by <a target = 'top' href = 'http://www.ncbi.nlm.nih.gov/sites/entrez?db=pubmed&uid=10688190&cmd=showdetailview&indexed=google'>Uetz et al (2001)</a>. It consists in 865 interactions between 926 proteins. The nodes for which the degree is seeked were chosen in this network amongst the more or the less connected nodes." );
+  &nbsp;&nbsp;&nbsp;<a href = 'help.graph_node_degree.html#formats'><B>Input format</B></a>");
+  if (!$pipe) {
+    echo ("
+    &nbsp;<select name='in_format'>
+    <option selected value = 'tab'> tab-delimited format
+    <option value = 'adj_matrix'> Adjacency matrix
+    <option value = 'gml'> GML format
+    </select><br><br>");
+  } else {
+    echo ": $graph_format<br>";
+    echo "<input type='hidden' NAME='in_format' VALUE='$graph_format'>";
   }
-  echo ("<textarea name='graph' rows='6' cols='65'>$demo_graph</textarea>
-  <br>Upload graph from file : <br>
-  <input type='file' name='graph_file' size='45' /><br>
-  &nbsp;&nbsp;&nbsp;
-  
-  <br><a href = 'help.graph_node_degree.html#columns'>Column specification (only relevant for tab-delimited input)</a><br>
-  <table>
-  <tr><td><B><a href = 'help.graph_node_degree.html#scol'>Source node</a></B></td><td><input type = 'text' name='s_col' value = '$default_scol' size = 1></input></td></tr>
-  <tr><td><B><a href = 'help.graph_node_degree.html#scol'>Target node</a></B></td><td><input type = 'text' name='t_col' value = '$default_tcol' size = 1></input></td></tr>
-  </table>
-  <br>
-  </select><br><br><b><a href = 'help.graph_node_degree.html#nodes'>Nodes</a></b><br>
+  if (!$pipe) {
+    if ($demo) {
+      demo("This demonstration graph is the yeast two-hybrid dataset produced by <a target = 'top' href = 'http://www.ncbi.nlm.nih.gov/sites/entrez?db=pubmed&uid=10688190&cmd=showdetailview&indexed=google'>Uetz et al (2001)</a>. It consists in 865 interactions between 926 proteins. The nodes for which the degree is seeked were chosen in this network amongst the more and the less connected nodes." );
+    }
+    echo ("<b>Graph</b><br>");
+    echo ("<textarea name='graph' rows='6' cols='65'>$demo_graph</textarea>
+    <br>Upload graph from file : <br>
+    <input type='file' name='graph_file' size='45' /><br>
+    &nbsp;&nbsp;&nbsp;
+    <br><a href = 'help.graph_node_degree.html#columns'>Column specification (only relevant for tab-delimited input)</a><br>
+    <table>
+    <tr><td><B><a href = 'help.graph_node_degree.html#scol'>Source node</a></B></td><td><input type = 'text' name='s_col' value = '$default_scol' size = 1></input></td></tr>
+    <tr><td><B><a href = 'help.graph_node_degree.html#scol'>Target node</a></B></td><td><input type = 'text' name='t_col' value = '$default_tcol' size = 1></input></td></tr>
+    </table>");
+  } else {
+    info("Graph uploaded from the previous treatment");
+    echo "<input type='hidden' NAME='pipe_graph_file' VALUE='$graph_file'>";
+  }
+  if ($graph_format == 'tab') {
+    echo "<input type='hidden' NAME='s_col' VALUE='$scol'/>\n";
+    echo "<input type='hidden' NAME='t_col' VALUE='$tcol'/>\n";
+  }
+  echo("
+  <br><br><b><a href = 'help.graph_node_degree.html#nodes'>Nodes selection</a></b><br>
+  <br><table>
+  <tr><td><input type = 'radio' $all_nodes_selected name='allnodes' value = 'all'/></td><td>All nodes</td>
+  <tr><td><input type='radio' $selection_nodes_selected name='allnodes' value='selection' /></td>
+  <td></select><br>
+  <b>List of nodes</b><br>
   <textarea name='nodes' rows='6' cols='65'>$demo_nodes</textarea>
   <br>Upload nodes from file : <br>
-  <input type='file' name='nodes_file' size='45' /><br>");
+  <input type='file' name='nodes_file' size='45' /></td></tr></table>");
   echo ("
   <ul><ul><table class='formbutton'>
   <TD><input type='submit' name='.submit' value='GO' /></TD>
