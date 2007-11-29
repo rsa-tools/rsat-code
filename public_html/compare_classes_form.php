5197f5d8fb996b101ada8e1a98bf8b6abb989167
rsat <rsat>
2007-11-29 09:52:54 +0000

    compare_classes web interface deals with matrix output

--- public_html/compare_classes_form.php
+++ public_html/compare_classes_form.php
@@ -61,31 +61,53 @@
   echo("Score column (only relevant when a classification file is compared to itself)
     <input type = 'text' name='score_col' size = 1></input></td></tr>");
   }
-  echo("<table border='0' cellspacing='0' cellpadding='0'>
+  
+  echo("<br>
+  &nbsp;&nbsp;&nbsp;<B><a href = 'help.compare_classes_fortm.html#formats'>Output format</a></B>&nbsp;<select name='out_format'>
+  <option selected value = 'class'> class file
+  <option value = 'matrix'> matrix file
+  </select><br>");
+  echo ("<table border = 1  cellspacing='0' cellpadding='4' align = 'center'><tr align = 'center'>");
+  echo ("<td><b>Class file output parameters</b></td><td><b>Matrix file parameters</b></td></tr><tr align = 'center' valign = 'TOP'><br>");
+  # CLASS FILE OUTPUT PARAMETERS
+  echo("<td><table border='0' cellspacing='0' cellpadding='0'>
   <tr>  <th> <A HREF='help.compare-classes.html#return_fields'>Return fields</A> </th></tr> 
   <tr><td><label><input type='checkbox' name='occ' value='on' checked='checked' /> Occurrences </label></td></tr> 
   <tr><td><label><input type='checkbox' name='freq' value='on' checked='checked' /> Frequencies </label></td></tr> 
   <tr><td><label><input type='checkbox' name='proba' value='on' checked='checked' /> Probabilities </label></td></tr> 
   <tr><td><label><input type='checkbox' name='jac_sim' value='on' checked='checked' /> Jaccard index </label></td></tr> 
   <tr><td><label><input type='checkbox' name='entropy' value='on' checked='checked' /> Entropy </label></td></tr> 
-  <tr><td><label><input type='checkbox' name='members' value='on' /> Members </label></td></tr></table>
-  <tr><td><label><input type='checkbox' name='dotproduct' value='on' /> Dotproduct <i> (Only relevant if a score column is specified)</i></label></td></tr></table>");
-  echo("<br><br>");
+  <tr><td><label><input type='checkbox' name='members' value='on' /> Members </label></td></tr>
+  <tr><td><label><input type='checkbox' name='dotprod' value='on' /> Dotproduct <i> (Only relevant if a score column is specified)</i></label></td></tr>");
+  echo("</table><br><br>");
      
   echo("<table border='0' cellspacing='0' cellpadding='0'>
         <tr><th> <A HREF='help.compare-classes.html#return_fields'>Thresholds on return fields</A> </th> <th> <A HREF='help.compare-classes.html#thresholds'>Lower<BR>Threshold</A> </th> <th> <A HREF='help.compare-classes.html#thresholds'>Upper<BR>Threshold</A> </th></tr> 
-        <tr align='left' valign='TOP'><td> Query size </td> <td><input type='text' name='lth_q'  size='5' /></td> <td><input type='text' name='uth_q'  size='5' /></td></tr> 
+        <tr align='left' valign='TOP'><td> Query size </td> <td><input type='text' name='lth_q' size='5' /></td> <td><input type='text' name='uth_q'  size='5' /></td></tr> 
         <tr align='left' valign='TOP'><td> Reference size </td> <td><input type='text' name='lth_r'  size='5' /></td> <td><input type='text' name='uth_r'  size='5' /></td></tr> 
         <tr align='left' valign='TOP'><td> Intersection size </td> <td><input type='text' name='lth_qr'  size='5' /></td> <td><input type='text' name='uth_qr'  size='5' /></td></tr> 
-        <tr align='left' valign='TOP'><td> Significance </td> <td><input type='text' name='lth_sig' value='0' size='5' /></td> <td><input type='text' name='uth_sig' value='none' size='5' /></td></tr> 
+        <tr align='left' valign='TOP'><td> Significance </td> <td><input type='text' name='lth_sig' value='0' size='5' /></td> <td><input type='text' name='uth_sig' value='' size='5' /></td></tr> 
         <tr align='left' valign='TOP'><td> P-value </td> <td><input type='text' name='lth_pval'  size='5' /></td> <td><input type='text' name='uth_pval'  size='5' /></td></tr> 
         <tr align='left' valign='TOP'><td> E-value </td> <td><input type='text' name='lth_eval'  size='5' /></td> <td><input type='text' name='uth_eval'  size='5' /></td></tr> 
         <tr align='left' valign='TOP'><td> Jaccard index </td> <td><input type='text' name='lth_jac'  size='5' /></td> <td><input type='text' name='uth_jac'  size='5' /></td></tr> 
         <tr align='left' valign='TOP'><td> Mutual information </td> <td><input type='text' name='lth_mi'  size='5' /></td> <td><input type='text' name='uth_mi'  size='5' /></td></tr>
-        <tr align='left' valign='TOP'><td> Dot product </td> <td><input type='text' name='lth_dp'  size='5' /></td> <td><input type='text' name='uth_dp'  size='5' /></td><td><i> (Only relevant if a score column is specified)</i></td></tr>
-        </table><br>");
         
-       
+        <tr><td><i> (Only relevant if a score column is specified)</i></td></tr>
+        
+        <tr align='left' valign='TOP'><td> Dot product </td> <td><input type='text' name='lth_dp'  size='5' /></td> <td><input type='text' name='uth_dp'  size='5' /></td></tr>
+        </table><br>");
+  echo ("</td>");
+  # MATRIX FILE OUTPUT PARAMETERS
+  echo ("<td><table>");
+  echo ("<tr><td><input type = 'radio' name = 'matrix_field' checked = 'checked' value = 'QR'/></td><td>Intersection</td></tr>
+         <tr><td><input type = 'radio' name = 'matrix_field' value = 'sig'/></td><td>Significance</td></tr>
+         <tr><td><input type = 'radio' name = 'matrix_field' value = 'jac_sim'/></td><td>Jaccard index</td></tr>
+         <tr><td><input type = 'radio' name = 'matrix_field' value = 'dotprod'/></td><td>Dot product</td></tr>
+         <tr><td><input type = 'radio' name = 'matrix_field' value = 'E_val'/></td><td>E-value</td></tr>
+         <tr><td><input type = 'radio' name = 'matrix_field' value = 'P_val'/></td><td>P-value</td></tr>
+         <tr><td><input type = 'radio' name = 'matrix_field' value = 'I(Q,R)'/></td><td>Mutual information</td></tr>
+         </table>");
+  echo ("</td></tr></table>");       
   echo ("<ul><ul><table class='formbutton'>
   <TD><input type='submit' name='.submit' value='GO' /></TD>
   <TD><B><A HREF='compare_classes_form.php?demo=0'>RESET</A></B></TD>
