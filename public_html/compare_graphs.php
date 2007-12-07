834ae771353facba1145122c63b21a06d543ea1b
jvanheld <jvanheld>
2007-12-07 08:32:21 +0000

    changed GrA for Network Analysis

--- public_html/compare_graphs.php
+++ public_html/compare_graphs.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-   <title>GrA-tools - convert-graph</title>
+   <title>Network Analysis Tools - convert-graph</title>
    <link rel="stylesheet" type="text/css" href = "main_grat.css" media="screen">
 </head>
 <body class="results">
