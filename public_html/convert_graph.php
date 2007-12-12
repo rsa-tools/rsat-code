e60f09e98850c367d123ed40b9293a3c5c3bd654
rsat <rsat>
2007-12-12 16:06:10 +0000

    Small change to convert_graph.php

--- public_html/convert_graph.php
+++ public_html/convert_graph.php
@@ -77,7 +77,7 @@
   
     $graph = trim_text($graph);
     $graph = space_to_tab($graph);
-    echo ("<pre>$graph</pre>");
+//     echo ("<pre>$graph</pre>");
     ## Load the parameters of the program in to an array
     $parameters = array( 
       "request" => array(
