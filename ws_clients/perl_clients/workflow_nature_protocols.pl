09cf4c69fb7e9d7f51a2ca73dac6101e9d0e7351
oly <oly>
2007-12-13 10:07:09 +0000

    print results to files

--- ws_clients/perl_clients/workflow_nature_protocols.pl
+++ ws_clients/perl_clients/workflow_nature_protocols.pl
@@ -48,13 +48,12 @@ my $scalebar = 1;
 my $scalestep = 50;
 my $map_from = -800;
 my $map_to = 0;
-my $map_origin = 0;
 my $scorethick = 1;
-my $htmap = 1;
+# my $htmap = 1;
+my $map_format = 'jpg';
 
 ## Read cluster file
-# open INPUT, "/Users/oly/Desktop/ABF1_YPD_cds.fam" or die "Can't open input file: $!\n";
-open INPUT, "/Users/oly/Desktop/Harbison_2004_sig0_4.fam" or die "Can't open input file: $!\n";
+open INPUT, "/Users/oly/Desktop/Nature_protocols/Harbison_2004_sig0.fam" or die "Can't open input file: $!\n";
 
 ## Loop over gene clusters
 my @genes;
@@ -71,230 +70,7 @@ while (my $line = <INPUT>) {
     next;
   } else {
     if (@genes) {
-
-      #  die ("TEST FINISHED\n");
-      #################################################
-      ## Retrieve-seq part
-
-      ## Output option
-      my $output_choice = 'server'; ## The result will stay in a file on the server
-
-      my %args = (
-		  'output' => $output_choice,
-		  'organism' => $organism,
-		  'query' => \@genes,  ## An array in a hash has to be referenced
-		  'noorf' => $noorf
-	   );
-
-      ## Send request to the server
-      print "\nRetrieve-seq: sending request to the server\t", $server, "\n";
-      my $som = $soap->call('retrieve_seq' => 'request' => \%args);
-
-      ## Get the result
-      my $sequence_file;  ## That variable needs to be declared outside the if..else block to be useable in the next part
-      if ($som->fault){  ## Report error if any
-	printf "A fault (%s) occured: %s\n", $som->faultcode, $som->faultstring;
-      } else {
-	my $results_ref = $som->result;  ## A reference to the result hash table
-	my %results = %$results_ref;  ## Dereference the result hash table
-
-	## Report the remote command
-	my $command = $results{'command'};
-	print "Command used on the server:\n\t".$command, "\n";
-
-	## Report the result file name on the server
-	$sequence_file = $results{'server'};
-	print "Result file on the server:\n\t".$sequence_file;
-      }
-      #################################################
-      ## Purge-sequence part
-
-      %args = (
-	       'output' => $output_choice,  ## Same 'server' output option
-	       'tmp_infile' => $sequence_file  ## Output from retrieve-seq part is used as input here
-	      );
-
-      ## Send the request to the server
-      print "\nPurge-sequence: sending request to the server\t", $server, "\n";
-      $som = $soap -> call('purge_seq' => 'request' => \%args);
-
-      ## Get the result
-      my $purged_sequence_file;  ## That variable needs to be declared outside the if..else block to be useable in the next part
-      if ($som->fault){  ## Report error if any
-	printf "A fault (%s) occured: %s\n", $som->faultcode, $som->faultstring;
-      } else {
-	my $results_ref = $som->result;  ## A reference to the result hash table
-	my %results = %$results_ref;  ## Dereference the result hash table
-
-	## Report the remote command
-	my $command = $results{'command'};
-	print "Command used on the server: \n\t".$command, "\n";
-
-	## Report the result file name on the server
-	$purged_sequence_file = $results{'server'};
-	print "Result file on the server: \n\t".$purged_sequence_file;
-      }
-      #################################################
-      ## Oligo-analysis part
-
-      %args = (
-	       'output' => $output_choice,
-	       'tmp_infile' => $purged_sequence_file,
-	       'length' => $length,
-	       'organism' => $organism,
-	       'background' => $background,
-	       'stats' => $stats,
-	       'noov' => $noov,
-	       'str' => $str,
-	       'sort' => $sort,
-	       'lth' => $lth,
-	       'verbosity' => 1
-	      );
-
-      ## Send request to the server
-      print "\nOligo-analysis: sending request to the server\t", $server, "\n";
-      $som = $soap->call('oligo_analysis' => 'request' => \%args);
-
-      ## Get the result
-      my $oligos_file;  ## That variable needs to be declared outside the if..else block to be useable in the next part
-      if ($som->fault){  ## Report error if any
-	printf "A fault (%s) occured: %s\n", $som->faultcode, $som->faultstring;
-      } else {
-	my $results_ref = $som->result;
-	my %results = %$results_ref;
-
-	## Report remote command
-	my $command = $results{'command'};
-	print "Command used on the server: \n\t".$command, "\n";
-
-	## Report the result file name on the server
-	$oligos_file = $results{'server'};
-	print "Result file on the server: \n\t".$oligos_file;
-
-	## Report the result
-	#  my $result = $results{'client'};
-	#  print "Discovered oligo(s): \n".$result;
-      }
-
-      #################################################
-      ## Dna-pattern part
-
-      $output_choice = 'both';
-
-      %args = ('output' => $output_choice,
-	       'tmp_infile' => $sequence_file,
-	       'tmp_pattern_file' => $oligos_file,
-	       'origin' => $origin,
-	       #	 'noov' => $noov,
-	       'str' => $str,
-	       'sort' => $sort,
-	       'th' => $th,
-	       'return' => 'sites,limits',
-	       'score' => $score_column
-	      );
-
-      ## Send request to the server
-      print "\nDna-pattern: sending request to the server\t", $server, "\n";
-      $som = $soap->call('dna_pattern' => 'request' => \%args);
-
-      ## Get the result
-      my $feature_file;
-      if ($som->fault){  ## Report error if any
-	printf "A fault (%s) occured: %s\n", $som->faultcode, $som->faultstring;
-      } else {
-	my $results_ref = $som->result;
-	my %results = %$results_ref;
-
-	## Report remote command
-	my $command = $results{'command'};
-	print "Command used on the server: \n\t".$command, "\n";
-
-	## Report the result file name on the server
-	$feature_file = $results{'server'};
-	print "Result file on the server: \n\t".$feature_file, "\n";
-
-	## Report the result
-	my $result = $results{'client'};
-	print "Features(s): \n\t".$result;
-      }
-
-      #################################################
-      ## Convert-features part
-
-      %args = ('output' => $output_choice,
-	       'tmp_infile' => $feature_file,
-	       'from' => $feature_in,
-	       'to' => $feature_out
-	      );
-
-      ## Send request to the server
-      print "\nConvert-features: sending request to the server\t", $server, "\n";
-      $som = $soap->call('convert_features' => 'request' => \%args);
-
-      ## Get the result
-      my $converted_feature_file;
-      if ($som->fault){  ## Report error if any
-	printf "A fault (%s) occured: %s\n", $som->faultcode, $som->faultstring;
-      } else {
-	my $results_ref = $som->result;
-	my %results = %$results_ref;
-
-	## Report remote command
-	my $command = $results{'command'};
-	print "Command used on the server: \n\t".$command, "\n";
-
-	## Report the result file name on the server
-	$converted_feature_file = $results{'server'};
-	print "Result file on the server: \n\t".$converted_feature_file, "\n";
-
-	## Report the result
-	#  my $result = $results{'client'};
-	#  print "Converted features(s): \n\t".$result;
-      }
-
-      #################################################
-      ## Feature-map part
-      chomp $sequence_file;
-
-      %args = (
-	       'output' => $output_choice,
-	       'tmp_infile' => $converted_feature_file,
-	       'tmp_sequence_file' => $sequence_file,
-	       #	 'sequence_format' => 'fasta',
-	       #	 'origin' => $map_origin,
-	       'legend' => $legend,
-	       'scalebar' => $scalebar,
-	       'scalestep' => $scalestep,
-	       'scorethick' => $scorethick,
-	       'from' => $map_from,
-	       'to' => $map_to,
-	       #	 'htmap' => $htmap
-	      );
-
-      ## Send request to the server
-      print "\nFeature-map: sending request to the server\t", $server, "\n";
-      $som = $soap->call('feature_map' => 'request' => \%args);
-
-      ## Get the result
-      my $feature_map_file;
-      if ($som->fault){  ## Report error if any
-	printf "A fault (%s) occured: %s\n", $som->faultcode, $som->faultstring;
-      } else {
-	my $results_ref = $som->result;
-	my %results = %$results_ref;
-
-	## Report remote command
-	my $command = $results{'command'};
-	print "Command used on the server: \n\t".$command, "\n";
-
-	## Report the result file name on the server
-	$feature_map_file = $results{'server'};
-	print "Result file on the server: \n\t".$feature_map_file, "\n";
-
-	## Report the result
-	#  my $result = $results{'client'};
-	#  print "Features(s): \n\t".$result;
-      }
+      &Analysis();
     }
     $cluster = $entry[1];
     @genes='';
@@ -302,5 +78,243 @@ while (my $line = <INPUT>) {
     next;
   }
 }
+&Analysis();  ## run with last cluster
 
 close INPUT;
+
+###################################################
+sub Analysis {
+
+#  die ("TEST FINISHED\n");
+  #################################################
+  ## Retrieve-seq part
+
+  ## Output option
+  my $output_choice = 'server'; ## The result will stay in a file on the server
+
+  my %args = (
+	      'output' => $output_choice,
+	      'organism' => $organism,
+	      'query' => \@genes,  ## An array in a hash has to be referenced
+	      'noorf' => $noorf
+	     );
+
+  ## Send request to the server
+  print "\nRetrieve-seq: sending request to the server\t", $server, "\n";
+  my $som = $soap->call('retrieve_seq' => 'request' => \%args);
+
+  ## Get the result
+  my $sequence_file;  ## That variable needs to be declared outside the if..else block to be useable in the next part
+  if ($som->fault){  ## Report error if any
+    printf "A fault (%s) occured: %s\n", $som->faultcode, $som->faultstring;
+  } else {
+    my $results_ref = $som->result;  ## A reference to the result hash table
+    my %results = %$results_ref;  ## Dereference the result hash table
+
+    ## Report the remote command
+    my $command = $results{'command'};
+    print "Command used on the server:\n\t".$command, "\n";
+
+    ## Report the result file name on the server
+    $sequence_file = $results{'server'};
+    print "Result file on the server:\n\t".$sequence_file;
+  }
+  #################################################
+  ## Purge-sequence part
+
+  %args = (
+	   'output' => $output_choice,  ## Same 'server' output option
+	   'tmp_infile' => $sequence_file  ## Output from retrieve-seq part is used as input here
+	  );
+
+  ## Send the request to the server
+  print "\nPurge-sequence: sending request to the server\t", $server, "\n";
+  $som = $soap -> call('purge_seq' => 'request' => \%args);
+
+  ## Get the result
+  my $purged_sequence_file;  ## That variable needs to be declared outside the if..else block to be useable in the next part
+  if ($som->fault){  ## Report error if any
+    printf "A fault (%s) occured: %s\n", $som->faultcode, $som->faultstring;
+  } else {
+    my $results_ref = $som->result;  ## A reference to the result hash table
+	my %results = %$results_ref;  ## Dereference the result hash table
+
+    ## Report the remote command
+    my $command = $results{'command'};
+    print "Command used on the server: \n\t".$command, "\n";
+
+    ## Report the result file name on the server
+    $purged_sequence_file = $results{'server'};
+    print "Result file on the server: \n\t".$purged_sequence_file;
+  }
+  #################################################
+  ## Oligo-analysis part
+
+  $output_choice = 'both';
+
+  %args = (
+	   'output' => $output_choice,
+	   'tmp_infile' => $purged_sequence_file,
+	   'length' => $length,
+	   'organism' => $organism,
+	   'background' => $background,
+	   'stats' => $stats,
+	   'noov' => $noov,
+	   'str' => $str,
+	   'sort' => $sort,
+	   'lth' => $lth,
+	   'verbosity' => 1
+	  );
+
+  ## Send request to the server
+  print "\nOligo-analysis: sending request to the server\t", $server, "\n";
+  $som = $soap->call('oligo_analysis' => 'request' => \%args);
+
+  ## Get the result
+  my $oligos_file;  ## That variable needs to be declared outside the if..else block to be useable in the next part
+  if ($som->fault){  ## Report error if any
+    printf "A fault (%s) occured: %s\n", $som->faultcode, $som->faultstring;
+  } else {
+    my $results_ref = $som->result;
+    my %results = %$results_ref;
+
+    ## Report remote command
+    my $command = $results{'command'};
+    print "Command used on the server: \n\t".$command, "\n";
+
+    ## Report the result file name on the server
+    $oligos_file = $results{'server'};
+    print "Result file on the server: \n\t".$oligos_file;
+
+    ## Store the result in file
+    my $result = $results{'client'};
+    my $output_oligo_file = "/Users/oly/Desktop/Nature_protocols/".$cluster."_oligos.tab";
+    open OUTPUT_OLIGO, ">$output_oligo_file" or die "Can't open input file: $!\n";
+    print OUTPUT_OLIGO $result;
+    close OUTPUT_OLIGO;
+  }
+
+  #################################################
+  ## Dna-pattern part
+
+  $output_choice = 'server';
+
+  %args = ('output' => $output_choice,
+	   'tmp_infile' => $sequence_file,
+	   'tmp_pattern_file' => $oligos_file,
+	   'origin' => $origin,
+	   #	 'noov' => $noov,
+	   'str' => $str,
+	   'sort' => $sort,
+	   'th' => $th,
+	   'return' => 'sites,limits',
+	   'score' => $score_column
+	  );
+
+  ## Send request to the server
+  print "\nDna-pattern: sending request to the server\t", $server, "\n";
+  $som = $soap->call('dna_pattern' => 'request' => \%args);
+
+  ## Get the result
+  my $feature_file;
+  if ($som->fault){  ## Report error if any
+    printf "A fault (%s) occured: %s\n", $som->faultcode, $som->faultstring;
+  } else {
+    my $results_ref = $som->result;
+    my %results = %$results_ref;
+
+    ## Report remote command
+    my $command = $results{'command'};
+    print "Command used on the server: \n\t".$command, "\n";
+
+    ## Report the result file name on the server
+    $feature_file = $results{'server'};
+    print "Result file on the server: \n\t".$feature_file, "\n";
+
+    ## Report the result
+#    my $result = $results{'client'};
+#    print "Features(s): \n\t".$result;
+  }
+
+  #################################################
+  ## Convert-features part
+
+  %args = ('output' => $output_choice,
+	   'tmp_infile' => $feature_file,
+	   'from' => $feature_in,
+	   'to' => $feature_out
+	  );
+
+  ## Send request to the server
+  print "\nConvert-features: sending request to the server\t", $server, "\n";
+  $som = $soap->call('convert_features' => 'request' => \%args);
+
+  ## Get the result
+  my $converted_feature_file;
+  if ($som->fault){  ## Report error if any
+    printf "A fault (%s) occured: %s\n", $som->faultcode, $som->faultstring;
+  } else {
+    my $results_ref = $som->result;
+    my %results = %$results_ref;
+
+    ## Report remote command
+    my $command = $results{'command'};
+    print "Command used on the server: \n\t".$command, "\n";
+
+    ## Report the result file name on the server
+    $converted_feature_file = $results{'server'};
+    print "Result file on the server: \n\t".$converted_feature_file, "\n";
+
+    ## Report the result
+    #  my $result = $results{'client'};
+    #  print "Converted features(s): \n\t".$result;
+  }
+
+  #################################################
+  ## Feature-map part
+  chomp $sequence_file;
+
+  $output_choice = 'both';
+
+  %args = (
+	   'output' => $output_choice,
+	   'tmp_infile' => $converted_feature_file,
+	   'tmp_sequence_file' => $sequence_file,
+	   'legend' => $legend,
+	   'scalebar' => $scalebar,
+	   'scalestep' => $scalestep,
+	   'scorethick' => $scorethick,
+	   'from' => $map_from,
+	   'to' => $map_to,
+	   #	 'htmap' => $htmap
+	   'format' => $map_format
+	  );
+
+  ## Send request to the server
+  print "\nFeature-map: sending request to the server\t", $server, "\n";
+  $som = $soap->call('feature_map' => 'request' => \%args);
+
+  ## Get the result
+  my $feature_map_file;
+  if ($som->fault){  ## Report error if any
+    printf "A fault (%s) occured: %s\n", $som->faultcode, $som->faultstring;
+  } else {
+    my $results_ref = $som->result;
+    my %results = %$results_ref;
+
+    ## Report remote command
+    my $command = $results{'command'};
+    print "Command used on the server: \n\t".$command, "\n";
+
+    ## Report the result file name on the server
+    $feature_map_file = $results{'server'};
+    print "Result file on the server: \n\t".$feature_map_file, "\n";
+
+    ## Store the result in file
+    my $result = $results{'client'};
+    my $output_map_file = "/Users/oly/Desktop/Nature_protocols/".$cluster."_map.".$map_format;
+    open OUTPUT_MAP, ">$output_map_file" or die "Can't open input file: $!\n";
+    print OUTPUT_MAP $result;
+    close OUTPUT_MAP;
+  }
+}
