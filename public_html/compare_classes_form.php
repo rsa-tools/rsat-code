1bc2bc3835fc9e60cafcffee8c23c59e68bee292
jvanheld <jvanheld>
2007-12-16 22:30:31 +0000

    added some default threshods

--- public_html/compare_classes_form.php
+++ public_html/compare_classes_form.php
@@ -106,20 +106,20 @@ echo ("<td><b>Parameters for output type 'class file'</b></td><td><b>Parameters 
 # CLASS FILE OUTPUT PARAMETERS
 echo("<td><table border='0' cellspacing='0' cellpadding='0'>
   <tr>  <th> <A HREF='help.compare_classes.html#return_fields'>Return fields</A> </th></tr> 
-  <tr><td><label><input type='checkbox' name='occ' value='on' checked='checked' /> Occurrences </label></td></tr> 
-  <tr><td><label><input type='checkbox' name='freq' value='on' checked='checked' /> Frequencies </label></td></tr> 
-  <tr><td><label><input type='checkbox' name='proba' value='on' checked='checked' /> Probabilities </label></td></tr> 
-  <tr><td><label><input type='checkbox' name='jac_sim' value='on' checked='checked' /> Jaccard index </label></td></tr> 
-  <tr><td><label><input type='checkbox' name='entropy' value='on' checked='checked' /> Entropy </label></td></tr> 
+  <tr><td><label><input type='checkbox' name='occ' value='on' checked='checked' />Occurrences</label></td></tr> 
+  <tr><td><label><input type='checkbox' name='freq' value='on' checked='checked' />Frequencies</label></td></tr> 
+  <tr><td><label><input type='checkbox' name='proba' value='on' checked='checked' />Hypergeometric probability</label></td></tr> 
+  <tr><td><label><input type='checkbox' name='jac_sim' value='on' checked='checked' />Jaccard index</label></td></tr> 
+  <tr><td><label><input type='checkbox' name='entropy' value='on' />Entropy</label></td></tr> 
   <tr><td><label><input type='checkbox' name='members' value='on' /> Members </label></td></tr>
   <tr><td><label><input type='checkbox' name='dotprod' value='on' /> Dotproduct <i> (Only relevant if a score column is specified)</i></label></td></tr>");
 echo("</table><br><br>");
      
 echo("<table border='0' cellspacing='0' cellpadding='0'>
         <tr><th> <A HREF='help.compare_classes.html#return_fields'>Thresholds on return fields</A> </th> <th> <A HREF='help.compare_classes.html#thresholds'>Lower<BR>Threshold</A> </th> <th> <A HREF='help.compare_classes.html#thresholds'>Upper<BR>Threshold</A> </th></tr> 
-        <tr align='left' valign='TOP'><td> Query size </td> <td><input type='text' name='lth_q' size='5' /></td> <td><input type='text' name='uth_q'  size='5' /></td></tr> 
-        <tr align='left' valign='TOP'><td> Reference size </td> <td><input type='text' name='lth_r'  size='5' /></td> <td><input type='text' name='uth_r'  size='5' /></td></tr> 
-        <tr align='left' valign='TOP'><td> Intersection size </td> <td><input type='text' name='lth_qr'  size='5' /></td> <td><input type='text' name='uth_qr'  size='5' /></td></tr> 
+        <tr align='left' valign='TOP'><td> Query size </td> <td><input type='text' name='lth_q' value='1' size='5' /></td> <td><input type='text' name='uth_q'  size='5' /></td></tr> 
+        <tr align='left' valign='TOP'><td> Reference size </td> <td><input type='text' name='lth_r' value='1'  size='5' /></td> <td><input type='text' name='uth_r'  size='5' /></td></tr> 
+        <tr align='left' valign='TOP'><td> Intersection size </td> <td><input type='text' name='lth_qr' value='1'  size='5' /></td> <td><input type='text' name='uth_qr'  size='5' /></td></tr> 
         <tr align='left' valign='TOP'><td> Significance </td> <td><input type='text' name='lth_sig' value='0' size='5' /></td> <td><input type='text' name='uth_sig' value='' size='5' /></td></tr> 
         <tr align='left' valign='TOP'><td> P-value </td> <td><input type='text' name='lth_pval'  size='5' /></td> <td><input type='text' name='uth_pval'  size='5' /></td></tr> 
         <tr align='left' valign='TOP'><td> E-value </td> <td><input type='text' name='lth_eval'  size='5' /></td> <td><input type='text' name='uth_eval'  size='5' /></td></tr> 
