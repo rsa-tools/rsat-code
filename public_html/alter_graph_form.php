cf98d719dffc444821c8d3a695094c4887874e67
rsat <rsat>
2008-01-17 15:51:46 +0000

    Small change to the NeAT tutorial

--- public_html/alter_graph_form.php
+++ public_html/alter_graph_form.php
@@ -116,7 +116,10 @@
   <TD><B><A HREF='alter_graph_form.php?demo=0'>RESET</A></B></TD>
   <TD><B><A HREF='alter_graph_form.php?demo=1'>DEMO</A></B></TD>
   </form>
+
   <TD><B><A HREF='help.alter_graph.html'>MANUAL</A></B></TD>
+  <TD><B><A target = '_blank' HREF='".checkNeatTutorial("tutorials/neat_tutorial/Influence_graph_alteration.html")."'>TUTORIAL</A></B></TD>
+
   <TD><B><A HREF='mailto:sylvain@scmbb.ulb.ac.be'>MAIL</A></B></TD>
   </TR></TABLE></ul></ul>
  ");
