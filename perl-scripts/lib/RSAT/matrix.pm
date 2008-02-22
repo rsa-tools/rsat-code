5126419c2d978fac27143ce7a05dd600182f8fb1
jvanheld <jvanheld>
2008-02-22 17:38:29 +0000

    added the option -logo_opt

--- perl-scripts/lib/RSAT/matrix.pm
+++ perl-scripts/lib/RSAT/matrix.pm
@@ -2921,20 +2921,21 @@ Return the logo from the matrix
 
 =cut
 sub makeLogo{
-	my ($self,$logo_file,$logo_formats,$logo_tmp_dir) = @_;
-	my (@logo_formats) = @{$logo_formats};
-	my ($seqs_file) =$self->seq_from_matrix($logo_tmp_dir);
-	foreach my $logo_format (@logo_formats){
-	  my $logo_cmd = $ENV{RSAT}."/bin/seqlogo ";
-	  $logo_cmd.= "-f ".$seqs_file;
-	  $logo_cmd .= " -F ".$logo_format." -c -Y -n -a -b -e -k 1";
-	  $logo_cmd .= " -o ". $logo_file;
-	  #	$logo_cmd .= " -t ".$self->get_attribute("name");
-	  &RSAT::message::Debug("Logo cmd :".$logo_cmd) if ($main::verbose >= 4);
-	  `$logo_cmd`;
-	  &RSAT::message::Info("Export logo in file ".$logo_file.".".$logo_format) if ($main::verbose >= 2);
-	}
-	unlink $seqs_file;
+  my ($self,$logo_file,$logo_formats,$logo_tmp_dir, $logo_options) = @_;
+  my (@logo_formats) = @{$logo_formats};
+  my ($seqs_file) =$self->seq_from_matrix($logo_tmp_dir);
+  foreach my $logo_format (@logo_formats){
+    my $logo_cmd = $ENV{RSAT}."/bin/seqlogo ";
+    $logo_cmd.= "-f ".$seqs_file;
+    $logo_cmd .= " -F ".$logo_format." -c -Y -n -a -b -e -k 1";
+    $logo_cmd .= " ".$logo_options;
+    $logo_cmd .= " -o ". $logo_file;
+    #	$logo_cmd .= " -t ".$self->get_attribute("name");
+    &RSAT::message::Info("Logo cmd :".$logo_cmd) if ($main::verbose >= 4);
+    system "$logo_cmd";
+    &RSAT::message::Info("Seq logo exported to file ".$logo_file.".".$logo_format) if ($main::verbose >= 2);
+  }
+  unlink $seqs_file;
 }
 
 sub seq_from_matrix {
