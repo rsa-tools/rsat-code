7fb0f1cb5b89fc0dff7c8ef650fb30050b81a220
rsat <rsat>
2008-03-07 09:04:04 +0000

    Addition of the help to string_download.php

--- public_html/string_dataset_form.php
+++ public_html/string_dataset_form.php
@@ -9,23 +9,23 @@
   require ('string_functions.php');
   title('String dataset download');
   
-  $evidence = array('automated_textmining','experimental_interaction_data','gene_cooccurence', 'gene_fusion_events','gene_coexpression', 'combined_confidence');
+  $evidence = array('combined_confidence','automated_textmining','experimental_interaction_data','gene_cooccurence', 'gene_fusion_events','gene_coexpression');
   # demo genes
   $demo = $_REQUEST['demo'];
   if ($demo == 1) {
     $demo_genes = "GAP1\nMEP2\nMEP1";
   }  
   
-   echo ("<center>Download a subgraph from the String database</center>\n");
+   echo ("<center>Download a subgraph from the <a href = 'http://string.embl.de/'>STRING database</a></center>\n");
    echo ("<br>
-   STRING is a database of known and predicted protein-protein interactions.
-The interactions include direct (physical) and indirect (functional) associations; they are derived from four sources. STRING quantitatively integrates interaction data from these sources for a large number of organisms, and transfers information between these organisms where applicable. The database currently contains 1,513,782 proteins in 373 species.<br> This program allows you to download a subgraph composed of the neighbours of the genes of the String database.
-          \n");
+   <a href = 'http://string.embl.de/'>STRING</a> is a database of known and predicted protein-protein interactions.
+The interactions include direct (physical) and indirect (functional) associations. <a href = 'http://string.embl.de/'>STRING</a> quantitatively integrates interaction data from various sources for a large number of organisms, and transfers information between these organisms where applicable. The database currently contains 1,513,782 proteins in 373 species.<br> This program allows you to download a subgraph composed of the neighbours of some query genes in the <a href = 'http://string.embl.de/'>STRING database</a>. 
+For more information on <a href = 'http://string.embl.de/'>STRING</a>, see <a href = 'http://www.ncbi.nlm.nih.gov/entrez/query.fcgi?cmd=Retrieve&db=PubMed&list_uids=17098935&dopt=Abstract'>Von Mering et al, 2007</a>          \n");
    
    echo ("<form method='post' action='string_names.php' enctype='multipart/form-data'> ");
    
   # LIST OF ORGANISM
-  echo("<br>&nbsp;&nbsp;&nbsp;<B>Organism</B><br>");
+  echo("<br>&nbsp;&nbsp;&nbsp;<a href = 'help.string_dataset.html#organism'><B>Organism</B></a><br>");
   $organisms = readStringOrganisms();
   echo ("<select name='organism'>");
   for ($i = 1; $i < count($organisms); $i++) {
@@ -38,11 +38,11 @@ The interactions include direct (physical) and indirect (functional) association
   echo ("</select>");
   
   # LIST OF PROTEINS
-  echo ("<br><br>&nbsp;&nbsp;&nbsp;<b>Genes</b><br>");
+  echo ("<br><br>&nbsp;&nbsp;&nbsp;<b><a href = 'help.string_dataset.html#genes'>Genes</a></b><br>");
   echo ("<textarea name='genes' rows='6' cols='65'>$demo_genes</textarea><br>");
   
   # SIGNIFICANCE THRESHOLD
-  echo ("<br><br>&nbsp;&nbsp;&nbsp;<b>String score threshold (between 0 and 1)</b><br>");
+  echo ("<br><br>&nbsp;&nbsp;&nbsp;<b><a href = 'help.string_dataset.html#threshold'>String score threshold</a> (between 0 and 1)</b><br>");
   echo("  
     <table>
     <tr><td><B>Lower threshold</B></td><td><input type = 'text' value = 'none' name='lth' size = 4></input></td></tr>
@@ -50,12 +50,16 @@ The interactions include direct (physical) and indirect (functional) association
     <table>");
   
   # EVIDENCE CHANNELS
-  echo ("<br><br>&nbsp;&nbsp;&nbsp;<b>Evidence channel</b><br><br>");
+  echo ("<br><br>&nbsp;&nbsp;&nbsp;<b><a href = 'help.string_dataset.html#channels'>Evidence channel</a></b><br><br>");
 
     for ($i = 0; $i < count ($evidence); $i++) {
       $evidence_channel = $evidence[$i];
-      str_replace("_", " ", $evidence_channel);
-      echo(" <input type='checkbox' checked name='$evidence[$i]' value='on'/>&nbsp;<B>$evidence_channel</B><br>");
+      $evidence_channel = str_replace("_", " ", $evidence_channel);
+      $checked = "";
+      if ($evidence[$i] == "combined_confidence") {
+        $checked = "checked";
+      }
+      echo(" <input type='checkbox' $checked name='$evidence[$i]' value='on'/>&nbsp;<B>$evidence_channel</B><br>");
     }
 
   
@@ -68,7 +72,7 @@ echo("
   </form>
 
   <TD><B><A target = '_blank' HREF='".checkNeatTutorial("")."'>TUTORIAL</A></B></TD>
-
+  <TD><B><A target = '_blank' HREF='help.string_dataset.html'>HELP</A></B></TD>
   <TD><B><A HREF='mailto:sylvain@scmbb.ulb.ac.be'>MAIL</A></B></TD>
   </TR></TABLE></ul></ul>
  ");
