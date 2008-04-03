04224e5b072aa4f844dcb57e5db2c459f42fba37
rsat <rsat>
2008-04-03 11:03:45 +0000

    Adaptation of RSATWS.pm et RSATWS.wsdl to the new options of convert-graph addition of path input parser

--- public_html/convert_graph.php
+++ public_html/convert_graph.php
@@ -104,6 +104,7 @@
     # Open the SOAP client
     $client = new SoapClient(
                        $neat_wsdl,
+// "http://rsat.scmbb.ulb.ac.be/rsat/web_services/RSATWS2.wsdl",
                            array(
                                  'trace' => 1,
                                  'soap_version' => SOAP_1_1,
