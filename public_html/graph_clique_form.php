40e3e367bbe370f9c782a5f52dd22c9b42d8db46
rsat <rsat>
2008-04-09 15:59:44 +0000

    Some bugs were solved in convert-graph web interface

--- public_html/graph_clique_form.php
+++ public_html/graph_clique_form.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-   <title>NeA-tools - graph-clique</title>
+   <title>NeA-tools - graph-cliques</title>
    <link rel="stylesheet" type="text/css" href = "main_grat.css" media="screen">
 </head>
 <body class="form">
@@ -25,18 +25,19 @@
   # demo graph
   $demo = $_REQUEST['demo'];
   if ($demo == 1) {
-    $demo_graph = $gavin_names;
-    $demo_nodes = $gavin_names_nodes;
-    $selection_nodes_selected = 'checked';
-    $all_nodes_selected = '';
+    $demo_graph = $uetz;
   }
 
-  title('graph-clique');
-  echo ("<center>Extract from a graph list of clique\n");
+  title('graph-cliques');
+  echo ("<center>Extract al cliques from a graph\n");
    echo ("<br>This program was developed by 
 	  <a target=_blank href=http://www.bigre.ulb.ac.be/Users/sylvain/>Sylvain Broh&eacute;e</a> and
           <a target=_blank href=http://www.bigre.ulb.ac.be/Users/jvanheld/>Jacques van Helden</a>.
+  
           </center>\n");
+   echo ("<b>Warning!</b> This program may be very slow depending on the size of the graph" 
+   );
+
   echo ("<form method='post' action='graph_clique.php' enctype='multipart/form-data'>
   &nbsp;&nbsp;&nbsp;<a href = 'help.graph_clique.html#formats'><B>Input format</B></a>");
   if (!$pipe) {
@@ -50,15 +51,13 @@
     echo ": $graph_format<br>";
     echo "<input type='hidden' NAME='in_format' VALUE='$graph_format'>";
   }
-  echo("&nbsp;&nbsp;&nbsp;<a href = 'help.graph_clique.html#out_format'><B>Output format</B></a>");
-  echo("&nbsp;<select name='stats'>
-    <option selected value = '0'> Classification output
-    <option value = '1'> One line for each seed node (weighted graph only)</select><br><br>
-    ");
+
 
   if (!$pipe) {
     if ($demo) {
-    demo("This demonstration graph consists in the yeast co-immunopreciptation interaction dataset described in <a href = 'http://www.ncbi.nlm.nih.gov/sites/entrez?Db=pubmed&Cmd=ShowDetailView&TermToSearch=16429126&ordinalpos=1&itool=EntrezSystem2.PEntrez.Pubmed.Pubmed_ResultsPanel.Pubmed_RVDocSum'>Gavin et al (2006)</a>. It contains 1430 nodes and 6531 edges. We will look for the cliques of polypeptides in this graph.");    }
+      demo ("This demonstration graph consists in the first published large scale yeast interaction dataset (obtained via two-hybrid) (<a target = '_top' href = 'http://www.ncbi.nlm.nih.gov/sites/entrez?db=pubmed&uid=10688190&cmd=showdetailview&indexed=google'>Uetz et al, 2000)</a>. It contains 865 interactions between 926 proteins.");
+      
+    }
     echo ("<b>Graph</b><br>");
     echo ("<textarea name='graph' rows='6' cols='65'>$demo_graph</textarea>
     <br>Upload graph from file : <br>
@@ -69,6 +68,7 @@
     <tr><td><B><a href = 'help.graph_clique.html#scol'>Source node</a></B></td><td><input type = 'text' name='s_col' value = '$default_scol' size = 1></input></td></tr>
     <tr><td><B><a href = 'help.graph_clique.html#scol'>Target node</a></B></td><td><input type = 'text' name='t_col' value = '$default_tcol' size = 1></input></td></tr>
     </table>");
+    
   } else {
     info_link("Graph uploaded from the previous treatment", rsat_path_to_url($graph_file));
 
