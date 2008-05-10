93671669d9195b9f7a7e553f31f1fe50f6aeb3d6
rsat <rsat>
2008-05-10 00:08:37 +0000

    *** empty log message ***

---
+++ public_html/tutorials/neat_tutorial/internals.pl
@@ -0,0 +1,110 @@
+# LaTeX2HTML 2002-2-1 (1.70)
+# Associate internals original text with physical files.
+
+
+$key = q/cite_VanDongenPHD2000/;
+$ref_files{$key} = "$dir".q|Bibliography.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Jeong2000/;
+$ref_files{$key} = "$dir".q|Bibliography.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Mering2007/;
+$ref_files{$key} = "$dir".q|Bibliography.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_croes06/;
+$ref_files{$key} = "$dir".q|Bibliography.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Sharan2007/;
+$ref_files{$key} = "$dir".q|Bibliography.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Arita2003/;
+$ref_files{$key} = "$dir".q|Bibliography.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_GO2007/;
+$ref_files{$key} = "$dir".q|Bibliography.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Shannon2003/;
+$ref_files{$key} = "$dir".q|Bibliography.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Hu2007/;
+$ref_files{$key} = "$dir".q|Bibliography.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Gavin2006/;
+$ref_files{$key} = "$dir".q|Bibliography.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Pereira-Leal2004/;
+$ref_files{$key} = "$dir".q|Bibliography.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Brohee2006/;
+$ref_files{$key} = "$dir".q|Bibliography.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Leplae2004/;
+$ref_files{$key} = "$dir".q|Bibliography.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Gagneur2003/;
+$ref_files{$key} = "$dir".q|Bibliography.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Lattimore2005/;
+$ref_files{$key} = "$dir".q|Bibliography.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Krogan2006/;
+$ref_files{$key} = "$dir".q|Bibliography.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:hypergeometric_density_cdf/;
+$ref_files{$key} = "$dir".q|Comparisons_between_network.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Kotera2004/;
+$ref_files{$key} = "$dir".q|Bibliography.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Uetz:2000/;
+$ref_files{$key} = "$dir".q|Bibliography.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_croes05/;
+$ref_files{$key} = "$dir".q|Bibliography.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_King2004/;
+$ref_files{$key} = "$dir".q|Bibliography.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Spirin2003/;
+$ref_files{$key} = "$dir".q|Bibliography.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Ito:2001/;
+$ref_files{$key} = "$dir".q|Bibliography.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Mewes2007/;
+$ref_files{$key} = "$dir".q|Bibliography.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:hypergeometric_density/;
+$ref_files{$key} = "$dir".q|Comparisons_between_network.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Enright2002/;
+$ref_files{$key} = "$dir".q|Bibliography.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
