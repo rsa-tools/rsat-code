d1bbc54a85be347ff3c94a67e925004b5477b66a
jvanheld <jvanheld>
2008-06-15 23:10:47 +0000

    ongoing development

--- public_html/lib/footprint.lib.pl
+++ public_html/lib/footprint.lib.pl
@@ -20,7 +20,11 @@ local $all_genes = 0;         ## Analyze all the genes of the query organism
 sub one_command {
   my ($cmd) = @_;
   if ($main::batch) {
-    $main::batch_cmd .= " ; $cmd";
+    if ($main::batch_cmd =~/\S/) {
+      $main::batch_cmd .= " ; $cmd";
+    } else {
+      $main::batch_cmd = "$cmd";
+    }
   } else {
     print $out ("; ", &AlphaDate(), "\n", $cmd, "\n\n") if ($main::verbose >= 1);
     &doit($cmd, $dry, $die_on_error, $main::verbose, $batch, $job_prefix);
