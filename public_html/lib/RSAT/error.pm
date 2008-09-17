ee0d85b14deb2ebc663061bbc40089c005af4814
jvanheld <jvanheld>
2008-09-17 07:28:55 +0000

    *** empty log message ***

--- public_html/lib/RSAT/error.pm
+++ public_html/lib/RSAT/error.pm
@@ -54,12 +54,12 @@ Print a HTML message and die.
 =cut
 
 sub cgiError {
-    my $error_message = join "", @_;
-    my $error_color = "#FF0000";
-    $error_message .= "\n<p>(error occurred on host ".`hostname`.")";
-    RSAT::message::cgiMessage($error_message, "Error", $error_color);
-    print "</body></html>\n";
-    exit(1);
+  my $error_message = join "", @_;
+  my $error_color = "#FF0000";
+  $error_message .= "\n<p>(error occurred on host ".`hostname`.")";
+  RSAT::message::cgiMessage($error_message, "Error", $error_color);
+  print "</body></html>\n";
+  exit(1);
 }
 
 return 1;
