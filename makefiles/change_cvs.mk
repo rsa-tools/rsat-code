55cea8634168b6d0c846a9e3c053bac68b052e7e
jvanheld <jvanheld>
2008-12-30 10:15:51 +0000

    *** empty log message ***

--- makefiles/change_cvs.mk
+++ makefiles/change_cvs.mk
@@ -6,12 +6,12 @@ MAKEFILE=${RSAT}/makefiles/change_cvs.mk
 ## Change the CVS configuration for all directories
 
 OLD_LOGIN=`logname`
-OLD_SERVER=www.scmbb.ulb.ac.be
+OLD_SERVER=cvs.scmbb.ulb.ac.be
 OLD_ROOT=/cvs/rsat
 OLD_CONNECT=${OLD_LOGIN}\@${OLD_SERVER}:${OLD_ROOT}
 
 NEW_LOGIN=`logname`
-NEW_SERVER=cvs.scmbb.ulb.ac.be
+NEW_SERVER=cvs.bigre.ulb.ac.be
 NEW_ROOT=/cvs/rsat
 NEW_CONNECT=${NEW_LOGIN}\@${NEW_SERVER}:${NEW_ROOT}
 
