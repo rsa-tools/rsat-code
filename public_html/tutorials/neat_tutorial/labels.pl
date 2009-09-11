0200e50997d40fb6ad4440f620cc04cbd4d601ea
rsat <rsat>
2009-09-11 20:52:05 +0000

    *** empty log message ***

--- public_html/tutorials/neat_tutorial/labels.pl
+++ public_html/tutorials/neat_tutorial/labels.pl
@@ -1,4 +1,4 @@
-# LaTeX2HTML 2002-2-1 (1.71)
+# LaTeX2HTML 2002-2-1 (1.70)
 # Associate labels original text with physical files.
 
 
@@ -157,7 +157,7 @@ $noresave{$key} = "$nosave";
 1;
 
 
-# LaTeX2HTML 2002-2-1 (1.71)
+# LaTeX2HTML 2002-2-1 (1.70)
 # labels from external_latex_labels array.
 
 
