969db0a4606f282a536adee17a2e55a9830fec73
defrance <defrance>
2010-03-12 17:50:32 +0000

    *** empty log message ***

--- contrib/count-words/count.h
+++ contrib/count-words/count.h
@@ -3,6 +3,6 @@
 
 #include "utils.h"
 
-void count_in_file(FILE *input_fp, FILE *output_fp, int oligo_length, int add_rc, int noov, int grouprc, int argc, char *argv[]);
+void count_in_file(FILE *input_fp, FILE *output_fp, int oligo_length, int spacing, int add_rc, int noov, int grouprc, int argc, char *argv[], int header);
 
 #endif
