e01d2d40fdcb1674b6ca8b155ff84457648733b5
jvanheld <jvanheld>
2010-04-12 08:10:32 +0000

    commands are stored in a file

--- public_html/graph_cluster_membership.php
+++ public_html/graph_cluster_membership.php
@@ -15,6 +15,12 @@
   # log file update
   UpdateLogFile("neat","","");
   title('graph-cluster-membership - results');
+
+  # File to store the commands
+  $cmd_file = getTempFileName('commands_graph-cluster-membership');
+  $cmd_handle = fopen($cmd_file, 'a');
+
+
   # Error status
   $error = 0;
   # Get parameters
@@ -117,15 +123,18 @@
       $soap_error = 1;
     } 
 
-if ($soap_error!=1) {
-    $response =  $echoed->response;
-    $command = $response->command;
-    $server = $response->server;
+    if ($soap_error!=1) {
+      $response =  $echoed->response;
+      $command = $response->command;
+      store_command("$command", "graph-cluster-membership", $cmd_handle);
+      $server_file = $response->server;
     $client_result = $response->client;
-    $server = rtrim ($server);
-    $temp_file = explode('/',$server);
-    $temp_file = end($temp_file);
-    $resultURL = $WWW_RSA."/tmp/".$temp_file;
+    //     $server = rtrim ($server);
+    //     $temp_file = explode('/',$server);
+    //     $temp_file = end($temp_file);
+    $URL['Membership table'] = rsat_path_to_url($server_file);
+
+
     #comment_file 
     $comment_file = $server.".comments";
     $comments = storeFile($comment_file);
@@ -134,7 +143,7 @@ if ($soap_error!=1) {
       echo "<hr>";
     }
     # Text-to-html
-    $file = storeFile($server);
+    $file = storeFile($server_file);
     # echo "</pre>"; 
     #$tth_parameters = array( 
     #  "request" => array(
@@ -153,7 +162,7 @@ if ($soap_error!=1) {
     #$tth_server = rtrim ($tth_server);
    # $tth_temp_file = explode('/',$tth_server);
    # $tth_temp_file = end($tth_temp_file);
-   # $tth_resultURL = $WWW_RSA."/tmp/".$tth_temp_file;
+   # $tth_resultURL = rsat_path_to_url($tth_server);
   
     # Draw heatmap (low resolution)
      echo "</pre>"; 
@@ -171,17 +180,26 @@ if ($soap_error!=1) {
     $ldh_echoed = $client->draw_heatmap($ldh_parameters);
     $ldh_response = $ldh_echoed->response;
     $ldh_command = $ldh_response->command;
+    store_command("$ldh_command", "Low-resoltion map", $cmd_handle);
     $ldh_server = $ldh_response->server;
     $ldh_client = $ldh_response->client;
-    echo "</pre>";
-    $ldh_server = rtrim ($ldh_server);
-    $ldh_temp_file = explode('/',$ldh_server);
-    $ldh_temp_file = end($ldh_temp_file);
-    $ldh_resultURL = $WWW_RSA."/tmp/".$ldh_temp_file;
-    $ldh_resultURL_map = $WWW_RSA."/tmp/".$ldh_temp_file.".html";
+//     $ldh_server = rtrim ($ldh_server);
+//     $ldh_temp_file = explode('/',$ldh_server);
+//     $ldh_temp_file = end($ldh_temp_file);
+    $ldh_resultURL = rsat_path_to_url($ldh_server);
+    $lowres_size = urlfilesize($ldh_resultURL, "mb");
+    $URL['Low-res heat map ('.$lowres_size.' Mb)'] = $ldh_resultURL;
+
+    ////////////////////////////////////////////////////////////////
+    // JvH: I comment the line hereafter because this variable is not
+    // used anywhere, and the html find does not exist. Sylvain or
+    // Gipsi, can you please check if it can be suppressed ?
+    //
+    // $ldh_resultURL_map = rsat_path_to_url($ldh_temp_file.".html";
+    ////////////////////////////////////////////////////////////////
     
     # Draw heatmap (high resolution)
-     echo "</pre>"; 
+    //     echo "</pre>"; 
     $hdh_parameters = array( 
       "request" => array(
        "row_names"=>1,
@@ -193,30 +211,34 @@ if ($soap_error!=1) {
     $hdh_echoed = $client->draw_heatmap($hdh_parameters);
     $hdh_response = $hdh_echoed->response;
     $hdh_command = $hdh_response->command;
+    store_command("$hdh_command", "High-resolution map", $cmd_handle);
     $hdh_server = $hdh_response->server;
     $hdh_client = $hdh_response->client;
-    echo "</pre>";
-    $hdh_server = rtrim ($hdh_server);
-    $hdh_temp_file = explode('/',$hdh_server);
-    $hdh_temp_file = end($hdh_temp_file);
-    $hdh_resultURL = $WWW_RSA."/tmp/".$hdh_temp_file;  
+//     $hdh_server = rtrim ($hdh_server);
+//     $hdh_temp_file = explode('/',$hdh_server);
+//     $hdh_temp_file = end($hdh_temp_file);
+    $hdh_resultURL = rsat_path_to_url($hdh_server);  
+    $highres_size = urlfilesize($hdh_resultURL, "mb");
+    $URL['High-res heat map ('.$highres_size.' Mb)'] = $hdh_resultURL;
   
   
     hourglass("off");
-    $lowres_size = urlfilesize($ldh_resultURL, "mb");
-    $highres_size = urlfilesize($hdh_resultURL, "mb");
-    
+
+    ## Close command handle
+    fclose($cmd_handle);
+    $URL['Server commands'] = rsat_path_to_url($cmd_file);
     
     # Display the results
-    echo "The membership table (text file) is available at the following URL";
-    echo "<a href = '$resultURL'>$resultURL</a><br>";
-    #echo "The results are available as HTML page at the following URL ";
-    #echo "<a href = '$tth_resultURL'>$tth_resultURL</a><br>"; 
-    echo "<hr>";
+    print_url_table($URL);
+    //   echo "The membership table (text file) is available at the following URL";
+    //    echo "<a href = '$resultURL'>$resultURL</a><br>";
+    //    echo "The results are available as HTML page at the following URL ";
+    //    echo "<a href = '$tth_resultURL'>$tth_resultURL</a><br>"; 
+    //    echo "<hr>";
     info ("Depending on the web browser, and of the number of clusters and nodes, images might not be viewable as the dimension of the figure are too large. In this case, right click (save as) and open it with an other visualization tool");
-    echo "The heat map is available at the folllowing URL<br>";
-    echo "&nbsp;&nbsp;low resolution ($lowres_size Mb) <a href = '$ldh_resultURL'>$ldh_resultURL</a>.<br>"; 
-    echo "&nbsp;&nbsp;high resolution ($highres_size Mb) <a href = '$hdh_resultURL'>$hdh_resultURL</a>.<br>"; 
+    //    echo "The heat map is available at the folllowing URL<br>";
+    //    echo "&nbsp;&nbsp;low resolution ($lowres_size Mb) <a href = '$ldh_resultURL'>$ldh_resultURL</a>.<br>"; 
+    //    echo "&nbsp;&nbsp;high resolution ($highres_size Mb) <a href = '$hdh_resultURL'>$hdh_resultURL</a>.<br>"; 
     echo "<hr>\n";
  
   	}
