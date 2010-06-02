76ad4bb063b93ae2a95a9f99330cb07f6a347157
jvanheld <jvanheld>
2010-06-02 00:18:09 +0000

    *** empty log message ***

--- public_html/toc_graph.php
+++ public_html/toc_graph.php
@@ -95,7 +95,7 @@ onclick='toggleMenu('206')' id='heading206'>Data</div>
 <div class='menu_heading_open'
 onclick='toggleMenu('206')' id='heading206'>Graphics</div>
 <div id='menu206' >
-	<a class='menu_item' href='draw_heatmap_form.php' target='tools'>Generate a heatmap</a>
+	<a class='menu_item' href='draw_heatmap_form.php' target='tools'>Draw heatmap</a>
 </div>
 </div>
 
