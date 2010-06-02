6beb695ff38cac26fbc3f0176ac22e19054fc7b8
rsat <rsat>
2010-06-02 10:34:00 +0000

    fixed a bug with hard-coded IP adress

--- public_html/mcl.php
+++ public_html/mcl.php
@@ -67,9 +67,7 @@
      hourglass("on");
     # Open the SOAP client
     $client = new SoapClient(
-//                        $neat_wsdl,
-                       "http://rsat.ulb.ac.be/rsat/web_services/RSATWS.wsdl",
-
+                            $neat_wsdl,
                            array(
                                  'trace' => 1,
                                  'soap_version' => SOAP_1_1,
