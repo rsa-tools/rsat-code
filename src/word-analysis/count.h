c942198688be289581ce33aad5a31006aa6a8d74
defrance <defrance>
2010-06-08 10:37:54 +0000

    *** empty log message ***

--- src/word-analysis/count.h
+++ src/word-analysis/count.h
@@ -11,5 +11,15 @@
 
 #include "utils.h"
 #include "fasta.h"
+#include "markov.h"
+
+
+// count oligo occurrences in given sequence
+// last_position: used by noov to store last occurrence 
+// l: total oligo length
+// sp: spacing (> 0 for dyads)
+// rc: use the 2 strands of seq
+// noov: discard overlapping occurrences
+void count_occ(long *count_table, long *last_position, int l, int sp, seq_t *seq, int rc, int noov);
 
 #endif
