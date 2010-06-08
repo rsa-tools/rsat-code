355bb7618d6494ca9e442c5b3a9fffdbfb869b42
rsat <rsat>
2010-06-08 13:34:40 +0000

    *** empty log message ***

--- makefiles/change_cvs.mk
+++ makefiles/change_cvs.mk
@@ -1,16 +1,14 @@
-
-include ${RSAT}/makefiles/util.mk
-MAKEFILE=${RSAT}/makefiles/change_cvs.mk
-
 ################################################################
 ## Change the CVS configuration for all directories
 
-OLD_LOGIN=`logname`
-OLD_SERVER=www.bigre.ulb.ac.be
+include ${RSAT}/makefiles/util.mk
+MAKEFILE=${RSAT}/makefiles/change_cvs.mk
+OLD_LOGIN=jvanheld
+OLD_SERVER=cvs.bigre.ulb.ac.be
 OLD_ROOT=/cvs/rsat
 OLD_CONNECT=${OLD_LOGIN}\@${OLD_SERVER}:${OLD_ROOT}
 
-NEW_LOGIN=`logname`
+NEW_LOGIN=rsat
 NEW_SERVER=cvs.bigre.ulb.ac.be
 NEW_ROOT=/cvs/rsat
 NEW_CONNECT=${NEW_LOGIN}\@${NEW_SERVER}:${NEW_ROOT}
