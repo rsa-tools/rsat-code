f87bad9d30bba3d8850ca1bf506fbd5bccc0f601
rsat <rsat>
2010-06-10 09:29:33 +0000

    new version of php STRING downloader

--- public_html/string_dataset.php
+++ public_html/string_dataset.php
@@ -21,6 +21,7 @@
   # Get parameters
   $now = date("Ymd_His");
   $gene_list = $_REQUEST['genes'];
+  print_r($_REQUEST);
   $gene_list = explode("-sep-", $gene_list);
   $genes = array();
   for ($i = 0; $i <= count($gene_list); $i++) {
