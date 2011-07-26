2faba605972f5af38ace77b90b3e24a40fb4bba7
rsat <rsat>
2011-07-26 11:47:08 +0000

    *** empty log message ***

--- makefiles/intron_signals.mk
+++ makefiles/intron_signals.mk
@@ -7,9 +7,23 @@ include ${RSAT}/makefiles/util.mk
 MAKEFILE=makefile
 
 SEQ_TYPE=firstintron
-ORG=Homo_sapiens_EnsEMBL
+ORG=Homo_sapiens
+
+################################################################
+## Get all intron sequences from Ensembl.  Beware, this can take several hours
+## because sequences have to be fetched one by one.
 SEQ_DIR=data/sequences
-SEQ=${SEQ_DIR}/all_${SEQ_TYPE}_${ORG}
+SEQ=${SEQ_DIR}/all_${SEQ_TYPE}_${ORG}${RM}
+RM=-rm
+get_sequences:
+	@echo
+	@echo "Retrieving all sequences from Ensembl"
+	@mkdir -p ${SEQ_DIR}
+	retrieve-ensembl-seq.pl -v 2 \
+		-org ${ORG} \
+		-all -feattype intron -firstintron \
+		-mask coding ${RM} \
+		-o ${SEQ}.fasta
 
 ################################################################
 ## Compute sequence length distributions
@@ -18,8 +32,6 @@ seq_len_distrib:
 		| classfreq -v -col 2 -ci 100 \
 		-o ${SEQ}_len_distrib.tab
 	@echo ${SEQ}_len_distrib.tab
-
-it:
 	XYgraph -i ${SEQ}_len_distrib.tab \
 		-xcol 3 -ycol 7,8,9 \
 		-xsize 800 -ysize 400 \
@@ -37,7 +49,7 @@ FROM=-200
 CI=1
 TO=-1
 OL=3
-POS_DIR=results/${SEQ_TYPE}/${OL}nt
+POS_DIR=results/${ORG}/${SEQ_TYPE}/${OL}nt
 POS=${POS_DIR}/${OL}nt_ci${CI}_all_${SEQ_TYPE}_${ORI}_from${FROM}_to${TO}
 POS_CMD=sub-sequence -i ${SEQ}.fasta  -origin ${ORI} \
 		-from ${FROM} -to ${TO} \
