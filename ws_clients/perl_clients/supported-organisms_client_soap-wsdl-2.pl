ade32e085afeec6c7b41e0a93a34c4dfbbfb9ff3
jvanheld <jvanheld>
2012-01-30 15:38:43 +0000

    fixed inconsistencies in the output

--- ws_clients/perl_clients/supported-organisms_client_soap-wsdl-2.pl
+++ ws_clients/perl_clients/supported-organisms_client_soap-wsdl-2.pl
@@ -17,7 +17,12 @@ use MyInterfaces::RSATWebServices::RSATWSPortType;
 warn "\nThis demo script gets the list of supported organisms from the remote RSAT server\n\n";
 
 ## WSDL location
-my $server = $ARGV[0] || 'http://rsat.ulb.ac.be/rsat/web_services';
+##
+## Actually, the server location is not affected by the variable
+## $server, it depends on the stubb, which has to be configured
+## separately (comment by JvH, 2012-01-30) -> I suppress this misleading message
+##
+#my $server = $ARGV[0] || 'http://rsat.ulb.ac.be/rsat/web_services';
 
 ## Service call
 my $soap=MyInterfaces::RSATWebServices::RSATWSPortType->new();
@@ -34,30 +39,33 @@ my %args = (
 	    );
 
 ## Send the request to the server
-print "Sending request to the server $server\n";
+warn "Sending request to the server\n";
+warn "Taxon\t", $taxon, "\n";
 my $som = $soap->supported_organisms({'request' => \%args});
 
 ## Get the result
 unless ($som) {
-	printf "A fault (%s) occured: %s\n", $som->get_faultcode(), $som->get_faultstring();
+  die (printf "A fault (%s) occured: %s\n", $som->get_faultcode(), $som->get_faultstring());
 } else {
-	my $results = $som->get_response();
+  my $results = $som->get_response();
 
-    ## Report the remote command
-    my $command = $results -> get_command();
-    print "Command used on the server: ".$command, "\n";
+  ## Report the remote command
+  my $command = $results -> get_command();
+  warn "Command used on the server: ".$command, "\n";
 
-    ## Report the result
-    if ($output_choice eq 'server') {
- 		my $server_file = $results -> get_server();
-		print "Result file on the server: ".$server_file;
-   } elsif ($output_choice eq 'client') {
-		my $result = $results -> get_client();
-		print "Retrieved sequence(s): \n".$result;
-   } elsif ($output_choice eq 'both') {
-		my $server_file = $results -> get_server();
-		my $result = $results -> get_client();
-		print "Result file on the server: ".$server_file."\n";
-		print "Retrieved sequence(s): \n".$result;
-    }
+  ## Report the result
+  if ($output_choice eq 'server') {
+    my $server_file = $results -> get_server();
+    print "Result file on the server: ".$server_file;
+  } elsif ($output_choice eq 'client') {
+    my $result = $results -> get_client();
+    warn "Result: \n";
+    print $result;
+  } elsif ($output_choice eq 'both') {
+    my $server_file = $results -> get_server();
+    my $result = $results -> get_client();
+    warn "Result file on the server: ".$server_file."\n";
+    warn "Result: \n";
+    print $result;
+  }
 }
