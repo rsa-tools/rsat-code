5d1d0dfc8a60e45f8dd7e80f46cad90e0f3c298b
rsat <rsat>
2012-03-21 11:45:37 +0000

    *** empty log message ***

--- ws_clients/perl_clients/pathway-extractor_soapclient.pl
+++ ws_clients/perl_clients/pathway-extractor_soapclient.pl
@@ -28,6 +28,6 @@
     -> proxy('http://mamaze.ulb.ac.be/rsat/web_services/RSATWM.cgi',timeout=>1000);
 
   eval {  
-    my $results=  $soap->infer_pathway("NP_416523.1\tNP_416524.1\tNP_416525.1\tNP_416526.4\tNP_416527.1\tNP_416528.2\tNP_416529.1\tNP_416530.1","Escherichia_coli_strain_K12-83333-Uniprot","MetaCyc_v141"); 
+    my $results=  $soap->infer_pathway("NP_416523.1\tNP_416524.1\tNP_416525.1\tNP_416526.4\tNP_416527.1\tNP_416528.2\tNP_416529.1\tNP_416530.1","Escherichia_coli_strain_K12-83333-Uniprot","MetaCyc_v141_directed"); 
     print "Results:" . $results->result ."\n"; 
   1 } or die;
