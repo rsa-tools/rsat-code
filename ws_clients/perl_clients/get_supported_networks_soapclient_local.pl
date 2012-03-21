dc9af07740b8da4c7dfdaf08db58c60f39803330
rsat <rsat>
2012-03-21 11:34:51 +0000

    *** empty log message ***

--- ws_clients/perl_clients/get_supported_networks_soapclient_local.pl
+++ ws_clients/perl_clients/get_supported_networks_soapclient_local.pl
@@ -11,7 +11,7 @@
 # #  my $results = SOAP->inferpathway();
 # my $results = SOAP->hi();
 #   print "return :".$results."\n";
-
+#   use SOAP::SOM; 
   use SOAP::Lite 
     on_action => sub {
        sprintf '%s#%s',@_
@@ -24,10 +24,14 @@
     };
     
   my $soap = SOAP::Lite
-    -> uri('http://mamaze.ulb.ac.be/PathwayExtractor_WS')
-    -> proxy('http://mamaze.ulb.ac.be/rsat/web_services/RSATWM.cgi',timeout=>1000);
+    -> uri('http://localhost/PathwayExtractor_WS')
+    -> proxy('http://localhost/rsat/web_services/RSATWM.cgi');
 
-  eval {  
-    my $results=  $soap->infer_pathway("NP_416523.1\tNP_416524.1\tNP_416525.1\tNP_416526.4\tNP_416527.1\tNP_416528.2\tNP_416529.1\tNP_416530.1","Escherichia_coli_strain_K12-83333-Uniprot","MetaCyc_v141"); 
-    print "Results:" . $results->result ."\n"; 
-  1 } or die;
+   eval {  
+    my $results=  $soap->get_supported_networks(); 
+    my @responses = $results->paramsout;
+    print "Results:" .$results->result."\n";
+    print "Results:" . join (',',@responses) ."\n";
+  1 
+   } or die
+  ;
