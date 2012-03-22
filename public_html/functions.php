c7100d8a9fa18c86b98e4b5d0d9cdb89346526ab
jvanheld <jvanheld>
2012-03-22 16:28:36 +0000

    suppressed useless php tags between successive functions

--- public_html/functions.php
+++ public_html/functions.php
@@ -1,59 +1,50 @@
 <?php
   // NeAT TITLE
-  Function title($title) {
-    echo "<H3><a href='NeAT_home.html'>NeAT</a> - $title</H3>\n";
+Function title($title) {
+  echo "<H3><a href='NeAT_home.html'>NeAT</a> - $title</H3>\n";
   }
-?>
-
-<?php
-  // NeAT ERROR
-  Function error($error) {
-    echo "<H4>Error: </H4><blockquote class='error'>$error</blockquote></h4><br>";
-  }
-?>
-
-<?php
-  // NeAT WARNING
-  Function warning($warning) {
-    echo "<H4>Warning: </H4><blockquote class ='warning'>$warning</blockquote><br>";
-  }
-?>
 
 
-<?php
-  // NeAT WARNING
-  Function demo($demo) {
-    echo "<H4>Comment on the demonstration example: </H4><blockquote class ='demo'>$demo</blockquote><hr>";
-  }
-?>
+// NeAT ERROR
+Function error($error) {
+  echo "<H4>Error: </H4><blockquote class='error'>$error</blockquote></h4><br>";
+}
 
-<?php
-  // NeAT INFO
+
+// NeAT WARNING
+Function warning($warning) {
+  echo "<H4>Warning: </H4><blockquote class ='warning'>$warning</blockquote><br>";
+}
+
+
+// NeAT WARNING
+Function demo($demo) {
+  echo "<H4>Comment on the demonstration example: </H4><blockquote class ='demo'>$demo</blockquote><hr>";
+}
+
+
+// NeAT INFO
 Function info($info) {
   echo "<h2>Info</h2><class='information'><font color='#00BB00'>$info</font></class><br>";
   //    echo "<h2>Info</h2><blockquote class='information'><font color='#00BB00'>$info</font></blockquote><br>";
-  }
-?>
-
-<?php
-  // NeAT INFO LINK
-  // This function adds an hypertext link
-  Function info_link($info, $link_url) {
-    echo "<H4>Info: </H4><blockquote class = 'info'><a href = '$link_url'>$info </a></blockquote></a><br>";
-  }
-?>
+}
 
 
-<?php
-  Function uploadFile($file) {
-//     $tmpDir = "tmp/";
-//     $tmpFile = $_FILES[$file]["name"];
-//     $now = date("Ymd_His");
-//     $tmpFile = $tmpFile.$now;
-//     $tmpFileName = $tmpDir.$tmpFile;
-   $tmpFileName = getTempFileName('upload');
-//   echo ("TEMP $tmpFileName");
+// NeAT INFO LINK
+// This function adds an hypertext link
+Function info_link($info, $link_url) {
+  echo "<H4>Info: </H4><blockquote class = 'info'><a href = '$link_url'>$info </a></blockquote></a><br>";
+}
 
+
+Function uploadFile($file) {
+  //     $tmpDir = "tmp/";
+  //     $tmpFile = $_FILES[$file]["name"];
+  //     $now = date("Ymd_His");
+  //     $tmpFile = $tmpFile.$now;
+  //     $tmpFileName = $tmpDir.$tmpFile;
+  $tmpFileName = getTempFileName('upload');
+  //   echo ("TEMP $tmpFileName");
   if (is_uploaded_file($_FILES[$file]['tmp_name'])) {
     if (rename($_FILES[$file]['tmp_name'], $tmpFileName)) {
       //             echo "File ".$_FILES[$file]['tmp_name']." was moved to  $tmpDir/$tmpFile <br>";
@@ -62,75 +53,62 @@ Function info($info) {
     }
   } else {
     echo "File ".$_FILES[$file]['tmp_name']." could not be uploaded<br>";
-//     print_r($_FILES[$file]);
+    //     print_r($_FILES[$file]);
   }
   return $tmpFileName;
-  }
-?>
+}
 
 
-<?php
 Function getTempFileName($prefix) {
-    $tmpDir = "tmp/";
-    $tmpFile = $_FILES[$file]["name"];
-    $tmpFile = $tmpFile.$prefix;
-    $randval = randchar(3);  
-    $now = date("Ymd_His");
-    $tmpFile = $tmpFile.'_'.$randval."_".$now;
-#    $unique = mktemp('XXXXX');
-    return $tmpDir.$tmpFile;
+  $tmpDir = "tmp/";
+  $tmpFile = $_FILES[$file]["name"];
+  $tmpFile = $tmpFile.$prefix;
+  $randval = randchar(3);  
+  $now = date("Ymd_His");
+  $tmpFile = $tmpFile.'_'.$randval."_".$now;
+  #    $unique = mktemp('XXXXX');
+  return $tmpDir.$tmpFile;
+}
+
+
+Function storeFile($file) {
+  $fh = fopen($file, 'r');
+  $theData = "";
+  if (filesize($file) > 0) {
+    $theData = fread($fh, filesize($file));
+    fclose($fh);
   }
-?>
+  return $theData;
+}
 
 
-
-<?php
-    Function storeFile($file) {
-
-      $fh = fopen($file, 'r');
-      $theData = "";
-      if (filesize($file) > 0) {
-        $theData = fread($fh, filesize($file));
-        fclose($fh);
-      }
-      return $theData;
-  }
-?>
-
-<?php
-    Function writeTempFile($prefix, $data) {
-      $temp_command = "mktemp tmp/$prefix.XXXXX";
-      exec($temp_command, $temp_file);
-
-      $temp_file = $temp_file[0];
-      $temp_file = trim_text($temp_file);
-      $temp_file = trim($temp_file);
-      $fh = fopen($temp_file, 'w');
-      fwrite($fh, $data);
-      fclose($fh);
-      return $temp_file;
-  }
-?>
+Function writeTempFile($prefix, $data) {
+  $temp_command = "mktemp tmp/$prefix.XXXXX";
+  exec($temp_command, $temp_file);
+  $temp_file = $temp_file[0];
+  $temp_file = trim_text($temp_file);
+  $temp_file = trim($temp_file);
+  $fh = fopen($temp_file, 'w');
+  fwrite($fh, $data);
+  fclose($fh);
+  return $temp_file;
+}
 
 
-<?php
 /**
  * Strip special invisible characters
  */
 Function trim_text($text) {
-   $trimmed_text = "";
-   $lines = explode("\n",$text);
-   $array_count = count($lines);
-   for($y=0; $y<$array_count; $y++) {
-     $trimmed_text .= rtrim($lines[$y])."\n";
-   }
-   return $trimmed_text;
+  $trimmed_text = "";
+  $lines = explode("\n",$text);
+  $array_count = count($lines);
+  for($y=0; $y<$array_count; $y++) {
+    $trimmed_text .= rtrim($lines[$y])."\n";
+  }
+  return $trimmed_text;
 }
-?>
 
 
-
-<?php
 /**
  * Echo a command by suppress the full path to rsa-tools.
  */
@@ -142,26 +120,22 @@ Function store_command($command, $name, $cmd_handle) {
   fwrite($cmd_handle, "# ".$name."\n");
   fwrite($cmd_handle, $clean_command."\n\n");
 }
-?>
 
 
-
-<?php
 /**
  * Print a table with URLs to the result files
  */
 Function print_url_table($URL) {
-    echo "<table class=\"resultlink\">\n";
-    echo "<tr><th colspan='2'>Result file(s)</th></tr>\n";
-    foreach ($URL as $key => $value) {
-      echo "<tr><td>",$key,"</td><td><a href = '",$value,"'>",$value,"</a></td></tr>\n"; 
-    }
-    echo "</table>\n";
-    echo"<hr>\n";
+  echo "<table class=\"resultlink\">\n";
+  echo "<tr><th colspan='2'>Result file(s)</th></tr>\n";
+  foreach ($URL as $key => $value) {
+    echo "<tr><td>",$key,"</td><td><a href = '",$value,"'>",$value,"</a></td></tr>\n"; 
+  }
+  echo "</table>\n";
+  echo"<hr>\n";
 }
-?>
 
-<?php
+
 /**
  * Read a property file $props and return a hash
  */
@@ -171,54 +145,50 @@ Function load_props($props) {
   $lines = explode("\n",$properties);
   $array_count = count($lines);
   for($y=0; $y<$array_count; $y++) {
-   $line = trim($lines[$y]);
-   if (!preg_match("/^\#/", $line)) {
-     $property = explode('=', $line);
-     if (isset($property[1])) {
-       $prop_array[$property[0]] = $property[1];
-     }
-   }
+    $line = trim($lines[$y]);
+    if (!preg_match("/^\#/", $line)) {
+      $property = explode('=', $line);
+      if (isset($property[1])) {
+	$prop_array[$property[0]] = $property[1];
+      }
+    }
   }
   return $prop_array;
 }
-?>
 
 
-<?php
-  # SET OF OPERATION DONE WHEN LOADING EACH PHP PAGE
-  $rsat_main = getcwd()."/..";
-  $rsat_logs = $rsat_main."/public_html/logs";
+# SET OF OPERATION DONE WHEN LOADING EACH PHP PAGE
+$rsat_main = getcwd()."/..";
+$rsat_logs = $rsat_main."/public_html/logs";
+# LOAD PROPERTIES
+$properties = load_props($rsat_main."/RSAT_config.props");
+$tmp = $properties['rsat_tmp'];
+$WWW_RSA = $properties['rsat_www'];
+$log_name = $properties['rsat_site'];
+date_default_timezone_set("Europe/Paris");
+$neat_wsdl = $properties['neat_ws'];
+$neat_www_root = $properties['neat_www_root'];
+# Karoline: property neat_java_host not required for me, just need the host name here
+# in future: obtain host from url address
+# $scheme = parse_url($WWW_RSA,PHP_URL_SCHEME);
+# $host = parse_url($WWW_RSA,PHP_URL_HOST);
+# $neat_java_host = $scheme."://".$host;
+# for the moment: java tools run only on ulb host
+$neat_java_host = $properties['neat_java_host'];
+# host may include tomcat port
+$tomcat_port = $properties['tomcat_port'];
+if(strcmp($tomcat_port,"") != 0){
+  $neat_java_host = $neat_java_host . ":" . $tomcat_port;
+ }
+$neat_java_wsdl = $neat_java_host . "/be.ac.ulb.bigre.graphtools.server/wsdl/GraphAlgorithms.wsdl";
+$neat_java_remote_wsdl = $neat_java_host . "/be.ac.ulb.bigre.graphtools.server/wsdl/GraphAlgorithmsNeAT.wsdl";
+# LOG
+$year = date("Y");
+$month = date("m");
+$neat_log_file = sprintf ("$rsat_logs/log-file_$log_name"."_neat_%04d_%02d", $year,$month);
+$rsat_log_file = sprintf ("$rsat_logs/log-file_$log_name"."_%04d_%02d", $year,$month);
 
-  # LOAD PROPERTIES
-  $properties = load_props($rsat_main."/RSAT_config.props");
-  $tmp = $properties['rsat_tmp'];
-  $WWW_RSA = $properties['rsat_www'];
-  $log_name = $properties['rsat_site'];
-  date_default_timezone_set("Europe/Paris");
-  $neat_wsdl = $properties['neat_ws'];
-  $neat_www_root = $properties['neat_www_root'];
-  # Karoline: property neat_java_host not required for me, just need the host name here
-  # in future: obtain host from url address
-  # $scheme = parse_url($WWW_RSA,PHP_URL_SCHEME);
-  # $host = parse_url($WWW_RSA,PHP_URL_HOST);
-  # $neat_java_host = $scheme."://".$host;
-  # for the moment: java tools run only on ulb host
-  $neat_java_host = $properties['neat_java_host'];
-  # host may include tomcat port
-  $tomcat_port = $properties['tomcat_port'];
-  if(strcmp($tomcat_port,"") != 0){
-  	  $neat_java_host = $neat_java_host . ":" . $tomcat_port;
-  }
-  $neat_java_wsdl = $neat_java_host . "/be.ac.ulb.bigre.graphtools.server/wsdl/GraphAlgorithms.wsdl";
-  $neat_java_remote_wsdl = $neat_java_host . "/be.ac.ulb.bigre.graphtools.server/wsdl/GraphAlgorithmsNeAT.wsdl";
-  # LOG
-  $year = date("Y");
-  $month = date("m");
-  $neat_log_file = sprintf ("$rsat_logs/log-file_$log_name"."_neat_%04d_%02d", $year,$month);
-  $rsat_log_file = sprintf ("$rsat_logs/log-file_$log_name"."_%04d_%02d", $year,$month);
-?>
 
-<?php
 ## This function returns the name of the script executing it
 Function getCurrentScriptName() {
   $currentFile = $_SERVER["SCRIPT_NAME"];
@@ -226,8 +196,8 @@ Function getCurrentScriptName() {
   $currentFile = $parts[count($parts) - 1];
   return $currentFile;
 }
-?>
-<?php
+
+
 ## This function replaces all spaces of a string by tabulation
 ## If the line starts with a ';' or a '#' it is skipped.
 Function space_to_tab($string) {
@@ -245,8 +215,8 @@ Function space_to_tab($string) {
   }
   return $result;
 }
-?>
-<?php
+
+
 ## This function replaces the given number of spaces in a row inside a string by tabulation
 ## If the line starts with a ';' or a '#' it is skipped.
 ## This function allows to handle input where identifiers may contain less than the given
@@ -255,7 +225,7 @@ Function spaces_to_tab($string, $num) {
   $result = "";
   $replace = "";
   for($i=0; $i<$num; $i++){
-	$replace = $replace." ";
+    $replace = $replace." ";
   }
   $lines = explode("\n",$string);
   $array_count = count($lines);
@@ -271,41 +241,36 @@ Function spaces_to_tab($string, $num) {
   return $result;
 }
 
-?>
 
-<?php
 # This function converts a file name from its complete path
 # its URL on the RSAT webserver
 # For example: /home/rsat/rsa-tool/public_html/tmp/brol.truc
 # will be converted to
 # http://rsat.ulb.ac.be/rsat/tmp/brol.truc
 Function rsat_path_to_url ($file_name) {
-    global $WWW_RSA;
-    $temp_file = rtrim($file_name);
-    $temp_file = explode('/',$temp_file);
-    $temp_file = end($temp_file);
-    $resultURL = $WWW_RSA."/tmp/".$temp_file;
-    return $resultURL;
+  global $WWW_RSA;
+  $temp_file = rtrim($file_name);
+  $temp_file = explode('/',$temp_file);
+  $temp_file = end($temp_file);
+  $resultURL = $WWW_RSA."/tmp/".$temp_file;
+  return $resultURL;
 }
 
-?>
 
-
-<?php
 ## This function returns the name of the script executing it
 Function AlphaDate() {
   $my_date = exec("date +%Y_%m_%d.%H%M%S", $my_date);
   trim($my_date);
   return $my_date;
 }
-?>
-<?php
+
+
 ## This function returns the name of the script executing it
 Function check_integer($string) {
   return (preg_match("/[0-9]*/", $string));
 }
-?>
-<?php
+
+
 ################################################################
 ### store info into a log file in a conveninent way for
 ### subsequent login statistics
@@ -315,19 +280,18 @@ Function check_integer($string) {
 ###     UpdateLogFile($script_name, $message);
 ### If script name is empty or null... the program determine
 ### the name of the file
-
 Function UpdateLogFile($suite ,$script_name, $message) {
   if ($script_name == "") {
     $script_name = getCurrentScriptName();
   }
-  # LOAD GLOBAL VARIABLES
+# LOAD GLOBAL VARIABLES
   global $log_file, $log_name, $rsat_log_file, $neat_log_file;
   if ($suite == "rsat") {
     $log_file = $rsat_log_file;
   } else {
     $log_file = $neat_log_file;
   }
-  # LOAD OTHER VARIABLES
+# LOAD OTHER VARIABLES
   $my_date = AlphaDate();
   $user = getenv('REMOTE_USER');
   $address = getenv('REMOTE_ADDR');
@@ -335,16 +299,15 @@ Function UpdateLogFile($suite ,$script_name, $message) {
   $e_mail = "";
   $user_address_at_host = $user."@".$address." (".$host.")";
   $to_write = $my_date."\t".$log_name."\t".$user_address_at_host."\t".$script_name."\t".$e_mail."\t".$message."\n";
-  # Write to the file
+# Write to the file
   $log_handle = fopen($log_file, 'a');
-
   fwrite($log_handle, $to_write);
   fclose($log_handle);
   chmod ($log_file, 0777);
 }
-?>
-<?php
-  Function checkNeatTutorial($tutorial_url) {
+
+
+Function checkNeatTutorial($tutorial_url) {
   global $WWW_RSA;
   $pdf_tutorial = $WWW_RSA.'/tutorials/neat_tutorial.pdf';
   $address = $pdf_tutorial;
@@ -354,60 +317,55 @@ Function UpdateLogFile($suite ,$script_name, $message) {
   return $address;
 }
 
-?>
-<?php
-  Function HourglasOn() {
+
+Function HourglasOn() {
   echo("<div id='hourglass' class='hourglass'><img src='images/animated_hourglass.gif' height='50' border='1'></div>");
 }
 
-?>
-<?php
-  Function hourglass($status) {
+
+Function hourglass($status) {
   if ($status == "on") {
     echo("<div id='hourglass' class='hourglass'><img src='images/animated_hourglass.gif' height='50' border='1'></div>");
   } else {
     echo("<div id='hide' class='hide'><img src='images/hide_hourglass.jpg' height='60' border='0'></div>");
   }
 }
-?>
 
-<?php
-  function urlfilesize($url,$thereturn) {
-    if (substr($url,0,4)=='http') {
-      $x = array_change_key_case(get_headers($url, 1),CASE_LOWER);
-      $x = $x['content-length'];
-    }
-    else {
-      $x = @filesize($url); }
-      if (!$thereturn) {
-        return $x ;
-      }
-      elseif($thereturn == 'mb') {
-        return round($x / (1024*1024),2) ;
-      }
-      elseif($thereturn == 'kb') {
-        return round($x / (1024),2) ;
-      }
-  }
 
-?>
-<?php
-  function randchar($length) {
-    $result = "";
-    $abc= array("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
-    for ($i = 0; $i < $length; $i++) {
-      $result .= $abc[rand(0,(count($abc)-1))];
-    }
-    return $result;
+function urlfilesize($url,$thereturn) {
+  if (substr($url,0,4)=='http') {
+    $x = array_change_key_case(get_headers($url, 1),CASE_LOWER);
+    $x = $x['content-length'];
   }
+  else {
+    $x = @filesize($url); }
+  if (!$thereturn) {
+    return $x ;
+  }
+  elseif($thereturn == 'mb') {
+    return round($x / (1024*1024),2) ;
+  }
+  elseif($thereturn == 'kb') {
+    return round($x / (1024),2) ;
+  }
+}
+
+
+function randchar($length) {
+  $result = "";
+  $abc= array("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
+  for ($i = 0; $i < $length; $i++) {
+    $result .= $abc[rand(0,(count($abc)-1))];
+  }
+  return $result;
+}
 ?> 
-
 <?php
-  ini_set('max_execution_time', 2400);
-  ini_set('max_input_time', 6000);
-  ini_set('memory_limit', "100M");
-  ini_set('display_errors', 'on');
-  ini_set('post_max_size', 80000000);
-  ini_set('error_reporting', "E_ALL & ~E_NOTICE");
-  ini_set('upload_max_filesize', 100000000);
+ini_set('max_execution_time', 2400);
+ini_set('max_input_time', 6000);
+ini_set('memory_limit', "100M");
+ini_set('display_errors', 'on');
+ini_set('post_max_size', 80000000);
+ini_set('error_reporting', "E_ALL & ~E_NOTICE");
+ini_set('upload_max_filesize', 100000000);
 ?>
