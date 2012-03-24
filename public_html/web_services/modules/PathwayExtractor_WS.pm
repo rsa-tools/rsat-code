dec044bac3169e1655e215fc1984b8dd6fe67eee
rsat <rsat>
2012-03-24 18:31:15 +0000

    Now parameters are check for supported organism and network. if incorrect an error message is sent back
    The stdERR is stored in a log file and zipped with the resutls

--- public_html/web_services/modules/PathwayExtractor_WS.pm
+++ public_html/web_services/modules/PathwayExtractor_WS.pm
@@ -18,6 +18,14 @@ use File::Basename;
 use File::Temp qw/tempfile/;
 use RSAT::PathwayExtraction;
 use Cwd 'abs_path';
+#  my $stderr = `$command 2>&1 1>/dev/null`;
+#     $stderr = &error_handling($stderr, 1);
+# 
+#     if ($stderr =~ /Warning/) {
+# 	die SOAP::Fault -> faultcode('Server.ExecError') -> faultstring("Execution error: $stderr\ncommand: $command");
+#     }
+   
+
 
 # use Pathwayinference;
 
@@ -33,41 +41,55 @@ use Cwd 'abs_path';
   ## Web service for the pathway inference tool
   ##########################################
   sub infer_pathway {
+  
+  
 
 #   return "RSATPATH: $ENV{RSAT}\nREA_ROOT: $ENV{REA_ROOT}\nKWALKS_ROOT: $ENV{KWALKS_ROOT}\nCLASSPATH:  $ENV{CLASSPATH}\n";
     my ($class, $seeds,$organism,$network,$directed) = @_;
+    
     my $basedir="$ENV{RSAT}/public_html/data/metabolic_networks";
     my $GERdir = "$basedir/GER_files/$organism";
     my $outputdir = "$ENV{RSAT}/public_html/tmp/";
     my $outputurl = $ENV{rsat_ws_tmp};
-    open(STDERR,">","$outputdir/stdERR.log");
+    
+         # build random id for file uniqueness
+      my $filename;
+      my (undef, $file) =  File::Temp::tempfile('XXXXXXXXX', OPEN=>0);
+      $file = "PathwayExtractorWS$file";#fileprefix
+ 
+    
+    open(STDERR,">","$outputdir/$file.log");
     
     # get directory from network
     my $neworkfilepattern= $network;
     $neworkfilepattern =~ s/_v.*//gi; 
     $neworkfilepattern = "$basedir/networks/$neworkfilepattern/metab_$network";
+    my $networknodenames = "$neworkfilepattern"."_node_names.tab";
+    my $networkfile = "$neworkfilepattern"."_network.tab";
+    
     print STDERR "networkfilepattern: $neworkfilepattern\n";
+    
+    # check parameters
+    die SOAP::Fault -> faultcode('Server.ExecError') -> faultstring("Execution error: $basedir\n does not exits!") unless -e $basedir;
+    die SOAP::Fault -> faultcode('Server.ExecError') -> faultstring("Execution error: Organism $organism not supported!") unless -e $GERdir;
+    die SOAP::Fault -> faultcode('Server.ExecError') -> faultstring("Execution error: Network $network not supported!") unless -e $networkfile;
+    
+     $neworkfilepattern .= "/metab_$network";
+
     # workaround try to guess REA_ROOT and KWALKS_ROOT if unable to get it from prperties
      $ENV{"REA_ROOT"} = "$ENV{RSAT}/contrib/REA" if (not exists($ENV{"REA_ROOT"}));
      $ENV{"KWALKS_ROOT"} = "$ENV{RSAT}/contrib/kwalks/kwalks/bin" if (not exists($ENV{"KWALKS_ROOT"}));
      $ENV{"CLASSPATH"} .= ":$ENV{RSAT}/java/lib/NeAT_javatools.jar" unless ( exists($ENV{"CLASSPATH"})&& !($ENV{"CLASSPATH"}=~ /NeAT_javatools.jar/));
 #     
-    print STDERR "RSATPATH: $ENV{RSAT}\n";
-    print STDERR "REA_ROOT: $ENV{REA_ROOT}\n";
-    print STDERR "KWALKS_ROOT: $ENV{KWALKS_ROOT}\n";
-    print STDERR "CLASSPATH: $ENV{CLASSPATH}\n";
+      print STDERR "RSATPATH: $ENV{RSAT}\n";
+      print STDERR "REA_ROOT: $ENV{REA_ROOT}\n";
+      print STDERR "KWALKS_ROOT: $ENV{KWALKS_ROOT}\n";
+      print STDERR "CLASSPATH: $ENV{CLASSPATH}\n";
     
     
     
-    &RSAT::message::TimeWarn("running inferpathway:START");# if ($verbose >= 2);
+    &RSAT::message::TimeWarn("running inferpathway:START") if ($verbose >= 2);
     SOAPAction: "running inferpathway:START";
-    # build random id for file uniqueness
-      my $filename;
-      my (undef, $file) =  File::Temp::tempfile('XXXXXX', OPEN=>0);
-#      return "starting in $basedir \n";
-#     my $outputfile = &RSAT::Pathwayinference::Inferpathway(
- 
-      $file = "PathwayExtractorWS$file";
       $outputdir = Cwd::abs_path($outputdir);
       print STDERR "outputdir: $outputdir\n";
       #my $outputfile = PathwayExtraction::InferpathwaySub(
@@ -77,21 +99,20 @@ use Cwd 'abs_path';
       0,
       $outputdir,
       "$GERdir/$organism"."-gene_refseq_ec.tab",
-      "$neworkfilepattern"."_node_names.tab",
-      "$neworkfilepattern"."_network.tab",
+      $networknodenames,
+      $networkfile,
       $directed,
       $outputdir, "$file",3,{'-Q' => " ",'-J' => " "});
-#       
-        
-      &RSAT::message::TimeWarn("running inferpathway:END");# if ($verbose >= 2);
-#      --return  $outputfile;
-      &RSAT::message::TimeWarn("processing files:START in $outputdir");# if ($verbose >= 2);
+      &RSAT::message::TimeWarn("running inferpathway:END") if ($verbose >= 2);
+      &RSAT::message::TimeWarn("processing files:START in $outputdir") if ($verbose >= 2);
+     
+     
       &RSAT::PathwayExtraction::ProcessOutputFiles(
       $outputfile,
       $outputdir,
       "$GERdir/$organism"."-gene_refseq_ec.tab",
-      "$neworkfilepattern"."_node_names.tab",$directed,0);
-      &RSAT::message::TimeWarn("processing files:END");# if ($verbose >= 2); 
+      $networknodenames,$directed,0);
+      &RSAT::message::TimeWarn("processing files:END") if ($verbose >= 2); 
       
       my $cmd = "zip -j $outputdir/$file"."_results $outputdir/$file*";
       print STDERR $cmd."\n";
@@ -102,14 +123,11 @@ use Cwd 'abs_path';
     open RESFILE, "<$outputdir/$filename";
     my $resultfile =  do { local $/; <RESFILE> };
     close (RESFILE);
-#     my $resultfile = `cat $outputdir/$filename`;
     close STDERR;
     
     return SOAP::Data->name('response' => {'url' => $fileurl,
  				        'filename' => $filename,	
 				        'file' => $resultfile});
-#     return $outputurl."/$file"."_results.zip"; 
-#     return $resultshash;     
   }
  
   sub hi{
