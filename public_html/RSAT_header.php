91ea30c993dd956a0978a1fd52ad84243d08b708
jeremy <jeremy>
2012-03-29 11:43:37 +0000

    first version

---
+++ public_html/RSAT_header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+<?php
+if ($result) {
+	$res = " - results";
+	$class = "results";
+	echo '<link rel="stylesheet" type="text/css" href = "main_grat.css" media="screen">';
+} else  {
+	$res = "";
+	$class = "form";
+	echo '<link rel="stylesheet" type="text/css" href="main.css" media="screen,projection,print"/>';
+}
+?>
+		<link rel="stylesheet" type="text/css" href="tabs.css" media="screen,projection,print"/>
+		<script src="RSAT_menu.js" type="text/javascript"></script>
+		<script src="RSAT_tabs.js" type="text/javascript"></script>
+		<title>RSAT - <?php echo $prog_name, $res; ?></title>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	</head>
+
+	<body class="<?php echo $class; ?>">
+		<h3 align='center'><a href="<?php echo $properties['rsat_www']?>">RSA-tools</a> - <?php echo $prog_name, $res; ?></h3><br/>
