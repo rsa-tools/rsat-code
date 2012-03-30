9a818f7038164bac698d60ee21f9887e75bcd8f9
jeremy <jeremy>
2012-03-30 14:43:13 +0000

    add checking for bed file and transfac file

--- public_html/peak-footprints.php
+++ public_html/peak-footprints.php
@@ -4,7 +4,7 @@ require ('functions.php');
 UpdateLogFile("rsat","","");
 
 //print_r($properties);
-print_r($_POST);
+//print_r($_POST);
 //print_r($_FILES);
 
 // Import variables with prefix fs_ from form
@@ -97,16 +97,32 @@ function int($name, $value) {
 if ($pf_genome == "0")  {
 	error("You forgot to choose a genome");
 	$errors = true;
+} else {
+	$argument .= " --ref_species $pf_genome";
 }
 
 if ($_POST['species_ali_keep'] == "") {
 	error("You forgot to specify aligned species");
 	$errors = true;
+} else {
+	$al_species = ' --align_species "';
+	foreach($pf_species_ali_keep as $key => $value) {
+		if ($value!=end($pf_species_ali_keep)) {
+			$al_species .= "$value ";
+		} else {
+			$al_species .= $value.'"';
+		}
+	}
+	$argument .= $al_species;
 }
 
 if ($pf_r_motif == "") {
 	error("You forgot to specify Reference Motif");
 	$errors = true;
+} else {
+	if (preg_match("/^[\w\.]+$/",$pf_r_motif)) {
+		$argument .= " --ref_motif $pf_r_motif";
+	}	
 }
 
 if (numeric("Column-wise conservation threshold", $pf_cons_thres)) {
@@ -149,8 +165,6 @@ if (int("Maximum number of motif reported by family ", $pf_motif_number_family))
 	$errors = true;
 }
 
-echo $argument;
-
 //////////////////////////////////////////////////////////////////////////////
 //Writing bed/transfac file in tmp
 if (!$errors) {
@@ -158,22 +172,172 @@ if (!$errors) {
 	
 	// Bed data provided in text area
 	if ($pf_bed != "") {
+		$array_line = explode("\n",$pf_bed);
+		$bed_file = $properties['rsat_tmp']."/"."userbed".$suffix.".bed";
+		$file = fopen ($bed_file, "w");
+		$no_bed_line = true;
+		
+		foreach($array_line as $line) {
+			if (preg_match("/^[\w\-\+\s,\.\#; \/]+$/",$line)) {
+				$no_bed_line = false;
+				fwrite($file, $line);
+			} else {
+						warning (htmlspecialchars($line)." is not a bed line.\n");
+			}
+		}
+		fclose($file);
+		
+		if ($no_bed_line) {
+			error("All your line are not bed format");
+			$errors = true;
+		} else {
+			$argument .= " --input_peaks $bed_file";
+		}
+	}
+	
+	// Upload bed file from client machine
+	if ($_FILES["bedfile"]['name'] != "") {
+		$bed_file_name = basename($_FILES['bedfile']['name']);
+
+		// Move uploaded bed file in tmp
+		if (end(explode(".", $bed_file_name)) =="bed") {
+			$bed_file = $properties['rsat_tmp']."/".$bed_file_name;
+			$bed_file = str_replace(".bed",$suffix.".bed",$bed_file);
+			if(move_uploaded_file($_FILES['bedfile']['tmp_name'], $bed_file)) {
+				$argument .= " --input_peaks $bed_file";
+			} else {
+				error('File upload failed');
+				$errors = true;
+			}
+		}	else {
+			error("Wrong file extension");
+			$errors = true;
+		}
+	}
+
+	//Transfac data provided in text area
+	if ($pf_transfac != "") {
+		$array_line = explode("\n",$pf_transfac);
+		$transfac_file = $properties['rsat_tmp']."/"."userbed".$suffix.".transfac";
+		$file = fopen ($transfac_file, "w");
+		$no_transfac_line = true;
+	
+		foreach($array_line as $line) {
+			if (preg_match("/^[\w\-\+\s,\.\#; \/]+$/",$line)) {
+				$no_transfac_line = false;
+				fwrite($file, $line);
+			} else {
+				warning (htmlspecialchars($line)." is not a bed line.\n");
+			}
+		}
+		fclose($file);
+	
+		if ($no_transfac_line) {
+			error("All your line are not transfac format");
+			$errors = true;
+		} else {
+			$argument .= " --??? $bed_file";  //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+		}
+	}	
+	
+	// Upload transfac file from client machine
+	if ($_FILES["transfacfile"]['name'] != "") {
+		$transfac_file_name = basename($_FILES['transfacfile']['name']);
+	
+		// Move uploaded transfac file in tmp
+		if (end(explode(".", $transfac_file_name)) == "transfac") {
+			$transfac_file = $properties['rsat_tmp']."/".$transfac_file_name;
+			$transfac_file = str_replace(".transfac",$suffix.".transfac",$transfac_file);
+			if(move_uploaded_file($_FILES['transfacfile']['tmp_name'], $transfac_file)) {
+				$argument .= " --??? $transfac_file";   //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+			} else {
+				error('File upload failed');
+				$errors = true;
+			}
+		}	else {
+			error("Wrong file extension"); 
+			$errors = true;
+		}
 	}
 }
 
-
-
 /////////////////////////////////////////////////////////////////////////////
 if (!$errors) {
+	
+	// Specify output file
+	$output_file = str_replace(".bed",".fasta",$bed_file);
+	$argument .= " -o $output_file";
+	$URL['Genomic coordinates (bed)'] = rsat_path_to_url($bed_file);
+	$URL['Motif (transfac)'] = rsat_path_to_url($transfac_file);
+	$URL['Peak-footprints (fasta)'] = rsat_path_to_url($output_file);
+	
+	// Add arguments to the command
+	$cmd .= $argument;
+	
 	// Announce job starting
 	$msg = "Starting job.";
 	if ($fs_output =="email")  {
 		$msg .= " After job completion, email will be sent to ".$fs_user_email;
 	}
-	
 	info($msg);
 	echo "<hr>";
+	
+	// Display the command
+/* $cmd_report = str_replace($properties['RSAT'], '$RSAT', $cmd);
+	info("Command : ".$cmd_report);
+	echo "<hr>";
+*/
 	flush();
+	
+	// Display the result
+	print_url_table($URL);
+	
+	///////////////////////////////////////////////////////////////
+	// Send email with notification of task completion
+	if ($fs_output =="email")  {
+		// Parammeters for sending the mail
+		$to = $fs_user_email;
+		$subject = "[RSAT] $prog_name result ".$now."_".$suffix;
+	
+		// Store the URL table in a variable
+		$html_mail = 0; // Boolean variable indicating whether HTML format is supported in email
+		$headers = ""; // Header (specifying Mime types)
+		if ($html_mail) {
+			$msg = "<table class='resultlink'>\n";
+			$msg .= "<tr><th colspan='2'>Result file(s)</th></tr>\n";
+			foreach ($URL as $key => $value) {
+				$msg .= "<tr><td>".$key."</td><td><a href = '".$value."'>".$value."</a></td></tr>\n";
+			}
+			$msg .= "</table>\n";
+	
+			$headers .= 'Mime-Version: 1.0'."\r\n";
+			$headers .= 'Content-type: text/html; charset=utf-8'."\r\n";
+			$headers .= "\r\n";
+		} else {
+			$msg = "$prog_name result\n\n";
+			$msg .= "Result files:\n";
+			foreach ($URL as $key => $value) {
+				$msg .= "\t".$key."\t".$value."\n";
+			}
+		}
+
+		// Sending mail
+		$smtp = $properties["smtp"];
+	
+		// Check that the SMTP was specificed in the property file of the server
+		if ($smtp == "") {
+			error("SMTP server is not specified in the RSAT config file. Please contact system administrator.");
+		} else {
+			info("Sending mail via SMTP ".$smtp);
+			ini_set ( "SMTP", $smtp);
+			$mail_sent = mail($to, $subject, $msg, $headers);
+			if ($mail_sent) {
+				info("Job done, email sent to ".$to);
+			} else {
+				error("Notification mail could not be sent.\n\n.".$msg);
+			}
+		}
+	}	
 }
 
 ?>
