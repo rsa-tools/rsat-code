2f0093e0988a61f104ac325340d2963300b9e79e
rsat <rsat>
2012-06-22 23:36:32 +0000

    added parameters for install_software.mk

--- RSAT_config_default.mk
+++ RSAT_config_default.mk
@@ -5,13 +5,31 @@
 ## ${RSAT}/makefiles/util.mk, which is itself loaded by all the RSAT
 ## makefile scripts.
 
+## Operating system
+## Supported: linux  | macosx
+#OS=linux
+OS=linux
+ARCHITECTURE=x64
 
-## This parameter is essential, since the same program (qsub) has
-## different parameters depending on the queue manager. Thus, a qsub
-## command for torque will not be understood by sge and reciprocally;
+#UCSC_OS=macOSX.i386
+UCSC_OS=linux.x86_64
+
+## Default installation dir for binaries
+BIN_DIR=/usr/local/bin
+
+## Main directory for software installation
+SOFT_DIR=${PWD}
+
+################################################################
+##
+## Configuration of a PC cluster
+##
+## The parameter QUEUE_MANAGER is required for running tasks on a PC
+## cluster. The same program (qsub) has different parameters depending on the
+## queue manager. Thus, a qsub command for torque will not be understood by
+## sge and reciprocally;
 ##
 ## supported: torque | sge
-##
 QUEUE_MANAGER=sge
 
 ## Name of the queue where the jobs have to be sent
