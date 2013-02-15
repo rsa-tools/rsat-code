025d17e8996d220e6dd9aff209fa4de813f3faf2
jvanheld <jvanheld>
2013-02-15 23:21:58 +0000

    added Sorensen similarity

--- public_html/compare_classes_form.php
+++ public_html/compare_classes_form.php
@@ -21,31 +21,37 @@ $demo = $_REQUEST['demo'];
 if ($demo == 1) {
   $demo_classesQ = storeFile("demo_files/gavin_mcl_clusters_inf2.1.tab") ;
   $demo_classesR = storeFile("demo_files/mips_complexes.tab") ;
-
+      
   $demo_remark = "This demonstration consists in the comparaison between clusters obtained after application of the <a href = 'http://micans.org/mcl/' target = 'top'>MCL</a> clustering algorithm to the <a target = '_blank' href = 'http://www.ncbi.nlm.nih.gov/sites/entrez?Db=pubmed&Cmd=ShowDetailView&TermToSearch=16429126&ordinalpos=1&itool=EntrezSystem2.PEntrez.Pubmed.Pubmed_ResultsPanel.Pubmed_RVDocSum'>Gavin et al (2006)</a> interaction network and the complexes annotated in the <a target = '_blank' href = 'http://mips.gsf.de/'>MIPS database</a>.";
 }
 title('compare-classes');
+?>
 
-echo("<center>Compare two classifications (clustering results, functional
+<center>Compare two classifications (clustering results, functional
   classes, ...), and assess the statistical significance of common
-  members between each pair of classes."); 
+  members between each pair of classes.
 
-echo ("<br>This program was
+<br>This program was
   developed <A HREF='mailto:jacques.van.helden@ulb.ac.be (Jacques van
   Helden)'>Jacques van Helden</a>, with a contribution of Joseph Tran
-  for a first prototype.</center>");
-echo ("<hr>");
+  for a first prototype.</center>
+
+<hr>
   
-echo ("<form method='post' action='compare_classes.php' enctype='multipart/form-data'>");
+<form method='post' action='compare_classes.php' enctype='multipart/form-data'>
+
+<?php
 if($demo) {
   demo($demo_remark);
  }
+?>
 
-echo ("<blockquote>");
-echo ("<table border=0 cellspacing=0 cellpadding=4>\n");
+<blockquote>
+<table border=0 cellspacing=0 cellpadding=4>
 
-echo ("<tr align = 'center'><th>Query classes</th><th>Reference classes</th></tr>\n");
+<tr align = 'center'><th>Query classes</th><th>Reference classes</th></tr>
 
+<?php
 ################################################################
 ## Query input classes textarea
 echo "<tr><td>\n";
@@ -86,32 +92,31 @@ if (!$pipe) {
         <input type = 'text' name='score_col' size = 2\> 
         (optional; if specified, must be valid for both query and reference classes)");
 }
+?>
 
-################################################################
-## Comparaison between query classes
-## options for self-comparison
-echo ("<br><input type = 'radio' checked value='off' name='self_compa' size = 1> 
+<!-- Comparaison between query classes - options for self-comparison-->
+<br><input type = 'radio' checked value='off' name='self_compa' size = 1> 
       <a href = 'help.compare_classes.html#query_vs_ref'>
-      <b>Compare query classes to reference classes</a></b>");
+      <b>Compare query classes to reference classes</a></b>
 
-echo ("<br><input type = 'radio' value='on' name='self_compa' size = 1> 
+<br><input type = 'radio' value='on' name='self_compa' size = 1> 
       <a href = 'help.compare_classes.html#query_vs_query'>
-      <b>Compare query classes to query classes</a></b> (do not specify reference classes)<br>");
+      <b>Compare query classes to query classes</a></b> (do not specify reference classes)<br>
 
-echo ("<ul><input type='checkbox' value='on' name='distinct' size = 1 checked/> 
+<ul><input type='checkbox' value='on' name='distinct' size = 1 checked/> 
       <a href = 'help.compare_classes.html#distinct'>
-      <b>Prevent self-comparison</a></b><br>");
+      <b>Prevent self-comparison</a></b><br>
 
-echo ("<input type = 'checkbox' value='on' name='triangle' size = 1 checked/>
+<input type = 'checkbox' value='on' name='triangle' size = 1 checked/>
       <a href = 'help.compare_classes.html#triangle'>
-      <b>Prevent reciprocal comparisons</a></b><br></ul></td>");
+      <b>Prevent reciprocal comparisons</a></b><br></ul></td>
 
-## Output format
-// echo("<br><table>
-//   <tr><td colspan = 2 >&nbsp;&nbsp;&nbsp;<B><a href = 'help.compare_classes.html#formats'>Output format</a></B>
-//   <tr><td><input type = 'radio' checked name='out_format' value = 'class'/></td><td>Class pairs</td></tr>
-//   <tr><td><input type = 'radio' name='out_format' value = 'matrix'/></td><td>Reference/query matrix</td></tr>
-// ");
+<br><table>
+   <tr><td colspan = 2 >&nbsp;&nbsp;&nbsp;<B><a href = 'help.compare_classes.html#formats'>Output format</a></B>
+   <tr><td><input type = 'radio' checked name='out_format' value = 'class'/></td><td>Class pairs</td></tr>
+   <tr><td><input type = 'radio' name='out_format' value = 'matrix'/></td><td>Reference/query matrix</td></tr>
+
+<?php
 
 echo "<hr>";
 echo "<b>Output options</b><br>";
@@ -128,7 +133,8 @@ echo("<td><table border='0' cellspacing='0' cellpadding='0'>
   <tr><td><label><input type='checkbox' name='freq' value='on' />Frequencies</label></td></tr> 
   <tr><td><label><input type='checkbox' name='proba' value='on' checked='checked' />Hypergeometric probability</label>
   &nbsp;&nbsp;<label><input type='checkbox' name='sort' value='on' checked='checked' />Sorting criterion</label></td></tr> 
-  <tr><td><label><input type='checkbox' name='jac_sim' value='on' checked='checked' />Jaccard index</label></td></tr> 
+  <tr><td><label><input type='checkbox' name='jac_sim' value='on' checked='checked' />Jaccard similarity</label></td></tr> 
+  <tr><td><label><input type='checkbox' name='sor_sim' value='on' />Sorensen similarity</label></td></tr> 
   <tr><td><label><input type='checkbox' name='dotprod' value='on' />Dotproduct <i>(Only relevant if a score column is specified)</i></label></td></tr>
   <tr><td><label><input type='checkbox' name='entropy' value='on' />Entropy</label></td></tr> 
   <tr><td><label><input type='checkbox' name='members' value='on' /> Members <i> (Beware, this might generate large result files)</i></label></td></tr>
@@ -164,9 +170,13 @@ echo("<tr align='left' valign='TOP'>
      <td><input type='text' name='lth_eval' value='none' size='5' /></td> 
      <td><input type='text' name='uth_eval'  size='5' value='none' /></td></tr>\n"); 
 echo("<tr align='left' valign='TOP'>
-     <td> Jaccard index </td> 
+     <td> Jaccard similarity </td> 
      <td><input type='text' name='lth_jac' value='none' size='5' /></td> 
      <td><input type='text' name='uth_jac'  size='5' value='none' /></td></tr>\n"); 
+echo("<tr align='left' valign='TOP'>
+     <td> Sorensen similarity </td> 
+     <td><input type='text' name='lth_sor' value='none' size='5' /></td> 
+     <td><input type='text' name='uth_sor'  size='5' value='none' /></td></tr>\n"); 
 echo("<tr align='left' valign='TOP'>
      <td> Mutual information </td> 
      <td><input type='text' name='lth_mi' value='none' size='5' /></td> 
@@ -183,7 +193,8 @@ echo ("</td>\n");
 echo ("<td><table>");
 echo ("<tr><td><input type='radio' name='matrix_field' checked='checked' value='QR'/></td><td>Intersection</td></tr>\n
          <tr><td><input type='radio' name='matrix_field' value='sig'/></td><td>Significance</td></tr>\n
-         <tr><td><input type='radio' name='matrix_field' value='jac_sim'/></td><td>Jaccard index</td></tr>\n
+         <tr><td><input type='radio' name='matrix_field' value='jac_sim'/></td><td>Jaccard similarity</td></tr>\n
+         <tr><td><input type='radio' name='matrix_field' value='sor_sim'/></td><td>Sorensen similarity</td></tr>\n
          <tr><td><input type='radio' name='matrix_field' value='dotprod'/></td><td>Dot product</td></tr>\n
          <tr><td><input type='radio' name='matrix_field' value='E_val'/></td><td>E-value</td></tr>\n
          <tr><td><input type='radio' name='matrix_field' value='P_val'/></td><td>P-value</td></tr>\n
