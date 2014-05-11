d9715b7dd9a8bc597d3d650f3ffb7d4288c55b8a
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2014-05-11 06:20:40 +0200

    verbosity

--- perl-scripts/lib/RSAT/matrix.pm
+++ perl-scripts/lib/RSAT/matrix.pm
@@ -593,7 +593,7 @@ sub insert_columns {
   $right_col_nb = 0 if ($right_col_nb eq "");
   &RSAT::error::FatalError("Invalid value for right values") unless &RSAT::util::IsNatural($right_col_nb);
   
-  &RSAT::message::TimeWarn("Inserting columns to matrix", $self->get_attribute("name"), $left_col_nb, $right_col_nb, $fill_value) if ($main::verbose >= 0);
+  &RSAT::message::TimeWarn("Inserting columns to matrix", $self->get_attribute("name"), $left_col_nb, $right_col_nb, $fill_value) if ($main::verbose >= 5);
   
 
   my @counts = $self->getMatrix();
@@ -3980,6 +3980,7 @@ sub makeLogo{
     my $logo_verbose = 0;
     my $logo_batch = 0;
     my $logo_job_prefix = "";
+
     &RSAT::util::doit($logo_cmd,$logo_dry,$logo_die,$logo_verbose,$logo_prefix);
 
     my $logo_file = $logo_basename.".".$logo_format;
@@ -3991,6 +3992,7 @@ sub makeLogo{
     push @logo_files, $logo_file;
     &RSAT::message::Info("matrix", $self->get_attribute("id"), "Logo file", $logo_file) if ($main::verbose >= 5);
   }
+
 
   ## Remove the fake sequences, not necessary anymore
   #    &RSAT::server::DelayedRemoval($fake_seq_file);
@@ -4067,6 +4069,7 @@ sub fake_seq_from_matrix {
   ## create a temporary sequence file which will be deleted after logo creation
   my $id = $self->get_attribute("id")|| $self->get_attribute("identifier") ;
   my $tmp_seq_file = &RSAT::util::make_temp_file($logo_dir, $id);
+#  &RSAT::message::Debug("Tempory file for fake sequences to generate logo", $tmp_seq_file) if ($main::verbose >= 5);
 #  my $tmp_seq_file = &RSAT::util::make_temp_file($seq_prefix, $id);
   if ($main::verbose >= 5) {
     &RSAT::message::Debug("Fake sequence files",
@@ -4077,6 +4080,7 @@ sub fake_seq_from_matrix {
   my $seq_handle = &RSAT::util::OpenOutputFile($tmp_seq_file);
   print $seq_handle join("\n",@seqs)."\n";
   close $seq_handle;
+
   return ($tmp_seq_file,$seq_number);
 }
 
