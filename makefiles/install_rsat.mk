885671b4d3d9680c3e9ecc4d267c553843022702
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2014-06-17 13:44:14 +0200

    added perl_modules_install_by_force

--- makefiles/install_rsat.mk
+++ makefiles/install_rsat.mk
@@ -192,7 +192,7 @@ perl_modules_install_one_by_one:
 
 ## Some Perl modules cannot be installed without force
 perl_modules_install_by_force:
-	@sudo ${PERL} -MCPAN -e 'force install SOAP::SWDL'
+	@sudo ${PERL} -MCPAN -e 'force install SOAP::WSDL'
 	@sudo ${PERL} -MCPAN -e 'force install SOAP::Transport::HTTP'
 
 ## Install a single Perl module
