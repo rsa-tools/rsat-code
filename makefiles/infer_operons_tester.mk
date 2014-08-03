da14e5fd562ebfe65e0fb5376b4e0bc3151d84c8
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2014-08-03 23:29:11 +0200

    in construction

---
+++ makefiles/infer_operons_tester.mk
@@ -0,0 +1,24 @@
+################################################################
+## Run infer-operons to detect operons and directons
+
+include ${RSAT}/makefiles/util.mk
+MAKEFILE=${RSAT}/makefiles/infer_operons_tester.mk
+
+ORG=Escherichia_coli_K_12_substr__MG1655_uid57779
+OPERON_DIR=operons/${ORG}
+TYPE=operons
+OPERONS=${OPERON_DIR}/${ORG}_${TYPE}_dist${DIST}_mingenes${MIN_GN}
+FIELDS=name,operon,query,leader,upstr_dist,gene_nb
+DIST=55
+MIN_GN=1
+infer_operons:
+	@echo
+	@echo "Inferring operons	${ORG}"
+	@mkdir -p ${OPERON_DIR}
+	infer-operon -v 1 -dist ${DIST} -min_gene_nb ${MIN_GN} -return ${FIELDS} -org ${ORG} -all -o ${OPERONS}.tab
+	@echo "	${OPERONS}.tab"
+	@text-to-html -i ${OPERONS}.tab -o ${OPERONS}.html
+	@echo "	${OPERONS}.html"
+
+infer_directons:
+	${MAKE} infer_operons DIST=1000000 TYPE=directons
