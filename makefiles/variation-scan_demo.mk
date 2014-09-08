43eadad7605e9cdd58e4a0e49984bbc2b31ee225
Jacques van Helden <jacques.van-helden@univ-amu.fr>
2014-09-08 18:12:27 +0200

    preparing demo case

---
+++ makefiles/variation-scan_demo.mk
@@ -0,0 +1,8 @@
+################################################################
+## Demonstrator for variation-scan and related programs.
+
+include ${RSAT}/makefiles/util.mk
+MAKEFILE=${RSAT}/makefiles/variation-scan_demo.mk
+
+SNP=rs2736191
+MATRIX=data/
