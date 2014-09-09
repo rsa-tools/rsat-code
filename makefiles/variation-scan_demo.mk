bcd7e40a73c634bfc9ae0ce4cf296c7619cc7f1b
Jacques van Helden <jacques.van-helden@univ-amu.fr>
2014-09-09 16:13:09 +0200

    added comments

--- makefiles/variation-scan_demo.mk
+++ makefiles/variation-scan_demo.mk
@@ -4,31 +4,40 @@
 include ${RSAT}/makefiles/util.mk
 MAKEFILE=${RSAT}/makefiles/variation-scan_demo.mk
 
-SNP=rs2736191
+################################################################
+## The test matrix comes from Ballester et al.
 MATRIX=${RSAT}/public_html/demo_files/do798+do735_mmus_hnf6_liver.transfac
+
+## Variants selected to illustate the typology of cases, including
+## non-trivial cases with >2 variants.
 VARIANTS=${RSAT}/public_html/demo_files/test_complex_tab
 
 E_VERSION=72
 V=3
-
+################################################################
+## Convert variations in VCF (variation X file) format into the format
+## supported as input by RSAT retrieve-var.
 CONVERT_VAR_CMD=convert-variations -i ${VARIANTS}.vcf  -e_version ${E_VERSION} -v ${V} -from vcf -to rsat-var -o ${VARIANTS}.rsat_var
-
 convert_var:
+	@echo ""
+	@echo "Converting variations"
 	@echo "${CONVERT_VAR_CMD}"
 	@${CONVERT_VAR_CMD}
 
+################################################################
+## Retrieve the sequences surrounding a set of input variations
 ORG=Homo_sapiens
 RETRIEVE_VAR_CMD=retrieve-variation-seq  -v ${V} -species ${ORG}  -e_version ${E_VERSION} -i ${VARIANTS}.rsat_var  -mml 30 -o ${VARIANTS}_rsat_var.seq -format rsat-var
-
 retrieve_var:
 	@echo "${RETRIEVE_VAR_CMD}"
 	@${RETRIEVE_VAR_CMD}
 
+################################################################
+## Scan selected variations with the matrix of interest
 PVAL=1e-2
 PVAL_RATIO=1
 BG_MODEL=public_html/demo_files/all_human_ENCODE_DNAse_mk1_bg.ol
 VAR_SCAN_CMD=variation-scan -i ${VARIANTS}_rsat_var.seq -m ${MATRIX} -bg ${BG_MODEL} -uth pval ${PVAL} -lth pval_ratio ${PVAL_RATIO} -o ${VARIANTS}_rsat_var_scan_pval${PVAL}_pvalratio${PVAL_RATIO}.tab
-
 variation_scan:
 	@echo "${VAR_SCAN_CMD}"
 	@${VAR_SCAN_CMD}
