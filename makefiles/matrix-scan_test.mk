b83fcd61f6307a281f6bd788481ade9f1b492f3b
Jacques van Helden <jacques.van-helden@univ-amu.fr>
2014-10-21 21:45:11 +0200

    in progress

--- makefiles/matrix-scan_test.mk
+++ makefiles/matrix-scan_test.mk
@@ -4,29 +4,46 @@ include ${RSAT}/makefiles/util.mk
 MAKEFILE=${RSAT}/makefiles/matrix-scan_test.mk
 
 ################################################################
-## Scan Drosophila even-skipped promoter with matrices corresponding
-## to 12 transcription factors involved in embryonic segmentation.
+## List parameters
 V=2
 SEQ=${RSAT}/public_html/demo_files/Dmelanogaster_eve_up5000.fasta
 MATRIX_FILE=${RSAT}/public_html/demo_files/Dmelanogaster_segmentation_12matrices.tf
+MATRIX_NAME_OPT=-matrix_name Kr -matrix_name bcd -matrix_name eve
 RES_DIR=results/matrix-scan_test
-SITES=${RES_DIR}/Dmelanogaster_eve_up5000_sites${SUFFIX}.ft
+SITES=${RES_DIR}/Dmelanogaster_eve_up5000_sites${SUFFIX}
 SUFFIX=
+list_param:
+	@echo "matrix-scan test parameters"
+	@echo "SEQ		${SEQ}"
+	@echo "MATRIX_FILE	${MATRIX_FILE}"
+	@echo "RES_DIR		${RES_DIR}"
+	@echo "SITES		${SITES}.ft"
+	@echo "SITES (quick)	${SITES}-quick.ft"
+	@echo "SUFFIX		${SUFFIX}"
+	@echo "MATRIX_NAME_OPT	${MATRIX_NAME_OPT}"
+	@echo "OPT		${OPT}"
+	@echo "MATRIX_SCAN_CMD	${MATRIX_SCAN_CMD}"
+
+################################################################
+## Scan Drosophila even-skipped promoter with matrices corresponding
+## to 12 transcription factors involved in embryonic segmentation.
+MATRIX_SCAN_CMD=matrix-scan -v ${V} \
+		-matrix_format transfac -m ${MATRIX_FILE} \
+		${MATRIX_NAME_OPT} \
+		-pseudo 1 -decimals 1 -2str -bginput -markov 0 -bg_pseudo 0.01 \
+		-seq_format fasta -n score -origin genomic \
+		-return limits,sites,pval -lth score 1 -uth pval 1e-4 \
+		-i ${SEQ} \
+		${OPT} -o ${SITES}.ft
 scan_eve:
 	@echo
 	@echo "SEQ		${SEQ}"
 	@echo "MATRIX_FILE	${MATRIX_FILE}"
 	@echo "Scanning"
 	@mkdir -p ${RES_DIR}
-	matrix-scan -v ${V} \
-		-matrix_format transfac -m ${MATRIX_FILE} \
-		-pseudo 1 -decimals 1 -2str -bginput -markov 0 -bg_pseudo 0.01 \
-		-seq_format fasta -n score -origin genomic \
-		-return limits,sites,pval -lth score 1 -uth pval 1e-4 \
-		-i ${SEQ} \
-		${OPT} -o ${SITES}
+	${MATRIX_SCAN_CMD}
 	@echo "Result file"
-	@echo "	${SITES}"
+	@echo "	${SITES}.ft"
 
 ## Select a subset of matrices specified by their name
 JASPAR=${RSAT}/public_html/data/motif_databases/JASPAR/jaspar_core_insects_2013-11.tf
@@ -46,4 +63,4 @@ scan_jaspar_selected_names:
 ################################################################
 ## Scan even-skipped promoter with the option -quick
 scan_eve_quick:
-	${MAKE} scan_eve OPT=-quick SUFFIX=-quick
+	${MAKE} scan_eve OPT=-quick SUFFIX=-quick 
