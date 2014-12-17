20f1bcde62b39bec832d7efdf8c1245c13ec740e
Jaime Castro <jcastro@lcg.unam.mx>
2014-12-17 11:43:23 +0100

    Test update R package

--- R-scripts/TFBMclust/R/hclust.motifs.R
+++ R-scripts/TFBMclust/R/hclust.motifs.R
@@ -2,14 +2,5 @@
 ## Build the tree by hierarchical clustering using Rcpp library
 ## if it is indicated, export the tree in Newick format
 hclust.motifs <- function(dist.matrix, hclust.method = "average"){
-
-  ## Require Rclusterpp if it is required
-  if(!require("Rclusterpp")){
-    install.packages("Rcpp")
-  }
-
-  ## Load library
-  suppressPackageStartupMessages(library(Rclusterpp, warn.conflicts=FALSE))
-
   return(Rclusterpp.hclust(dist.matrix, method = hclust.method))
 }
