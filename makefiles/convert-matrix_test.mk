c4748b89c83e22ec1bf8ad1693228f421b367b1e
Jacques van Helden <jacques.van-helden@univ-amu.fr>
2015-02-06 01:40:37 +0100

    added option -logo_program weblogo|seqlogo

--- makefiles/convert-matrix_test.mk
+++ makefiles/convert-matrix_test.mk
@@ -8,12 +8,15 @@
 include ${RSAT}/makefiles/util.mk
 MAKEFILE=${RSAT}/makefiles/convert-matrix_test.mk
 
+## Choose logo program (weblogo|seqlogo)
+LOGO_PROGRAM=weblogo
+
 #################################################################
 ## Generate logos from the matrices
 PREFIX=Dmelanogaster_segmentation_12matrices
 IN_MATRICES=${RSAT}/public_html/demo_files/${PREFIX}
 RES_DIR=results/convert-matrix_test/${PREFIX}
-LOGO_DIR=${RES_DIR}/logos
+LOGO_DIR=${RES_DIR}/${LOGO_PROGRAM}
 OUT_MATRICES=${RES_DIR}/${PREFIX}
 FROM=tf
 TO=tab
@@ -26,5 +29,14 @@ logos:
 	@mkdir -p ${RES_DIR} ${LOGO_DIR}
 	convert-matrix -v ${V} -i ${IN_MATRICES}.${FROM} -from ${FROM} \
 		-return counts,parameters,logo -logo_dir ${LOGO_DIR} \
+		-logo_program ${LOGO_PROGRAM} \
 		-to ${TO} -o ${OUT_MATRICES}.${TO}
 	@echo "	${OUT_MATRICES}.${TO}"
+
+## Convert 12 matrices involved in Drosophila segmentation
+droso_segm:
+	@${MAKE} logos PREFIX=Dmelanogaster_segmentation_12matrices
+
+## Convert LexA matrix (E.coli)
+lexa:
+	@${MAKE} logos PREFIX=LexA.2nt_upstream-noorf-ovlp-2str.20
