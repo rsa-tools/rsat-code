49cb25e2259f515706cd31c40df3fc3c930375f9
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2015-02-11 08:36:19 +0100

    adapted parameters to fit Web DEMO

--- makefiles/crer-scan_test.mk
+++ makefiles/crer-scan_test.mk
@@ -26,6 +26,21 @@ demo_dorsal:
 
 CRER_DIR=${RES_DIR}/crers
 EVE_UPSTREAM_SITES=${RSAT}/public_html/demo_files/Drosophila_melanogaster_eve_segmentation_sites_pval0.001.ft
+
+
+python2.7 $RSAT/python-scripts/crer_scan.py  -v 1 \
+		-s  \
+		-i $RSAT/public_html/tmp/apache/2015/02/11/crer_scan_2015-02-11.074115_6vu1Mo_query.txt \
+		-in_format ft \
+		-lth_crer_size 100 \
+		-uth_crer_size 500 \
+		-lth_crer_sites 2 \
+		-lth_crer_sites_distance 1 \
+		-uth_crer_sites_distance 100 \
+		-uth_site_pval 1e-3 \
+		-lth_crer_sig 0 \
+		-return_limits_filtered
+
 demo_eve:
 	@echo "Detecting CRERs for segmentation TF in even-skipped upstream sequence"
 	@echo "Sites	${EVE_UPSTREAM_SITES}"
@@ -34,14 +49,13 @@ demo_eve:
 		-i  ${EVE_UPSTREAM_SITES} \
 		-s \
 		-in_format ft \
-		-lth_crer_size 30 -uth_crer_size 500 \
-		-lth_crer_sites 2 -uth_crer_sites 1000 \
-		-lth_crer_sites_distance 1 -uth_crer_sites_distance 1000 \
-		-uth_site_pval 0.001\
-		-uth_crer_eval 0.0001 \
-		-uth_crer_pval 0.0001 \
-		-lth_crer_sig 2 \
-		-lth_score 0 -uth_score 1000 \
+		-lth_crer_size 100 \
+		-uth_crer_size 500 \
+		-lth_crer_sites 2 \
+		-lth_crer_sites_distance 1 \
+		-uth_crer_sites_distance 100 \
+		-uth_site_pval 1e-3 \
+		-lth_crer_sig 0 \
 		-uth_overlap 1 \
 		-return_limits_filtered \
 		-o ${CRER_DIR}/Drosophila_melanogaster_eve_segmentation_sitespval0.001_crer.ft
