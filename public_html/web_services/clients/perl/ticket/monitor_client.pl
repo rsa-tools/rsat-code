3e2cfbdcc79c4494bf83ddf2b22bc4542654ec29
Olivier OS. Sand <olivier.sand@good.ibl.fr>
2015-02-16 17:32:18 +0100

    more ws perl clients moved to public_html

---
+++ public_html/web_services/clients/perl/ticket/monitor_client.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/perl -w
+
+use strict;
+use XML::Compile::SOAP11;
+use XML::Compile::WSDL11;
+use XML::Compile::Transport::SOAPHTTP;
+use Data::Dumper;
+
+eval
+{
+    # Retriving and processing the WSDL
+    my $wsdl  = XML::LibXML->new->parse_file('http://pedagogix-tagc.univ-mrs.fr/rsat/web_services/RSATWS.wsdl');
+    my $proxy = XML::Compile::WSDL11->new($wsdl);
+    
+    # Generating a request message based on the WSDL
+    my $client = $proxy->compileClient('monitor');
+
+    my $ticket = $ARGV[0];
+
+    my %args = (
+	'ticket' => $ticket
+	);
+
+    # Calling the service and getting the response
+    print "Sending request to the server\n";
+    my $answer = $client->( request => {%args});
+
+
+    if ( defined $answer ) {
+      if ($answer->{output}->{response}->{status}){
+	print "\nStatus : ", $answer->{output}->{response}->{status}, "\n";
+	exit 0;
+      }
+    } else {    
+      print "Failed\n";
+      exit 1;
+    }
+};
+
+if ($@)
+{
+    print "Caught an exception\n";
+    print $@."\n";
+    exit 1;
+}
