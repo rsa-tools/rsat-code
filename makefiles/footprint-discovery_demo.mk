2201880f632805fbf7f5ce9187b69ace2d9a24f4
RSAT user on miztli <Jacques.van-Helden@univ-amu.fr>
2015-02-16 23:27:08 -0600

    in construction

---
+++ makefiles/footprint-discovery_demo.mk
@@ -0,0 +1,27 @@
+################################################################
+## Demonstration for the too footprint-discovery
+
+include ${RSAT}/makefiles/util.mk
+MAKEFILE=${RSAT}/makefiles/footprint-discovery_demo.mk
+
+ORG=${ORG}
+TAXON=Enterobacteriales
+GENE=lexA
+
+TASK=query_seq,filter_dyads,orthologs,ortho_seq,purge,dyads,maps,gene_index,index
+
+## Run footprint-discovery for a single gene of interest.
+FP_DISCO_DIR=results/footprint-discovery_demo/${ORG}/${TAXON}/${GENE}
+disco:
+	@echo
+	@echo "Running footprint-discovery 	${GEBE}	${ORG}	${TAXON}"
+	footprint-discovery  -v 1 -org ${ORG} -taxon ${TAXON} \
+		-q ${GENE} \
+		-lth occ 1 \
+		-lth occ_sig 0 \
+		-uth rank 50 \
+		-return occ,proba,rank \
+		-filter \
+		-bg_model taxfreq \
+		-task ${TASK} ${OPT} \
+		-o ${FP_DISCO_DIR}
