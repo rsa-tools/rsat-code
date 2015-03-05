898eadd517eac136193b44ee5528cd1b06e635ee
Jacques van Helden <jacques.van-helden@univ-amu.fr>
2015-03-05 11:01:28 +0100

    added target with gene file as input

--- makefiles/footprint-discovery_demo.mk
+++ makefiles/footprint-discovery_demo.mk
@@ -28,8 +28,8 @@ FP_DISCO_DIR=results/footprint-discovery_demo
 _fp_disco:
 	@mkdir -p ${FP_DISCO_DIR}
 	@echo
-	@echo "Running footprint-discovery 	${GEBE}	${ORG}	${TAXON}"
-	footprint-discovery  -v 1 -org ${ORG} -taxon ${TAXON} \
+	@echo "Running footprint-discovery 	${ORG}	${TAXON}	${QUERY}"
+	footprint-discovery -v ${V} -org ${ORG} -taxon ${TAXON} \
 		${QUERY} \
 		-lth occ 1 \
 		-lth occ_sig 0 \
@@ -40,16 +40,38 @@ _fp_disco:
 		-task ${TASK} ${BATCH} ${OPT} \
 		-o ${FP_DISCO_DIR}
 
+################################################################
+## Run footprint discovery with selected genes
+##
+## THIS FAILS HERE:  &InitQueryOutput(), footprint.lib.pl line389
+YEAST_DEMO_GENES=${RSAT}/public_html/demo_files/footprint-discovery_selected_yeast_genes.tab
+fp_disco_selected_genes_yeast:
+	@echo "Selected genes	${YEAST_DEMO_GENES}"
+	@${MAKE} _fp_disco QUERY='-genes ${YEAST_DEMO_GENES}' ORG=Saccharomyces_cerevisiae TAXON=Saccharomycetaceae
+
+################################################################
+## Run footprint-discovery with one selected gene
 fp_disco_one_gene: list_param
 	@${MAKE} _fp_disco QUERY='-q ${GENE}'
 
+## Run footprint discovery with all the genes of the genome of
+## interest. This costs several hours of computation, it should better
+## run on a cluster with the option -batch (see target
+## fp_disco_all_genes_batch) or with a resricted number of genes
+## (target fp_disco_gene_slice).
 fp_disco_all_genes: list_param
 	@${MAKE} _fp_disco  QUERY='-all_genes'
 
+## Run the analysis of each gene of a genome in batch. This requires a
+## PC cluster and a properly configured job manager (see cluster
+## options in RSAT_config.props).
+fp_disco_all_genes_batch: list_param
+	@${MAKE} fp_disco_all_genes BATCH='-batch'
+
+## Run footprint discovery for a limited number of genes amon all the
+## genes of the genome (for quick tests and debugging)
 SKIP=0
 LAST=25
 fp_disco_gene_slice: list_param
 	@${MAKE} fp_disco_all_genes OPT='-skip ${SKIP} -last ${LAST}'
 
-fp_disco_all_genes_batch: list_param
-	@${MAKE} fp_disco_all_genes BATCH='-batch'
