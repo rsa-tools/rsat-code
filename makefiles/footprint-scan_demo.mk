635e6f27bce6aa8756b152dbdde207b86ca4a739
Jacques van Helden <jacques.van-helden@univ-amu.fr>
2015-03-09 09:25:08 +0100

    modif

--- makefiles/footprint-scan_demo.mk
+++ makefiles/footprint-scan_demo.mk
@@ -1,5 +1,5 @@
 ################################################################
-## Demonstration for the too footprint-scan
+## Demonstration for the tool footprint-scan
 
 include ${RSAT}/makefiles/util.mk
 MAKEFILE=${RSAT}/makefiles/footprint-scan_demo.mk
@@ -39,6 +39,7 @@ _fp_scan:
 		-info_lines \
 		-bgfile ${RSAT}/public_html/data/genomes/${ORG}/oligo-frequencies/1nt_upstream-noorf_${ORG}-ovlp-2str.freq.gz \
 		-plot_format jpg -map_format jpg \
+		${BATCH} ${OPT} \
 		-o ${FP_SCAN_DIR}
 
 
@@ -46,3 +47,26 @@ _fp_scan:
 ## Run footprint-scan with selected gene
 fp_scan_some_genes: list_param
 	@${MAKE} _fp_scan QUERY='-q lexA -q recA -q uvrA -q uvrB'
+
+################################################################
+## Run footprint scan with all the genes of the genome of
+## interest. This costs several hours of computation, it should better
+## run on a cluster with the option -batch (see target
+## fp_scan_all_genes_batch) or with a resricted number of genes
+## (target fp_scan_gene_slice).
+fp_scan_all_genes: list_param
+	@${MAKE} _fp_scan  QUERY='-all_genes'
+
+## Run the analysis of each gene of a genome in batch. This requires a
+## PC cluster and a properly configured job manager (see cluster
+## options in RSAT_config.props).
+fp_scan_all_genes_batch: list_param
+	@${MAKE} fp_scan_all_genes BATCH='-batch'
+
+## Run footprint scan for a limited number of genes amon all the
+## genes of the genome (for quick tests and debugging)
+SKIP=0
+LAST=25
+fp_scan_gene_slice: list_param
+	@${MAKE} fp_scan_all_genes OPT='-skip ${SKIP} -last ${LAST}'
+
