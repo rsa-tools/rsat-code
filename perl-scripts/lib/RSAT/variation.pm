1effa5339e6e9ca923f47a357c1348fce4586ae4
Regulatory Sequence Analysis Tools account <Jacques.van-Helden@univ-amu.fr>
2015-03-16 18:00:23 +0100

    to be developed

---
+++ perl-scripts/lib/RSAT/variation.pm
@@ -0,0 +1,15 @@
+################################################################
+## This module handles a library of utilities to manipulate variations
+## and regulatory SNPs.
+
+=pod
+
+=item B<read_varscan_file>
+
+Read predicted rSNPs from a variation scan result file.
+
+=cut
+sub read_varscan_file {
+  &RSAT::message::Warning("NOT IMPLEMENTED YET");
+
+}
