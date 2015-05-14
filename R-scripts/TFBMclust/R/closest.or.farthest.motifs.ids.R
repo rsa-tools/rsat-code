7826878917f3ab6ae01f0c49c607591180695c4d
Jaime CASTRO <jcastro@lcg.unam.mx>
2015-05-14 15:11:58 +0200

    Paremeters update

--- R-scripts/TFBMclust/R/closest.or.farthest.motifs.ids.R
+++ R-scripts/TFBMclust/R/closest.or.farthest.motifs.ids.R
@@ -2,7 +2,7 @@
 ## Identify the "central" leaf (motif) of a subtree (cluster),
 ## i.e. the pair of motif with the smallest average distance to
 ## among all the other motifs of these clusters.
-closest.or.farthest.motifs.ids <- function(id1, id2, compa.table, score = "Ncor", closest = TRUE){
+closest.or.farthest.motifs.ids <- function(id1, id2, compa.table, metric = "Ncor", closest = TRUE){
 
   ## Get the comparison number between the pairs of motifs of both clusters
   compa.numbers <- as.vector(get.comparison.number(id1, id2, compa.table))
@@ -11,31 +11,31 @@ closest.or.farthest.motifs.ids <- function(id1, id2, compa.table, score = "Ncor"
 
   #########################################
   ## For correlations (Higher is better)
-  if ((score == "Ncor")
-      || (score=="cor")
-      || (score=="logocor")
-      || (score=="Nlocogor")
-      || (score=="Icor")
-      || (score=="NIcor")
+  if ((metric == "Ncor")
+      || (metric=="cor")
+      || (metric=="logocor")
+      || (metric=="Nlocogor")
+      || (metric=="Icor")
+      || (metric=="NIcor")
   ) {
     if(closest == TRUE){
-      compa.info <- compa.table[compa.numbers,][which(compa.table[compa.numbers, score] == max(compa.table[compa.numbers, score])),c("id1","id2")][1,]
+      compa.info <- compa.table[compa.numbers,][which(compa.table[compa.numbers, metric] == max(compa.table[compa.numbers, metric])),c("id1","id2")][1,]
     }else{
-      compa.info <- compa.table[compa.numbers,][which(compa.table[compa.numbers, score] == min(compa.table[compa.numbers, score])),c("id1","id2")][1,]
+      compa.info <- compa.table[compa.numbers,][which(compa.table[compa.numbers, metric] == min(compa.table[compa.numbers, metric])),c("id1","id2")][1,]
     }
 
   #################################
   ## For distances (Lower is better)
-  }else if ((score == "dEucl")
-            || (score == "NsEucl")
-            || (score == "SSD")
-            || (score == "SW")
-            || (score == "NSW")
+  }else if ((metric == "dEucl")
+            || (metric == "NsEucl")
+            || (metric == "SSD")
+            || (metric == "SW")
+            || (metric == "NSW")
   ){
     if(closest == TRUE){
-      compa.info <- compa.table[compa.numbers,][which(compa.table[compa.numbers, score] == min(compa.table[compa.numbers, score])),c("id1","id2")][1,]
+      compa.info <- compa.table[compa.numbers,][which(compa.table[compa.numbers, metric] == min(compa.table[compa.numbers, metric])),c("id1","id2")][1,]
     } else {
-      compa.info <- compa.table[compa.numbers,][which(compa.table[compa.numbers, score] == max(compa.table[compa.numbers, score])),c("id1","id2")][1,]
+      compa.info <- compa.table[compa.numbers,][which(compa.table[compa.numbers, metric] == max(compa.table[compa.numbers, metric])),c("id1","id2")][1,]
     }
   }
   return(as.vector(unlist(compa.info)))
