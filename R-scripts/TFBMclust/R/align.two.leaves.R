cb75a35811d64dcee8dee4c7d9e9455e887e4ada
Jaime CASTRO <jcastro@lcg.unam.mx>
2015-08-20 18:15:53 +0200

    Change merge_level for node

--- R-scripts/TFBMclust/R/align.two.leaves.R
+++ R-scripts/TFBMclust/R/align.two.leaves.R
@@ -50,19 +50,19 @@ align.two.leaves <- function(child1,
   if(nodes.attributes == TRUE){
 
     ## Save the merge class: (class 1) two leaves, (class 2) one leaf and one cluster, (class 3) two clusters
-    internal.nodes.attributes[[paste("level_", merge.level, sep = "")]][["merge_class"]] <<- 1
+    internal.nodes.attributes[[paste("node_", merge.level, sep = "")]][["merge_class"]] <<- 1
 
     ## Save the status of the alignment
     if(aligned.motif.flag == 0){
-      internal.nodes.attributes[[paste("level_", merge.level, sep = "")]][["alignment_status"]] <<- "Non-aligned"
+      internal.nodes.attributes[[paste("node_", merge.level, sep = "")]][["alignment_status"]] <<- "Non-aligned"
     } else{
-      internal.nodes.attributes[[paste("level_", merge.level, sep = "")]][["alignment_status"]] <<- "Aligned"
+      internal.nodes.attributes[[paste("node_", merge.level, sep = "")]][["alignment_status"]] <<- "Aligned"
     }
-    internal.nodes.attributes[[paste("level_", merge.level, sep = "")]][["alignment_flag"]] <<- aligned.motif.flag
+    internal.nodes.attributes[[paste("node_", merge.level, sep = "")]][["alignment_flag"]] <<- aligned.motif.flag
 
     ## Save the number of the motifs on each cluster, for each merge level
-    internal.nodes.attributes[[paste("level_", merge.level, sep = "")]][["cluster_1"]] <<- paste(n1, collapse = " ")
-    internal.nodes.attributes[[paste("level_", merge.level, sep = "")]][["cluster_2"]] <<- paste(n2, collapse = " ")
+    internal.nodes.attributes[[paste("node_", merge.level, sep = "")]][["cluster_1"]] <<- paste(n1, collapse = " ")
+    internal.nodes.attributes[[paste("node_", merge.level, sep = "")]][["cluster_2"]] <<- paste(n2, collapse = " ")
   }
 
   ## In case the motifs should not be aligned export the default parameters
