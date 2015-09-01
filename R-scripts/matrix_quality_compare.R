239b698b64145b8580800ed90b5c36974820752e
amedina <medina.alexiel@gmail.com>
2015-09-01 17:25:11 -0500

    added max nwd to only singificant pvalues

--- R-scripts/matrix_quality_compare.R
+++ R-scripts/matrix_quality_compare.R
@@ -17,9 +17,10 @@ args <- commandArgs(TRUE)
 mtx.quality.prefix.list.file <- args[1]
 
 ## For debugging:
-##mtx.quality.prefix.list.file <-"../results/matrix_quality/20150428/zoo_chip_enrichment_quality_prefix_files.txt"
 
-mtx.quality.prefix.list <-as.vector(as.matrix( read.table(file=mtx.quality.prefix.list.file, header=FALSE, stringsAsFactors=FALSE)))
+mtx.quality.prefix.list.file <-"./results/matrix_quality/20150428/zoo_chip_enrichment_cluster_files.txt"
+
+mtx.quality.prefix.list <-as.list(as.matrix( read.table(file=mtx.quality.prefix.list.file, header=FALSE, stringsAsFactors=FALSE)))
 ## directory with all the matrix quality results
 ##DirList <-  list.files("mFuzz/mFuzz_counts_rlog_table_enh_Fd2_PulseFd05_ClustStdrz_k8/rsat/matrixQuality", pattern="mFuzz*", full.names=TRUE)
 
@@ -29,77 +30,101 @@ mtx.quality.prefix.list <-as.vector(as.matrix( read.table(file=mtx.quality.prefi
 
 # import all data in one: for each matrix-quality run, import the ...compare-scores.tab file (row=pVal, col=sequence NWD), get the max of each column (MNWD for each sequence) and make a list of all that
 
+################################################################
+## TODO ALE
+## 1) Read in full files (general matrix-quality run files) to keep using them for other processing options
+## 2) Program geting the maxvalue for each column in each nwd file
+## 3) Program getin only the maxvalue of significant pvalues
+## 4) Program geting the area under the curve of the NWDs
+## 5) Weigth the NWD area under the curve 
 ListAll<- lapply(mtx.quality.prefix.list , FUN=function(dir){
     dir <- dirname(dir)
-    as.data.frame(t(as.matrix(as.data.frame(lapply(list.files(dir, pattern=".+_nwd_.+_compare-scores.tab", full.names=TRUE), FUN=function(file){
+    table <- as.data.frame(t(as.matrix(as.data.frame(lapply(list.files(dir, pattern=".+_nwd_.+_compare-scores.tab", full.names=TRUE), FUN=function(file){
         print (paste("Reading in", file))
         MNWD <- apply(read.table(file, header=T, row.names=1, sep ="\t",na.strings="<NULL>",comment.char="", stringsAsFactors=FALSE), 2, FUN=function(x){
             max(x, na.rm=T)
         })
         ## This is a vector with the max NWD values
+        print (MNWD)
         return(MNWD)
         
 	})))))
+    #print (table)
 })
+################
+##
 
 # remove part of the motif name to make it more readable
 # # second line depens on the name of the file that was given for the output in matrix quality... ALE:Not sure what the rownames should be
 
-ListAll<-lapply(ListAll,function(DF) {rownames(DF) <- paste("",seq(1)); DF})
+ListAll<-lapply(ListAll,function(DF) {rownames(DF) <- paste("",seq(dim(ListAll[[1]])[1])); DF})
+
 #ListAll<-lapply(ListAll,function(DF) {colnames(DF)<-gsub("mFuzz_counts_rlog_table_enh_Fd2_PulseFd05_ClustStdrz_k8_","",colnames(DF)); DF})
 
 
 ListAll<-lapply(ListAll,function(DF) {colnames(DF)<-gsub("_score_distrib","",colnames(DF)); DF})
 ListAll<-lapply(ListAll,function(DF) {colnames(DF)<-gsub("_nwd.tab","",colnames(DF)); DF})
-
-# reformat the data
-## ALE: Since the rownames setting is not working properly this transformation is not correct
-
-TableAll<-do.call("cbind",ListAll)
-TableAll$cluster <- rownames(TableAll)
-TableAll_melt<-melt(TableAll, id.vars="cluster")
-TableAll_melt$cluster<-factor(TableAll_melt$cluster, level=(unique(TableAll_melt$cluster)))
-
-# put a higher threeshold on MNWD for scaling
-TableAll_melt$value[TableAll_melt$value>0.8] <- 0.8
-# heatmap
-heatmap<-ggplot(TableAll_melt, aes(cluster,variable)) +
-		geom_tile(aes(fill=value)) +
-		scale_fill_gradientn(colours=colorRampPalette(brewer.pal(9, "Greys"))(20)) +
-		theme(axis.ticks=element_blank()) + labs(x="clusters",y="") + scale_y_discrete(expand=c(0,0)) + scale_x_discrete(expand=c(0,0))
-
-heatmap
-#ggsave(file="mFuzz/mFuzz_counts_rlog_table_enh_Fd2_PulseFd05_ClustStdrz_k8/rsat/matrixQuality/MNWD_Heatmap.pdf",plot=heatmap, height=5, width=20)
+ListAll<-lapply(ListAll,function(DF) {colnames(DF)<-gsub("_top_.+","",colnames(DF)); DF})
 
 
+draw.heatmap <- function (ListAll,heatmap.file){
+    ## reformat the data
+    
+    TableAll<-do.call("cbind",ListAll)
+    TableAll$sequence <- rownames(TableAll)
+    TableAll_melt<-melt(TableAll, id.vars="sequence")
+    TableAll_melt$sequence<-factor(TableAll_melt$sequence, level=(unique(TableAll_melt$sequence)))
+    
+    ## put a higher threeshold on MNWD for scaling
+    
+    
+    TableAll_melt$value[TableAll_melt$value>0.8] <- 0.8
+    heatmap<-ggplot(TableAll_melt, aes(sequence,variable)) +
+        geom_tile(aes(fill=value)) +
+            scale_fill_gradientn(colours=colorRampPalette(brewer.pal(9, "Greys"))(20)) +
+		theme(axis.ticks=element_blank()) + labs(x="sequence",y="") + scale_y_discrete(expand=c(0,0)) + scale_x_discrete(expand=c(0,0))
+    
+    heatmap
+    ggsave(file=heatmap.file,plot=heatmap, height=5, width=20)
+    
+}
 
-# same for a subset of selected motifs
-## TableAll_selected<-TableAll[,as.vector(SelectedMotifs$MotifId)]
-## colnames(TableAll_selected)<-SelectedMotifs$KnownMotif
-## TableAll_selected$cluster <- rownames(TableAll_selected)
-## TableAll_selected_melt<-melt(TableAll_selected, id.vars="cluster")
-## TableAll_selected_melt$cluster<-factor(TableAll_selected_melt$cluster, level=(unique(TableAll_selected_melt$cluster)))
+draw.heatmap(ListAll=ListAll,heatmap.file="maxNWD.pdf")
 
-## heatmap<-ggplot(TableAll_selected_melt, aes(cluster,variable)) +
-## 		geom_tile(aes(fill=value)) +
-## 		scale_fill_gradientn(colours=colorRampPalette(brewer.pal(9, "Greys"))(20)) +
-## 		theme(axis.ticks=element_blank()) + labs(x="clusters",y="") + scale_y_discrete(expand=c(0,0)) + scale_x_discrete(expand=c(0,0))
-## ggsave(file="mFuzz/mFuzz_counts_rlog_table_enh_Fd2_PulseFd05_ClustStdrz_k8/rsat/matrixQuality/MNWD_Heatmap_selected.pdf",plot=heatmap, height=5, width=20)
+#################
+
+ListAll.SigPval<- lapply(mtx.quality.prefix.list , FUN=function(dir){
+    dir <- dirname(dir)
+    table <- as.data.frame(t(as.matrix(as.data.frame(lapply(list.files(dir, pattern=".+_nwd_.+_compare-scores.tab", full.names=TRUE), FUN=function(file){
+        print (paste("Reading in", file))
+        nwd.table <- read.table(file, header=T, row.names=1, sep ="\t",na.strings="<NULL>",comment.char="", stringsAsFactors=FALSE)
+        print (dim( nwd.table))
+        nwd.table <-nwd.table[row.names(nwd.table)<=0.005,]
+        #print (head(nwd.table))
+        print (dim( nwd.table))
+        #stop()
+        
+        MNWD <- apply( nwd.table, 2, FUN=function(x){            
+            max(x, na.rm=T)
+        })
+        ## This is a vector with the max NWD values
+        #print (MNWD)
+        return(MNWD)
+        
+	})))))
+    #print (table)
+})
+
+# remove part of the motif name to make it more readable
+# # second line depens on the name of the file that was given for the output in matrix quality... ALE:Not sure what the rownames should be
+
+ListAll.SigPval<-lapply(ListAll.SigPval,function(DF) {rownames(DF) <- paste("",seq(dim(ListAll[[1]])[1])); DF})
+
+#ListAll<-lapply(ListAll,function(DF) {colnames(DF)<-gsub("mFuzz_counts_rlog_table_enh_Fd2_PulseFd05_ClustStdrz_k8_","",colnames(DF)); DF})
 
 
-## # same for a subset of selected motifs
-## TableAll_selectedStringent<-TableAll[,as.vector(SelectedMotifsStringent$MotifId)]
-## colnames(TableAll_selectedStringent)<-SelectedMotifsStringent$KnownMotif
-## TableAll_selectedStringent$cluster <- rownames(TableAll_selectedStringent)
-## TableAll_selectedStringent_melt<-melt(TableAll_selectedStringent, id.vars="cluster")
-## TableAll_selectedStringent_melt$cluster<-factor(TableAll_selectedStringent_melt$cluster, level=(unique(TableAll_selectedStringent_melt$cluster)))
+ListAll.SigPval<-lapply(ListAll.SigPval,function(DF) {colnames(DF)<-gsub("_score_distrib","",colnames(DF)); DF})
+ListAll.SigPval<-lapply(ListAll.SigPval,function(DF) {colnames(DF)<-gsub("_nwd.tab","",colnames(DF)); DF})
+ListAll.SigPval<-lapply(ListAll.SigPval,function(DF) {colnames(DF)<-gsub("_top_.+","",colnames(DF)); DF})
 
-## heatmap<-ggplot(TableAll_selectedStringent_melt, aes(cluster,variable)) +
-## 		geom_tile(aes(fill=value)) +
-## 		scale_fill_gradientn(colours=colorRampPalette(brewer.pal(9, "YlGnBu"))(20)) +
-## 		theme(text = element_text(size=30),axis.ticks=element_blank()) + labs(x="clusters",y="") + scale_y_discrete(expand=c(0,0)) + scale_x_discrete(expand=c(0,0))
-
-## heatmap
-
-##                                         #ggsave(file="mFuzz/mFuzz_counts_rlog_table_enh_Fd2_PulseFd05_ClustStdrz_k8/rsat/matrixQuality/MNWD_Heatmap_selected2.pdf",plot=heatmap, height=10, width=5)
-
+draw.heatmap(ListAll=ListAll.SigPval,heatmap.file="maxNWD_on_significant_pvalues.pdf")
