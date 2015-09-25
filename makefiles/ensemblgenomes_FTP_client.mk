5ff69c88a6fd5d0b7e758af45a5e30d6634a1197
RSAT admin at floresta plants.rsat.eu <bcontreras@eead.csic.es>
2015-09-25 16:29:00 +0200

    removed wget -N for target organisms

--- makefiles/ensemblgenomes_FTP_client.mk
+++ makefiles/ensemblgenomes_FTP_client.mk
@@ -33,7 +33,7 @@ organisms:
 	@echo
 	@mkdir -p ${ORGANISMS_DIR}
 	@echo "Getting list if organisms from ${DATABASE}"
-	@wget -Ncnv ${SERVERLIST} -O ${ORGANISMS_LIST} 
+	@wget -cnv ${SERVERLIST} -O ${ORGANISMS_LIST} 
 	@echo
 	@echo "	${ORGANISMS_LIST}"
 
