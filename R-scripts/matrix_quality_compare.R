377b488642cde1614004ec36d4146447252906d6
Jaime CASTRO <jcastro@lcg.unam.mx>
2015-11-06 15:11:58 +0100

    Change heatmap colors and labels

--- R-scripts/matrix_quality_compare.R
+++ R-scripts/matrix_quality_compare.R
@@ -126,8 +126,8 @@ draw.heatmap <- function (ListAll,metric="max.nwd",heatmap.file, formats=c("pdf"
         
         ## JaimeCastro: Given the values can be negative I change the color scale to identify the negative values
 #         heatmap.2(as.matrix(metric.table), col=colorRampPalette(brewer.pal(9,"Blues"))(100) 
-#         heatmap.2(as.matrix(metric.table), col=colorRampPalette(brewer.pal(9, "YlOrRd"), space="Lab")(100) 
-        heatmap.2(as.matrix(metric.table), col=colorRampPalette(brewer.pal(11, "RdBu"), space="Lab")(100) 
+        heatmap.2(as.matrix(metric.table), col=colorRampPalette(brewer.pal(9, "YlOrRd"), space="Lab")(100) 
+#         heatmap.2(as.matrix(metric.table) , col=colorRampPalette(brewer.pal(11, "RdBu"), space="Lab")(100) 
                 , trace="none"
                 , margins=c(6,10)
                 , cexRow = 0.75 
