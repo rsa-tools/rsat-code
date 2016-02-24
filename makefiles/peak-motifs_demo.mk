4c4fba58ba6a66800ebc159feff5c985122cea0f
Jacques van Helden <jacques.van-helden@univ-amu.fr>
2016-02-24 08:41:26 -0600

    minor improvements

--- makefiles/peak-motifs_demo.mk
+++ makefiles/peak-motifs_demo.mk
@@ -24,7 +24,7 @@ list_params:
 	@echo "	DISCO		${DISCO}"
 	@echo "	MIN_OL		${MIN_OL}"
 	@echo "	MAX_OL		${MAX_OL}"
-	@echo "	PM_TASK		${PM_TASK}"
+	@echo "	TASK		${PM_TASK}"
 	@echo "	OPT		${OPT}"
 	@echo "	PM_CMD		${PM_CMD}"
 
@@ -33,23 +33,25 @@ list_params:
 MIN_OL=6
 MAX_OL=7
 DISCO=oligos,dyads,positions
-PM_TASK=purge,seqlen,composition,disco,merge_motifs,split_motifs,motifs_vs_motifs,cluster_motifs,motifs_vs_db,timelog,archive,scan,small_summary,synthesis
+TASK=purge,seqlen,composition,disco,merge_motifs,split_motifs,motifs_vs_motifs,cluster_motifs,motifs_vs_db,timelog,archive,scan,small_summary,synthesis
 MOTIF_PREFIX=Oct4_Chen2008_sites_from_Jaspar
 MERGE_LEN_OPT=-no_merge_lengths
+NMOTIFS=5
 PM_CMD=peak-motifs -v ${V} \
 		-title ${MOTIF_PREFIX} \
 		-i ${RSAT}/public_html/demo_files/peak-motifs_demo.fa \
 		-markov auto \
 		-disco ${DISCO} \
-		-nmotifs 5 -minol ${MIN_OL} -maxol ${MAX_OL} \
+		-nmotifs ${NMOTIFS} -minol ${MIN_OL} -maxol ${MAX_OL} \
 		${MERGE_LEN_OPT} -2str \
 		-origin center \
 		-source galaxy \
 		-motif_db jaspar_core_vertebrates tf ${RSAT}/public_html/motif_databases/JASPAR/jaspar_core_vertebrates_2015_03.tf \
 		-scan_markov 1 -source galaxy \
-		-task ${PM_TASK} \
+		-task ${TASK} \
 		-prefix peak-motifs \
 		-noov -img_format png \
+		${OPT} ${OPT2} \
 		-outdir ${DIR_PEAKMO}
 peakmo_demo:
 	@echo
@@ -62,4 +64,4 @@ peakmo_demo:
 quick_test:
 	@echo
 	@echo "Running peak-motifs demo with time-minimizing options for quick testing (results may be less relevant)."
-	@${MAKE} peakmo_demo OPT="-top_peaks 500 -max_seq_len 300" DIR_PEAKMO=results/peak-motifs_demo/Oct4_Chen2008_sites_from_Jaspar_quick_test DISCO=oligos MAX_OL=6 MIN_OL=6
+	@${MAKE} peakmo_demo OPT="-top_peaks 500 -max_seq_len 300" DIR_PEAKMO=results/peak-motifs_demo/Oct4_Chen2008_sites_from_Jaspar_quick_test DISCO=oligos MAX_OL=6 MIN_OL=6 NMOTIFS=2
