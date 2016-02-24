61d13efbd03e6649747df97ebd22b932f0bb60ff
Jacques van Helden <jacques.van-helden@univ-amu.fr>
2016-02-24 06:48:05 -0600

    added target quick_test

--- makefiles/peak-motifs_demo.mk
+++ makefiles/peak-motifs_demo.mk
@@ -9,6 +9,10 @@ V=2
 DIR_DATA=data
 DIR_PEAKMO=results/peak-motifs_demo/Oct4_Chen2008_sites_from_Jaspar
 
+################################################################
+## Print info about the demo
+info:
+	@echo "This demo runs peak-motifs on a sample data set containing Oct4 peaks detected by MACS+PeakSplitter in data from Chen et al., 2008."
 
 ################################################################
 ##  List default parameters
@@ -21,6 +25,7 @@ list_params:
 	@echo "	MIN_OL		${MIN_OL}"
 	@echo "	MAX_OL		${MAX_OL}"
 	@echo "	PM_TASK		${PM_TASK}"
+	@echo "	OPT		${OPT}"
 	@echo "	PM_CMD		${PM_CMD}"
 
 ################################################################
@@ -30,13 +35,14 @@ MAX_OL=7
 DISCO=oligos,dyads,positions
 PM_TASK=purge,seqlen,composition,disco,merge_motifs,split_motifs,motifs_vs_motifs,cluster_motifs,motifs_vs_db,timelog,archive,scan,small_summary,synthesis
 MOTIF_PREFIX=Oct4_Chen2008_sites_from_Jaspar
+MERGE_LEN_OPT=-no_merge_lengths
 PM_CMD=peak-motifs -v ${V} \
 		-title ${MOTIF_PREFIX} \
 		-i ${RSAT}/public_html/demo_files/peak-motifs_demo.fa \
 		-markov auto \
 		-disco ${DISCO} \
 		-nmotifs 5 -minol ${MIN_OL} -maxol ${MAX_OL} \
-		-no_merge_lengths -2str \
+		${MERGE_LEN_OPT} -2str \
 		-origin center \
 		-source galaxy \
 		-motif_db jaspar_core_vertebrates tf ${RSAT}/public_html/motif_databases/JASPAR/jaspar_core_vertebrates_2015_03.tf \
@@ -47,8 +53,13 @@ PM_CMD=peak-motifs -v ${V} \
 		-outdir ${DIR_PEAKMO}
 peakmo_demo:
 	@echo
-	@echo "Running peak motifs demo	${MOTIF_PREFIX}"
+	@echo "Running peak-motifs demo	${MOTIF_PREFIX}"
 	@mkdir -p ${DIR_PEAKMO}
 	${PM_CMD}
 	@echo "	${DIR_PEAKMO}"
 
+
+quick_test:
+	@echo
+	@echo "Running peak-motifs demo with time-minimizing options for quick testing (results may be less relevant)."
+	@${MAKE} peakmo_demo OPT="-top_peaks 500 -max_seq_len 300" DIR_PEAKMO=results/peak-motifs_demo/Oct4_Chen2008_sites_from_Jaspar_quick_test DISCO=oligos MAX_OL=6 MIN_OL=6
