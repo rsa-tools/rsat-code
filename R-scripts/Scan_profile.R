06d5c11d8ebb44aa5d81085481e587a2c588f94c
Regulatory Sequence Analysis Tools account <Jacques.van-Helden@univ-amu.fr>
2016-03-11 01:34:29 +0100

    Test to add chi-square values

--- R-scripts/Scan_profile.R
+++ R-scripts/Scan_profile.R
@@ -633,7 +633,7 @@ all.motifs <- all.motifs
 # html.template.file <- "Template/index.html"
 html.report <- readLines(html.template.file)
 
-profile.data.tab.html <- create.html.tab(datatable.info.tab[,c(1, 12,2:6,9:10,13)], img = 10)
+profile.data.tab.html <- create.html.tab(datatable.info.tab[,c(1, 12,2:6,9,2,13)], img = 10)
 profile.data.tab.html <- paste(profile.data.tab.html, collapse = "\n")
 html.report <- gsub("--tab--", profile.data.tab.html, html.report)
 
