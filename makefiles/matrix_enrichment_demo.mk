cc8d35485c870435c5dedf6fe96098f2d1c56c43
Regulatory Sequence Analysis Tools account <Jacques.van-Helden@univ-amu.fr>
2016-04-28 00:13:35 +0200

    Added new demo

--- makefiles/matrix_enrichment_demo.mk
+++ makefiles/matrix_enrichment_demo.mk
@@ -1,22 +1,28 @@
 ################################################################
-## template.mk makefile
 ## Usage: make -f matrix_enrichment_demo.mk
 
-
 include ${RSAT}/makefiles/util.mk
-
 MAKEFILE=${RSAT}/makefiles/matrix_enrichment_demo.mk
 
-DATE=20160218
+DATE=20160427
 DEMO_FILE_DIR=${RSAT}/public_html/demo_files
 
 V=2
-## CapStarr-seq demo 
+
+## Outfile
+MTXE_OUT=./results/matrix_enrichment/${DATE}
+MTXE_CAP_OUT=${MTXE_OUT}/capstarr-seq
+
+
+#######################
+## CapStarr-seq demo ##
+#######################
 
 ## 
 MOTIFS_FILE=${DEMO_FILE_DIR}/Capstarr_selected_TFs.tf
 MTX_FORMAT=transfac
 PSEUDO=1
+OPT=
 
 ## Bg model based in all humam promoter regions
 BACKGROUND_MODEL=${DEMO_FILE_DIR}/All_hpromoterRegions_BG_model_mkv_2.oligos
@@ -36,22 +42,67 @@ INACTIVE_SEQ_OPTIONS=-seq inactive ${INACTIVE_SEQ}
 
 SEQUENCES=${HELA_SEQ_OPTIONS} ${K562_SEQ_OPTIONS} ${INACTIVE_SEQ_OPTIONS}
 
+TITLE_CAPSTARR="'TF enrichment in CapStarr-seq data'"
 
-TITLE_CAPSTARR='TF enrichment in CapStarr-seq data'
 
-## Outfile
-MTXE_OUT=./results/matrix_enrichment/${DATE}
-MTXE_CAP_OUT=${MTXE_OUT}/capstarr-seq
+###################
+## ES cells demo ##
+###################
 
-## test command
+TITLE_ES_CELLS="'OCT4 motifs in OCT_SOX_NANOG sequences'"
+MTXE_ES_CELLS=${MTXE_OUT}/OCT4_enrichment/OCT4_enrichment
+OCT_MOTIFS=${DEMO_FILE_DIR}/MEME_ChIP_Oct4_matrices.tf
+
+OCT_SEQ=${DEMO_FILE_DIR}/Oct4_peaks.fasta
+NANOG_SEQ=${DEMO_FILE_DIR}/Nanog_peaks.fasta
+SOX_SEQ=${DEMO_FILE_DIR}/Sox2_peaks.fasta
+
+## sequence options
+OCT_SEQ_OPTIONS=-seq Oct4 ${OCT_SEQ}
+NANOG_SEQ_OPTIONS=-seq Nanog ${NANOG_SEQ}
+SOX_SEQ_OPTIONS=-seq Sox2 ${SOX_SEQ}
+
+ES_CELLS_SEQ=${OCT_SEQ_OPTIONS} ${NANOG_SEQ_OPTIONS} ${SOX_SEQ_OPTIONS}
+
+
+## Command
 MATRIX_ENRICHMENT_CDM=matrix-enrichment -v ${V} \
 	-matrix ${MOTIFS_FILE} -matrix_format ${MTX_FORMAT} -pseudo ${PSEUDO} \
 	-bgfile ${BACKGROUND_MODEL} -bg_format ${BG_FORMAT} \
 	${SEQUENCES} \
-	-title ${TITLE_CAPSTARR} \
-	-o ${MTXE_CAP_OUT}
+	-title ${TITLE} ${OPT}\
+	-o ${PREFIX}
 
-capstar_seq_demo:
-	@echo ${TITLE_CAPSTARR}
-	@echo ${MATRIX_ENRICHMENT_CDM}
-	@${MATRIX_ENRICHMENT_CDM}
+## Enrichment with different BG models
+MATRIX_ENRICHMENT_BG_INPUT_CDM=matrix-enrichment -v ${V} \
+	-matrix ${MOTIFS_FILE} -matrix_format ${MTX_FORMAT} -pseudo ${PSEUDO} \
+	-bg_input by_seq -markov 2 \
+	${SEQUENCES} \
+	-title ${TITLE} ${OPT} \
+	-o ${PREFIX}
+
+
+_enrichment:
+	@echo
+	@echo "Running 	${PREFIX}	${OPT}"
+	${MAKE} my_command MY_COMMAND="${MATRIX_ENRICHMENT_CDM}"
+	@echo "		${MATRIX_ENRICHMENT_CDM}"
+	@echo "		${PREFIX}_report.html"
+
+_bg_input:
+	@echo
+	@echo "Running 	${PREFIX}	${OPT}"
+	${MAKE} my_command MY_COMMAND="${MATRIX_ENRICHMENT_BG_INPUT_CDM}"
+	@echo "		${MATRIX_ENRICHMENT_BG_INPUT_CDM}"
+	@echo "		${PREFIX}_report.html"
+
+# capstar_seq_demo:
+# 	@echo
+# 	@echo ${TITLE_CAPSTARR}
+# 	@echo ${MATRIX_ENRICHMENT_CDM}
+# 	${MAKE} _enrichment TITLE=${TITLE_CAPSTARR} PREFIX=${MTXE_CAP_OUT}
+
+es_cells_demo:
+	@echo
+	@echo ${TITLE_ES_CELLS}
+	${MAKE} _bg_input TITLE=${TITLE_ES_CELLS} PREFIX=${MTXE_ES_CELLS} MOTIFS_FILE=${OCT_MOTIFS} SEQUENCES="${ES_CELLS_SEQ}"
