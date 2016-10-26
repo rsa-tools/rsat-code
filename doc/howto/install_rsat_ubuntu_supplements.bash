9230ade0def6152b9ed393e52874885600adcdec
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2016-10-26 18:37:51 +0200

    moved some stuff from main script to supplement installation options

--- doc/howto/install_rsat_ubuntu_supplements.bash
+++ doc/howto/install_rsat_ubuntu_supplements.bash
@@ -1,10 +1,124 @@
+################################################################
+## Optional elements for RSAT installation on specific servers.
+##
+## BEWARE: this script should not be run as such.  Each chunk of code
+## should be read by a Human being in order to evaluate if it has to
+## run for the local RSAT instance.
+
+################################################################
+##
+## Web server with Hard-wired IP. Only for fixed servers, not for
+## Virtual machines !
+##
+## Check the URL of the web services (RSAT_WS). By default, the server
+## addresses the WS requests to itself (http://localhost/rsat) because
+## web services are used for multi-tierd architecture of some Web
+## tools (retrieve-ensembl-seq, NeAT).
+cd $RSAT
+
+## Get the current IP address
+export IP="$(ifconfig | grep 'inet addr' |head -1 | cut -d ':' -f 2| cut -d ' ' -f 1)"
+# export IP=192.168.56.101
+echo ${IP}
+
+## Set a fixed IP address for the web services
+perl ${RSAT}/perl-scripts/configure_rsat.pl auto RSAT_WS=http://${IP}/rsat RSAT_WWWW=http://${IP}/rsat SUDO=''
+#perl ${RSAT}/perl-scripts/configure_rsat.pl auto RSAT_WS=auto RSAT_WWWW=auto
+# rsync -ruptvl RSAT_config.bashrc /etc/bash_completion.d/
 
 
+################################################################
+## Last step before delivery: reset the passowrd of the RSAT
+## administrator user (rsat), and define a user (vmuser).
+
+## Create a user for the virtual machine
+##
+## This VM user is separate from the rsat user, which only serves to
+## manage the RSAT software suite and related packages.
+##
+## For the sake of security, we force this user to change password at
+## first login
+
+## First delete this user (in case it was previously defined)
+##  sudo userdel --remove vmuser
+
+## Then create vmuser
+sudo useradd --password `openssl passwd -1 -salt xyz tochng`\
+    --home /home/vmuser \
+    --create-home \
+    --shell /bin/bash \
+    --comment "VM user" \
+    vmuser
+
+## Force vmuser to change password at first login
+sudo chage -d 0 vmuser
+
+## Force rsat user to change password at first login
+usermod --password `openssl passwd -1 -salt xyz tochng` rsat
+sudo chage -d 0 rsat
+
+## Add sudoer rights to vmuser and rsat users
+sudo chmod 644 /etc/sudoers
+sudo emacs -nw /etc/sudoers
+## Find the following line
+##     # User privilege specification
+##     root    ALL=(ALL:ALL) ALL
+## Below it, add the following line:
+##     rsat    ALL=(ALL:ALL) ALL
+##     vmuser  ALL=(ALL:ALL) ALL
+
+
+################################################################
+## We now change the owner of the RSAT package to the rsat user.
+cd ${RSAT_PARENT_PATH}
+chown -R rsat.rsat ${RSAT_HOME}
+
+
+## New (2016-03-25) : for the IFB cloud I suppress the RSAT user, and
+## install everything as root.
+
+## Note (2016-10-17) : I could actually always do the whole
+## installation as root, and if required create RSAT user only at the
+## very end, and chown the rsat directory then.
+
+# ## Create a specific user for RSAT. The user is named rsat
+# sudo adduser rsat
+# ## Full Name: Regulatory Sequence Analysis Tools admin
+
+# ## Grant sudoer privileges to the rsat user (will be more convenient for
+# ## installing Perl modules, software tools, etc)
+# visudo
+# ## then add the following line below "User privilege specification"
+# # rsat    ALL=(ALL:ALL) ALL
+
+################################################################
+## Install some software tools for NGS analysis
+################################################################
+
+cd ${RSAT}
+
+## TO BE DONE
+##
+## I should evaluate if I can use conda for this, it would greatly
+## facilitate the portability.
+
+
+
+################################################################
+## Install gene-regulation package
+
+export GR_PARENT_PATH=/packages
+mkdir -p ${GR_PARENT_PATH}
+cd ${GR_PARENT_PATH}
+
+################################################################
 ## !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 ## !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 ## !!!!!!!!!!!    ONLY FOR THE VM ON THE IFB CLOUD    !!!!!!!!!!!!!
 ## !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 ## !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
 ## Adapt RSAT icon for IFB cloud (should be adapted depending on VM
 ## type).
 cp ${RSAT}/public_html/images/ifb-logo-s.jpg   ${RSAT}/public_html/images/RSAT_icon.jpg
@@ -62,20 +176,3 @@ qconf -as localhost ## aggregate the localhost tho the list of submitters
 sudo apt-get install -y ganglia-monitor rrdtool gmetad ganglia-webfrontend
 sudo cp /etc/ganglia-webfrontend/apache.conf /etc/apache2/sites-enabled/ganglia.conf
 sudo apachectl restart
-
-################################################################
-## Install some software tools for NGS analysis
-################################################################
-
-cd ${RSAT}
-## TO BE DONE
-
-
-
-
-################################################################
-## Install gene-regulation package
-
-export GR_PARENT_PATH=/packages
-mkdir -p ${GR_PARENT_PATH}
-cd ${GR_PARENT_PATH}
