5768f0b5145f060b83270922ce3fc1dc23ae09c8
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2017-03-10 16:34:38 +0100

    restored sudo for cran

--- installer/06_perl_packages.bash
+++ installer/06_perl_packages.bash
@@ -1,5 +1,11 @@
 source installer/00_config.bash
 
+echo
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo "!!!!!!!     BEWARE: INSTALLATION REQUIRES SUDO RIGHTS       !!!!"
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo
+
 cd ${RSAT}; source RSAT_config.bashrc ## Reload the (updated) RSAT environment variables
 
 
@@ -57,14 +63,14 @@ echo "Missing Perl modules:     ${MISSING_PERL_MODULES}"
 ## Beware: the _noprompt suffix is optional. It has the advantage to
 ## avoid for the admin to confirm each installation step, but the
 ## counterpart is that errors may be overlooked.
-make SUDO='' -f makefiles/install_rsat.mk perl_modules_install PERL_MODULES="${MISSING_PERL_MODULES}"
+make SUDO='sudo' -f makefiles/install_rsat.mk perl_modules_install PERL_MODULES="${MISSING_PERL_MODULES}"
 
 ## Note: I had to force installation for the some modules, because
 ## there seem to be some circular dependencies.
 make -f makefiles/install_rsat.mk perl_modules_check
 MISSING_PERL_MODULES=`grep -v '^OK'  check_perl_modules_eval.txt | grep -v '^;' | grep -v "Object::InsideOut"`
 echo "Missing Perl modules:     ${MISSING_PERL_MODULES}"
-make SUDO='' -f makefiles/install_rsat.mk perl_modules_install_by_force PERL_MODULES_TO_FORCE="`grep -v '^OK'  check_perl_modules_eval.txt | grep -v '^;' | grep -v Object::InsideOut| cut -f 2 | xargs`"
+make SUDO='sudo' -f makefiles/install_rsat.mk perl_modules_install_by_force PERL_MODULES_TO_FORCE="`grep -v '^OK'  check_perl_modules_eval.txt | grep -v '^;' | grep -v Object::InsideOut| cut -f 2 | xargs`"
 
 ## Last check for Perl modules. 
 ## If some of them still fail (except Object::InsideOut), manual intervention will be required.
