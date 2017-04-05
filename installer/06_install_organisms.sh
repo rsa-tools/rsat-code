721924d18a425e2799e1486c0d41d5862322457f
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2017-04-05 05:51:11 +0200

    separate file because download-organisms requires prior installtion of Perl packages

---
+++ installer/06_install_organisms.sh
@@ -0,0 +1,23 @@
+source installer/00_config.bash
+
+echo
+echo "================================================================"
+echo "====    Installing some organisms on this RSAT instance     ===="
+echo "================================================================"
+echo
+
+cd ${RSAT}; source RSAT_config.bashrc ## Reload the (updated) RSAT environment variables
+## Install two model organisms, required for some of the Web tools.
+download-organism -v 1 -org Saccharomyces_cerevisiae \
+ -org Escherichia_coli_K_12_substr__MG1655_uid57779
+
+## Optionally, install some pluricellular model organisms
+# download-organism -v 1 -org Drosophila_melanogaster
+# download-organism -v 1 -org Caenorhabditis_elegans
+# download-organism -v 1 -org Arabidopsis_thaliana
+
+## Get the list of organisms supported on your computer.
+supported-organisms
+
+df -m > ${RSAT}/install_logs/df_$(date +%Y-%m-%d_%H-%M-%S)_rsat_organism_installed.txt
+
