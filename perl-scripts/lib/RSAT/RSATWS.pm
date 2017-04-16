d272ada9098ffd18c8912a108c08db8ed0309015
Regulatory Sequence Analysis Tools account <Jacques.van-Helden@univ-amu.fr>
2017-04-16 10:12:26 +0200

    minor comment

--- perl-scripts/lib/RSAT/RSATWS.pm
+++ perl-scripts/lib/RSAT/RSATWS.pm
@@ -6621,6 +6621,8 @@ sub UpdateLogFileWS {
     $ENV{rsat_site} = `hostname`;
     chomp($ENV{rsat_site});
   }
+
+  ## Define log file
   my $log_file = join("", $ENV{RSAT}, "/logs/log-file_", $ENV{rsat_site}, "_WS", sprintf("_%04d_%02d", $year+1900,$month+1));
   system("chmod a+w $log_file");
 #   print "LOG ### $log_file";
