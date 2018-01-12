f463cdeaa90dfbb0030a65b7735282023db15a0d
Raul Ossio <rossio@liigh.unam.mx>
2018-01-12 12:48:56 -0600

    Rauls Feature Map 2 basic

--- public_html/js/feature-map2.js
+++ public_html/js/feature-map2.js
@@ -1,24 +1,46 @@
 
-//Some global VARIABLES
-var graph = 0;
 
-//Event listeners
+//Some global variables
+var graph = 0; // to know if a feature graph has been made in the actual session (0 = not, 1 = yes)
+
+//EVENT LISTENERS
+//Input tab event listener
 document.getElementById('input_tab').addEventListener("click", function(){
    document.getElementById('File_input_section').style.display = "block";
    document.getElementById('FM_section').style.display = "none";
+   document.getElementById('Export_section').style.display = "none";
 });
 
+//Feature map tab event listener
 document.getElementById('FM_tab').addEventListener("click", function(){
    document.getElementById('File_input_section').style.display = "none";
+   document.getElementById('Export_section').style.display = "none";
    document.getElementById('FM_section').style.display = "block";
 });
 
-
-document.getElementById('Advanced_options_button').addEventListener("click", function(){
-   document.getElementById('Advanced_options_div').style.display = "block";
-   document.getElementById('Advanced_options_button').style.display = "none";
+//Export tab event listener
+document.getElementById('Export_tab').addEventListener("click", function(){
+   document.getElementById('File_input_section').style.display = "none";
+   document.getElementById('FM_section').style.display = "none";
+   document.getElementById('Export_section').style.display = "block";
 });
 
+//Advanced options button event listener
+var advanced_options_button = 0;
+document.getElementById('Advanced_options_button').addEventListener("click", function(){
+   if(advanced_options_button == 0){
+      document.getElementById('Advanced_options_div').style.display = "block";
+      document.getElementById('Advanced_options_button').innerHTML = "Hide advanced options";
+      advanced_options_button = 1;
+   }
+   else{
+      document.getElementById('Advanced_options_div').style.display = "none";
+      document.getElementById('Advanced_options_button').innerHTML = "View advanced options";
+      advanced_options_button = 0;
+   }
+});
+
+// Go button event listener
 document.getElementById("Go_button").addEventListener("click",function(){
    document.getElementById("File_input_section").style.display = "none";
    draw_feature_map();
@@ -30,9 +52,37 @@ document.getElementById("Go_button").addEventListener("click",function(){
    document.getElementById("FM_tab").classList.add("current");
 });
 
+// Reset button event listener
+document.getElementById("Reset_button").addEventListener("click",function(){
+   reset_fields();
+});
 
-//Javascript functions
+//CODE ON PROGRESS --- START  --- File uploading option
+// var f_name;
+// function handleFileSelect(evt) {
+// var files = evt.target.files; // FileList object
+//
+// // files is a FileList of File objects. List some properties.
+//
+// var output = [];
+// for (var i = 0, f; f = files[i]; i++) {
+//    output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
+//                f.size, ' bytes, last modified: ',
+//                f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
+//                '</li>');
+//
+//    console.log("NOMBRE DE ARCHIVO: "+ f.name);
+// }
+// document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
+//
+// }
+//
+// document.getElementById('files').addEventListener('change', handleFileSelect, false);
 
+//CODE ON PROGRESS --- END --- File uploading option
+
+//JAVASCRIPT FUNCTIONS
+//Function to calculate the with of a text string according to a given font and size
 function calculate_text_width(text, font, size){
    var c = document.getElementById("myCanvas");
    var ctx = c.getContext("2d");
@@ -42,23 +92,65 @@ function calculate_text_width(text, font, size){
    return size;
 }
 
+//Function to be called by the reset button to restore all the values of the input tab (basic and advanced options)
+function reset_fields(){
+   //Reset of basic options
+   //Reset title of graph field
+   document.getElementById("user_title").value = "";
+
+   //Reset Upload your file field
+   document.getElementById("FileUpload_text").value = "";
+
+   //Reset Select a format field
+   document.getElementById("Select_format").value = "feature map";
+
+   //Reset Feature list field
+   document.getElementById("Feature_list").value = "";
+
+   //Reset of advanced options
+   //Reset of title position
+   document.getElementById("title_left").checked = "true";
+
+   //Reset of legends view labels
+   document.getElementById("view_legend").checked = "true";
+
+   //Reset of legends position
+   document.getElementById("legend_top").checked = "true";
+
+   //Reset of view Scalebar
+   document.getElementById("view_scale").checked = "true";
+
+   //Reset of view score
+   document.getElementById("view_score").checked = "true";
+
+}
+
+// Function to draw the feature map svg
 function draw_feature_map(){
 
-   //VARIABLES FOR USE IN THE GRAPH
+   //Get values of all the basic and advanced options to make the first iteration of the feature map graph
+   var user_title = document.getElementById("user_title").value;
+   var json_file = document.getElementById('FileUpload').files[0]; // File path
+
    var title_position_left = document.getElementById('title_left');
    var title_position_center = document.getElementById('title_center');
    var title_position_right = document.getElementById('title_right');
 
+   //Advanced options variables
    var view_legend = document.getElementById('view_legend');
    var legend_top = document.getElementById('legend_top');
    var legend_bottom = document.getElementById('legend_bottom');
    var legend_left = document.getElementById('legend_left');
    var legend_right = document.getElementById('legend_right');
 
-   //Opening og the file, important to do it first because some calculations need to be performed before the creation of the SVG
-   d3.json("feature_maps_jsons/prueba_3.json", function(data){
+   var view_scale = document.getElementById('view_scale')
+   var view_score = document.getElementById('view_score');
 
-      //Printing points for the SVG graph
+
+   //Opening of the file, important to do it first because some calculations need to be performed before the creation of the SVG
+   d3.json("js/prueba_3.json", function(data){
+
+      //Printing coordinates for the SVG graph
       var p_title_x = 25;
       var p_title_y = 20;
       var p_title_width = 0;
@@ -93,7 +185,6 @@ function draw_feature_map(){
 
       var total_svg_width = document.getElementById('feature_maps').offsetWidth;  // According to the size of the div, this depends of the grid configuration on the HTML file
 
-
       //Calculate the height  of the SVG according to number of genes in the json file
       var gene_num = 0;
       var dedicated_space = 100;
@@ -114,13 +205,13 @@ function draw_feature_map(){
          .attr("width",total_svg_width)
          .attr("height",total_svg_height);
 
-// TITLE BAR CODE
+      // TITLE BAR CODE
       var g_title_x;
       var g_title_y;
       var g_title_anchor;
 
       //If title position LEFT
-      if (title_position_left.checked == true){
+      if (title_left.checked == true){
          g_title_x = p_title_x + p_title_margin_left;
          g_title_y = p_title_y;
          g_title_anchor = "start";
@@ -128,14 +219,14 @@ function draw_feature_map(){
 
 
       //If title position is CENTER
-      if (title_position_center.checked == true){
+      if (title_center.checked == true){
          g_title_x = total_svg_width/2;
          g_title_y = p_title_y;
          g_title_anchor = "middle";
       }
 
       //If title position id RIGHT
-      if (title_position_right.checked == true){
+      if (title_right.checked == true){
          g_title_x = total_svg_width -20;
          g_title_y = p_title_y;
          g_title_anchor = "end";
@@ -155,7 +246,7 @@ function draw_feature_map(){
       var title_user_x = g_title_x;
       var title_user_y = g_title_y + p_title_space;
 
-      var user_title = document.getElementById("user_title").value;
+
 
       //Print of the User title
       graph_svg.append("text")
@@ -167,7 +258,7 @@ function draw_feature_map(){
          .attr("fill","rgb(51,51,51)")
          .text(user_title);
 
-//LEGENDS BAR CODE
+      //LEGENDS BAR
       //Get all the legends and extract discard the repeated ones
       var label_array_total = [];
 
@@ -549,7 +640,7 @@ function draw_feature_map(){
       .attr("y2",function(d,i){
          return (gene_name_y + ((i+1)*gene_jump))-10;
          })
-      .attr("stroke","red")
+      .attr("stroke","#333333")
       .attr("stroke-width",1);
 
    // //Print ticks on each line
@@ -559,7 +650,7 @@ function draw_feature_map(){
 
    for(i=0; i < data.length; i ++){
 
-       gene_line_ticks_y = gene_name_y + ((i+1)*gene_jump)-10;
+      gene_line_ticks_y = gene_name_y + ((i+1)*gene_jump)-10;
 
       //Tick on cero
        graph_svg.append("line")
@@ -567,7 +658,7 @@ function draw_feature_map(){
           .attr("y1",gene_line_ticks_y - 3)
           .attr("x2",total_svg_width - 50)
           .attr("y2",gene_line_ticks_y + 3)
-          .attr("stroke","red")
+          .attr("stroke","#333333")
           .attr("stroke-width",1);
 
       //Tick on Start
@@ -576,7 +667,7 @@ function draw_feature_map(){
         .attr("y1",gene_line_ticks_y - 3)
         .attr("x2",total_svg_width - 50 - ((data[i].Start * line_width)/range))
         .attr("y2",gene_line_ticks_y + 3)
-        .attr("stroke","red")
+        .attr("stroke","#333333")
         .attr("stroke-width",.5);
 
       while(gene_line_ticks_x > (total_svg_width - 50 - ((data[i].Start * line_width)/range))){
@@ -585,7 +676,7 @@ function draw_feature_map(){
              .attr("y1",gene_line_ticks_y - 3)
              .attr("x2",gene_line_ticks_x)
              .attr("y2",gene_line_ticks_y + 3)
-             .attr("stroke","red")
+             .attr("stroke","#333333")
              .attr("stroke-width",.5);
 
              gene_line_ticks_x = gene_line_ticks_x - ((50 * line_width)/range);
@@ -611,8 +702,6 @@ function draw_feature_map(){
             .attr("height", (data[i].Features[f].score * 3))
             .attr("fill", function(){
                for(c=0; c < label_array_color.length; c++){
-
-
 
                   if(data[i].Features[f].seq == label_array_color[c].label){
                      console.log("SEC: " + data[i].Features[f].seq + "LABEL: " + label_array_color[c].label + "Color " + label_array_color[c].color);
@@ -658,7 +747,7 @@ function draw_feature_map(){
 }
 
 //JQuery functions
-
+//Function for the top menu (input, feature map, export)
 $('.breadcrumb-counter-nav-item').click(function () {
   $('.breadcrumb-counter-nav-item').removeClass('current');
   $(this).addClass('current');
