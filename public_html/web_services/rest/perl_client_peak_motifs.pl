491106eb213233c6721a610b9ce6feade5fa351c
Thi-Thuy-Nga NGUYEN <thnguyen@ldogmac15.ens.fr>
2019-02-20 12:52:20 +0100

    rest web service

---
+++ public_html/web_services/rest/perl_client_peak_motifs.pl
@@ -0,0 +1,21 @@
+use REST::Client;
+use JSON qw/encode_json decode_json/;
+use HTTP::Request::Common;
+
+my $rsat_server = "http://rsat-tagc.univ-mrs.fr/rest/peak-motifs";
+my $client = REST::Client->new();
+
+my %args = ();
+$args{"title"} = "Human_from_Jaspar";
+$args{"markov"} = "auto";
+$args{"disco"} = "oligos,positions";
+$args{"motif_db"} = "jaspar_core_nonredundant_vertebrates";
+$args{"tmp_test_infile_URL"} = "http://rsat-tagc.univ-mrs.fr/rsat//demo_files/peak-motifs_demo.fa";
+$args{"task"} = "purge,seqlen,composition,disco,merge_motifs,split_motifs,motifs_vs_motifs,timelog,archive,synthesis,small_summary,motifs_vs_db,scan";
+
+$arg = encode_json(\%args);
+$client->POST($rsat_server, $arg, {'Content-type' => 'application/json'});
+
+print $client->responseContent;
+#my $ret = decode_json($client->responseContent);
+#print "Result file URL: " .$ret->{'server'} . "\n";
