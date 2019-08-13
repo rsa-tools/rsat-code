a6d4fa3b222996fde33a2b7f90ce1be4c14ac590
rsat user at TAGC <Jacques.van-Helden@univ-amu.fr>
2019-08-13 07:34:27 +0200

    added example of Perl REST client for variation-info

---
+++ public_html/web_services/rest/perl_client_variation_info.pl
@@ -0,0 +1,29 @@
+use JSON qw/encode_json decode_json/;
+use HTTP::Request::Common;
+
+use REST::Client;
+
+my $rsat_server = "http://rsat-tagc.univ-mrs.fr/rsat/rest/variation-info/Homo_sapiens/GRCh38";
+my $client = REST::Client->new();
+
+############## Example for sending JSON data
+my %args = ();
+$args{"i_string"} = "rs141426589";
+$args{"i_string_type"} = 'text';
+$args{"format"} = 'id';
+
+my $arg =  encode_json(\%args);
+$client->POST($rsat_server, $arg, {"Content-type"=>"application/json"});
+#$client->GET($rsat_server.'?content-type=application/json&genome=mm9&header_format=galaxy&i_string=http://rsat-tagc.univ-mrs.fr/rsat/demo_files/fetch-sequences_Schmidt_2011_mm9_CEBPA_SWEMBL_R0.12_702peaks.bed&i_string_type=url');
+my $ret = $client->responseContent;
+print $ret;
+
+############# Example for sending form-data including files
+#print "\nExample for sending form-data, return text/plain...\n";
+
+#my $req = HTTP::Request::Common::POST('', 'Content-type'=>'form-data', 'Content' => ['i'=>['/workspace/rsat/public_html/demo_files/fetch-sequences_Schmidt_2011_mm9_CEBPA_SWEMBL_R0.12_702peaks.bed'], 'genome'=>'mm9', 'header_format'=>'galaxy']);
+
+#my $body_content = $req->content;
+#my $headers = {'Content-type'=>$req->header('Content-type'), 'Accept'=>'text/plain'};
+#$client->POST($rsat_server, $body_content, $headers);
+#print $client->responseContent;
