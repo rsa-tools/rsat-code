9c9cdd769964474903f168a89542d43e0cfa3c2a
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2020-02-02 16:33:45 -0600

    moved protein interaction demo files in separate folder

--- public_html/alter_graph_form.php
+++ public_html/alter_graph_form.php
@@ -30,7 +30,7 @@
   # demo graph
   $demo = $_REQUEST['demo'];
   if ($demo == 1) {
-    $demo_graph = storeFile("demo_files/protein_interactions_uetz.tab");
+    $demo_graph = storeFile("demo_files/protein_interactions/protein_interactions_uetz.tab");
     $demo_ecolors = "selected";
     $demo_ewidth = "checked";
     $default_ecolors = "";
