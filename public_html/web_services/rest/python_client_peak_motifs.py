efed279ecda270b1f1577352eedee0988a509532
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2020-02-02 16:36:09 -0600

    moved Oct4 peaks to subfolder ChIP-seq_peaks

--- public_html/web_services/rest/python_client_peak_motifs.py
+++ public_html/web_services/rest/python_client_peak_motifs.py
@@ -4,14 +4,14 @@ import json
 rsat_server = "http://rsat-tagc.univ-mrs.fr/rest/"
 
 ###### EXAMPLE FOR SENDING JSON DATA #####
-#data = json.dumps({'title':'Human_from_Jaspar','markov':'auto','disco':'oligos,positions','motif_db':'jaspar_core_nonredundant_vertebrates','tmp_test_infile_URL':'http://rsat-tagc.univ-mrs.fr/rsat/demo_files/Oct4_peaks_top1000.fa','task':'purge,seqlen,composition,disco,merge_motifs,split_motifs,motifs_vs_motifs,timelog,archive,synthesis,small_summary,motifs_vs_db,scan'})
+#data = json.dumps({'title':'Human_from_Jaspar','markov':'auto','disco':'oligos,positions','motif_db':'jaspar_core_nonredundant_vertebrates','tmp_test_infile_URL':'http://rsat-tagc.univ-mrs.fr/rsat/demo_files/ChIP-seq_peaks/Oct4_peaks_top1000.fa','task':'purge,seqlen,composition,disco,merge_motifs,split_motifs,motifs_vs_motifs,timelog,archive,synthesis,small_summary,motifs_vs_db,scan'})
 #r = requests.post(rsat_server+'peak-motifs', headers={"Content-type":"application/json"}, data=data)
 #result = json.loads(r.content)
 #print("Result file URL : " + result['server'])
 ####### END EXAMPLE
 
 ######### EXAMPLE FOR SENDING FORM-DATA INCLUDING FILES AND OTHER TYPES OF DATA #######
-data = {'title':'Human_from_Jaspar', 'markov':'auto', 'disco':'oligos,positions', 'motif_db':'jaspar_core_nonredundant_vertebrates','tmp_test_infile_URL':'http://rsat-tagc.univ-mrs.fr/rsat/demo_files/Oct4_peaks_top1000.fa','task':'purge,seqlen,composition,disco,merge_motifs,split_motifs,motifs_vs_motifs,timelog,archive,synthesis,small_summary,motifs_vs_db,scan'}
+data = {'title':'Human_from_Jaspar', 'markov':'auto', 'disco':'oligos,positions', 'motif_db':'jaspar_core_nonredundant_vertebrates','tmp_test_infile_URL':'http://rsat-tagc.univ-mrs.fr/rsat/demo_files/ChIP-seq_peaks/Oct4_peaks_top1000.fa','task':'purge,seqlen,composition,disco,merge_motifs,split_motifs,motifs_vs_motifs,timelog,archive,synthesis,small_summary,motifs_vs_db,scan'}
 
 #files = {}
 r = requests.Request('POST', rsat_server+'peak-motifs', data=data).prepare()
