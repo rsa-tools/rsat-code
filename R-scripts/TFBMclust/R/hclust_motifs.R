1a33cdf0e72858f5ef17714e97c9ea30d094d320
rsat user at TAGC <Jacques.van-Helden@univ-amu.fr>
2020-02-25 15:34:58 +0100

    Improved/Updated comments

--- R-scripts/TFBMclust/R/hclust_motifs.R
+++ R-scripts/TFBMclust/R/hclust_motifs.R
@@ -1,12 +1,6 @@
-##################################################################
-## Build the tree by hierarchical clustering using Rcpp library
-## if it is indicated, export the tree in Newick format
+###################################################################
+## Compute the hierarchical clustering on a given distance matrix
 hclust.motifs <- function(dist.matrix, hclust.method = "average"){
 
-#   if(!require("Rclusterpp")){
-#     install.packages("Rclusterpp")
-#   }
-
-#    return(Rclusterpp.hclust(dist.matrix, method = hclust.method))
    return(hclust(dist.matrix, method = hclust.method))
 }
