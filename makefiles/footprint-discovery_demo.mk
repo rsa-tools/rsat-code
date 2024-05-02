5c6acf0172b8ca03e7ff8b590076edd54a09d5cd
RSAT user on sinik.ccg.unam.mx <Jacques.van-Helden@univ-amu.fr>
2024-05-02 02:11:23 -0500

    added diamond and some parameters

--- makefiles/footprint-discovery_demo.mk
+++ makefiles/footprint-discovery_demo.mk
@@ -5,12 +5,26 @@ include ${RSAT}/makefiles/util.mk
 MAKEFILE=${RSAT}/makefiles/footprint-discovery_demo.mk
 
 #ORG=Escherichia_coli_K_12_substr__MG1655_uid57779
+V=1
 ORG=Escherichia_coli_GCF_000005845.2_ASM584v2
 TAXON=Gammaproteobacteria
 GENE=lexA
 QUERY=-q ${GENE}
 BATCH=
+DIAMOND=-diamond
+UNIQUE_OPT=
+#UNIQUE_OPT=-unique_species
+FP_DISCO_DIR=results/footprints${INFER_OPERONS}/${BG_MODEL}
+BG_MODEL=taxfreq
+SKIP=0
+LAST=25
+#INFER_OPERONS=-infer_operons
+INFER_OPERONS=
+#TASK=all
 TASK=query_seq,filter_dyads,orthologs,ortho_seq,purge,dyads,maps,gene_index,index
+#TASK=operons,query_seq,filter_dyads,orthologs,ortho_seq,purge,dyads,maps,gene_index,index
+#TASK==bg_model,operons,query_seq,filter_dyads,orthologs,ortho_seq,purge,dyads,map_dyads,map_pssm,gene_index,index
+#TASK==bg_model,operons,query_seq,filter_dyads,orthologs,ortho_seq,purge,dyads,map_dyads,map_pssm,network,gene_index,network_index,index
 
 list_param:
 	@echo "Parameters"
@@ -20,31 +34,43 @@ list_param:
 	@echo "	GENE_FILE	${GENE_FILE}"
 	@echo "	QUERY		${QUERY}"
 	@echo "	TASK		${TASK}"
+	@echo "	DIAMOND		${DIAMOND}"
+	@echo "	BG_MODEL	${BG_MODEL}"
 	@echo "	SKIP		${SKIP}"
 	@echo "	LAST		${LAST}"
 	@echo "	BATCH		${BATCH}"
 	@echo "	FP_DISCO_DIR	${FP_DISCO_DIR}"
 	@echo "	UNIQUE_OPT	${UNIQUE_OPT}"
+	@echo "	INFER_OPERONS	${INFER_OPERONS}"
+	@echo "	OPT		${OPT}"
+	@echo "	FP_CMD		${FP_CMD}"
 
 ## Generic command for footprint-discovery (will be adapted in other
 ## targets by changing parameters).
-UNIQUE_OPT=-unique_species
-FP_DISCO_DIR=results/footprint-discovery
+FP_CMD=footprint-discovery -v ${V} ${DIAMOND} \
+	-org ${ORG} \
+	-taxon ${TAXON} \
+	${QUERY} ${INFER_OPERONS} \
+	${UNIQUE_OPT} \
+	-sep_genes \
+	-lth occ 1 \
+	-lth occ_sig 0 \
+	-uth rank 50 \
+	-return occ,proba,rank \
+	-filter \
+	-bg_model ${BG_MODEL} \
+	-task ${TASK} ${BATCH} ${OPT} \
+	-o ${FP_DISCO_DIR}
 _fp_disco:
 	@mkdir -p ${FP_DISCO_DIR}
 	@echo
 	@echo "Running footprint-discovery 	${ORG}	${TAXON}	${QUERY}"
-	footprint-discovery -v ${V} -org ${ORG} -taxon ${TAXON} \
-		${QUERY} ${UNIQUE_OPT} \
-		-sep_genes \
-		-lth occ 1 \
-		-lth occ_sig 0 \
-		-uth rank 50 \
-		-return occ,proba,rank \
-		-filter \
-		-bg_model taxfreq \
-		-task ${TASK} ${BATCH} ${OPT} \
-		-o ${FP_DISCO_DIR}
+	@${FP_CMD}
+
+################################################################
+## Run footprint-discovery with one selected gene
+fp_disco_one_gene: list_param
+	@${MAKE} _fp_disco QUERY='-q ${GENE}'
 
 
 ################################################################
@@ -62,11 +88,6 @@ fp_disco_selected_genes_yeast:
 		GENE_FILE=${YEAST_DEMO_GENES} \
 		ORG=Saccharomyces_cerevisiae \
 		TAXON=Saccharomycetales
-
-################################################################
-## Run footprint-discovery with one selected gene
-fp_disco_one_gene: list_param
-	@${MAKE} _fp_disco QUERY='-q ${GENE}'
 
 ################################################################
 ## Run footprint discovery with all the genes of the genome of
@@ -88,10 +109,8 @@ index_all_genes: list_param
 fp_disco_all_genes_batch: list_param
 	@${MAKE} fp_disco_all_genes BATCH='-batch'
 
-## Run footprint discovery for a limited number of genes amon all the
+## Run footprint discovery for a limited number of genes among all the
 ## genes of the genome (for quick tests and debugging)
-SKIP=0
-LAST=25
 fp_disco_gene_slice: list_param
 	@${MAKE} fp_disco_all_genes OPT='-skip ${SKIP} -last ${LAST}'
 
