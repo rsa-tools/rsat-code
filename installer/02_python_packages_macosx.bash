d93d2d6530deea003fc3e48b15d2a87b34482112
VAN HELDEN Jacques <Jacques.van-Helden@univ-amu.fr>
2026-05-17 10:24:22 +0200

    install weblogo with sudo  python3 -m pip

--- installer/02_python_packages_macosx.bash
+++ installer/02_python_packages_macosx.bash
@@ -19,10 +19,8 @@ brew install fisher
 brew install snakemake
 brew install python-requests
 brew install PyYAML
+brew install pipx
 
-# may2023, ubuntu 20.04
-# to avoid error with rpy2-3.5.12.tar.gz "note: This error originates from a subprocess, and is likely not a problem with pip",
-# not sure this solves "THIS FAILS on the IFB cloud. To be checked"
 
 ## 2024-02-24 : I don't find these libraries wih brew for MAC OS
 # brew install python-wheel
@@ -34,18 +32,10 @@ brew install PyYAML
 
 ## pip3 install pygraphviz ## This fails ! Command python setup.py egg_info failed with error code 1 in /tmp/pip_build_root/pygraphviz
 
-## Command python setup.py egg_info failed with error code 1 in /tmp/pip_build_root/wstools
-## Storing debug log for failure in /home/rsat/.pip/pip.log
-##
-## I also tried with easy_install3
-#       easy_install3 soappy
-## SAME ERROR: ImportError: No module named 'WSDLTools'
-
-## I should test one of the following SOAP packages
-# sudo pip3 install suds-jurko ## Inactivated on 2023-02-06 because does not work anymore with Ubuntu 22.04
-# soappy is not maintained, and don't install well on python3
-# sudo pip3 install soappy
-sudo python3 -m pip install weblogo
+# Install weblogo system-wide so that it is accessible
+# to the Apache user running RSAT services.
+echo "Installing weblogo via python2 pip requires sudo rights"
+sudo -H python3 -m pip install weblogo
 
 # required for downloading GO terms from biomart
 
