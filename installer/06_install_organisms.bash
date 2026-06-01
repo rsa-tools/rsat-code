ec2abc694a5e6c9f58d7def96915f01f1fe5523c
jvanheld <jacques.van-helden@univ-amu.fr>
2026-06-02 00:23:54 +0200

    updated default organism name for E.coli

--- installer/06_install_organisms.bash
+++ installer/06_install_organisms.bash
@@ -17,12 +17,13 @@ cd ${RSAT}; source RSAT_config.bashrc ## Reload the (updated) RSAT environment v
 download-organism -v 1 -server ${RSAT_SERVER} \
     -org Saccharomyces_cerevisiae \
     -org Escherichia_coli_K_12_substr__MG1655_uid57779 \
-    -org Escherichia_coli_GCF_000005845.2_ASM584v2
+    -org Escherichia_coli_str._K-12_substr._MG1655_GCF_000005845.2_ASM584v2
 
 ## Optionally, install some pluricellular model organisms
 # download-organism -v 1 -org Drosophila_melanogaster
 # download-organism -v 1 -org Caenorhabditis_elegans
 # download-organism -v 1 -org Arabidopsis_thaliana
+# download-organism -v 1-org Escherichia_coli_GCF_000005845.2_ASM584v2
 
 ## Get the list of organisms supported on your computer.
 supported-organisms
