6c508914911cb294bc01873dcd3d0a14ff15582a
jvanheld <jacques.van-helden@univ-amu.fr>
2026-06-01 10:27:39 +0200

    temporarily use Plants server to download model organisms

--- installer/06_install_organisms.bash
+++ installer/06_install_organisms.bash
@@ -8,9 +8,14 @@ echo "====    Installing some organisms on this RSAT instance     ===="
 echo "================================================================"
 echo
 
+
+## Temporary: use Plants server to download demo genomes
+RSAT_SERVER=https://rsat.eead.csic.es/plants/
+
 cd ${RSAT}; source RSAT_config.bashrc ## Reload the (updated) RSAT environment variables
 ## Install two model organisms, required for some of the Web tools.
-download-organism -v 1 -org Saccharomyces_cerevisiae \
+download-organism -v 1 -server ${RSAT_SERVER} \
+    -org Saccharomyces_cerevisiae \
     -org Escherichia_coli_K_12_substr__MG1655_uid57779 \
     -org Escherichia_coli_GCF_000005845.2_ASM584v2
 
